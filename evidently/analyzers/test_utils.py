d185160225fce56d3b435bdccb9e32c0a7e94b7b
Vyacheslav Morov <v.morov@corp.mail.ru>
2021-09-12 22:17:43 +0300

    Add example test and support tests in github actions.

---
+++ evidently/analyzers/test_utils.py
@@ -0,0 +1,22 @@
+import datetime
+import unittest
+
+import pandas
+
+from evidently.analyzers.utils import process_columns
+
+class TestUtils(unittest.TestCase):
+    def test_process_columns(self):
+        dataset = pandas.DataFrame.from_dict([
+            dict(datetime=datetime.datetime.now(),
+                 target=1,
+                 prediction=1,
+                 feature1=0,
+                 feature2=1,
+                 cat_feature1="o",
+                 cat_feature2="b")])
+
+        columns = process_columns(dataset, None)
+        self.assertIsNone(columns.utility_columns.id_column)
+        self.assertCountEqual(['feature1', 'feature2'], columns.num_feature_names)
+        self.assertCountEqual(['cat_feature1', 'cat_feature2'], columns.cat_feature_names)
