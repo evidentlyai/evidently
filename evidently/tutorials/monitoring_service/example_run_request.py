6545b92b362b115c781b6eafcb4abfd8687eb3c5
Andre B <AndreyMail@gmx.net>
2021-11-25 22:04:22 +0100

    A simple refactoring first (#1)
    refactor `NumpyEncoder`

--- evidently/tutorials/monitoring_service/example_run_request.py
+++ evidently/tutorials/monitoring_service/example_run_request.py
@@ -1,30 +1,10 @@
 import json
 import time
 
-import numpy as np
 import pandas
 import requests
 
-_integer_types = (np.int_, np.intc, np.intp,
-                  np.int8, np.int16, np.int32,
-                  np.int64, np.uint8, np.uint16, np.uint32, np.uint64)
-_float_types = (np.float_, np.float16, np.float32, np.float64)
-
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, _integer_types):
-            return int(o)
-        if isinstance(o, (np.float_, np.float16, np.float32, np.float64)):
-            return float(o)
-        if isinstance(o, (np.ndarray,)):
-            return o.tolist()
-        if isinstance(o, np.bool_):
-            return bool(o)
-        if isinstance(o, np.void):
-            return None
-
-        return json.JSONEncoder.default(self, o)
+from evidently.utils import NumpyEncoder
 
 
 if __name__ == '__main__':
