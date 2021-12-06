946422e3d6de16d4a933ce0ace319410a59a9629
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2021-12-06 18:26:55 +0300

    Add function options to data_drift.

--- evidently/analyzers/data_drift_analyzer.py
+++ evidently/analyzers/data_drift_analyzer.py
@@ -42,7 +42,14 @@ class DataDriftAnalyzer(Analyzer):
         p_values = []
 
         for feature_name in num_feature_names:
-            p_value = ks_2samp(reference_data[feature_name], current_data[feature_name])[1]
+            func = None if options.feature_stattest_func is None \
+                else options.feature_stattest_func.get(feature_name, None)
+            func = options.stattest_func if func is None else func
+
+            def ks_stat_test(reference_data, current_data):
+                return ks_2samp(reference_data, current_data)[1]
+            func = ks_stat_test if func is None else func
+            p_value = func(reference_data[feature_name], current_data[feature_name])
             p_values.append(p_value)
             if nbinsx:
                 current_nbinsx = nbinsx.get(feature_name) if nbinsx.get(feature_name) else 10
@@ -60,33 +67,18 @@ class DataDriftAnalyzer(Analyzer):
             )
 
         for feature_name in cat_feature_names:
-            ref_feature_vc = reference_data[feature_name][np.isfinite(reference_data[feature_name])].value_counts()
-            current_feature_vc = current_data[feature_name][np.isfinite(current_data[feature_name])].value_counts()
-
+            func = options.feature_stattest_func.get(feature_name, None)
+            func = options.stattest_func if func is None else func
             keys = set(list(reference_data[feature_name][np.isfinite(reference_data[feature_name])].unique()) +
                        list(current_data[feature_name][np.isfinite(current_data[feature_name])].unique()))
 
-            ref_feature_dict = dict.fromkeys(keys, 0)
-            for key, item in zip(ref_feature_vc.index, ref_feature_vc.values):
-                ref_feature_dict[key] = item
-
-            current_feature_dict = dict.fromkeys(keys, 0)
-            for key, item in zip(current_feature_vc.index, current_feature_vc.values):
-                current_feature_dict[key] = item
-
             if len(keys) > 2:
-                f_exp = [value[1] for value in sorted(ref_feature_dict.items())]
-                f_obs = [value[1] for value in sorted(current_feature_dict.items())]
                 # CHI2 to be implemented for cases with different categories
-                p_value = chisquare(f_exp, f_obs)[1]
+                func = chi_stat_test if func is None else func
+                p_value = func(reference_data[feature_name], current_data[feature_name])
             else:
-                ordered_keys = sorted(list(keys))
-                p_value = proportions_diff_z_test(
-                    proportions_diff_z_stat_ind(
-                        reference_data[feature_name].apply(lambda x, key=ordered_keys[0]: 0 if x == key else 1),
-                        current_data[feature_name].apply(lambda x, key=ordered_keys[0]: 0 if x == key else 1)
-                    )
-                )
+                func = z_stat_test if func is None else func
+                p_value = func(reference_data[feature_name], current_data[feature_name])
 
             p_values.append(p_value)
 
@@ -112,3 +104,33 @@ class DataDriftAnalyzer(Analyzer):
         result['metrics']['share_drifted_features'] = share_drifted_features
         result['metrics']['dataset_drift'] = dataset_drift
         return result
+
+
+def chi_stat_test(reference_data, current_data):
+    ref_feature_vc = reference_data[np.isfinite(reference_data)].value_counts()
+    current_feature_vc = current_data[np.isfinite(current_data)].value_counts()
+    keys = set(list(reference_data[np.isfinite(reference_data)].unique()) +
+               list(current_data[np.isfinite(current_data)].unique()))
+
+    ref_feature_dict = dict.fromkeys(keys, 0)
+    for key, item in zip(ref_feature_vc.index, ref_feature_vc.values):
+        ref_feature_dict[key] = item
+
+    current_feature_dict = dict.fromkeys(keys, 0)
+    for key, item in zip(current_feature_vc.index, current_feature_vc.values):
+        current_feature_dict[key] = item
+    f_exp = [value[1] for value in sorted(ref_feature_dict.items())]
+    f_obs = [value[1] for value in sorted(current_feature_dict.items())]
+    return chisquare(f_exp, f_obs)[1]
+
+
+def z_stat_test(reference_data, current_data):
+    keys = set(list(reference_data[np.isfinite(reference_data)].unique()) +
+               list(current_data[np.isfinite(current_data)].unique()))
+    ordered_keys = sorted(list(keys))
+    return proportions_diff_z_test(
+        proportions_diff_z_stat_ind(
+            reference_data.apply(lambda x, key=ordered_keys[0]: 0 if x == key else 1),
+            current_data.apply(lambda x, key=ordered_keys[0]: 0 if x == key else 1)
+        )
+    )
--- evidently/options/data_drift.py
+++ evidently/options/data_drift.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, Dict
+from typing import Optional, Dict, Callable
 
 
 @dataclass
@@ -8,6 +8,8 @@ class DataDriftOptions:
     drift_share: float = 0.5
     nbinsx: Optional[Dict[str, int]] = None
     xbins: Optional[Dict[str, int]] = None
+    stattest_func: Optional[Callable] = None
+    feature_stattest_func: Optional[Dict[str, Callable]] = None
 
     def as_dict(self):
         return {
