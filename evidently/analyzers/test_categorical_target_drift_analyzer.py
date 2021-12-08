fba6e338228e5fc76752b656f1714766c7bd8625
burkovae <AndreyMail@gmx.net>
2021-12-08 19:41:05 +0100

    add further tests

--- evidently/analyzers/test_categorical_target_drift_analyzer.py
+++ evidently/analyzers/test_categorical_target_drift_analyzer.py
@@ -53,6 +53,67 @@ class TestCatTargetDriftAnalyzer(TestCase):
 
         result = analyzer.calculate(df1, df2, ColumnMapping())
         self._assert_result_structure(result)
-        self.assertAlmostEqual(result['metrics']['target_drift'], 0.38, 2)
+        self.assertAlmostEqual(result['metrics']['target_drift'], 0.386, 2)
+        self.assertEqual(result['metrics']['target_name'], 'target')
+
+    def test_different_labels_1(self):
+        df1 = DataFrame({
+            'target': ['a', 'b']
+        })
+        df2 = DataFrame({
+            'target': ['c']
+        })
+        analyzer = CatTargetDriftAnalyzer()
+
+        # FIXME: RuntimeWarning: divide by zero encountered in true_divide
+        result = analyzer.calculate(df1, df2, ColumnMapping())
+        self._assert_result_structure(result)
+        self.assertAlmostEqual(result['metrics']['target_drift'], 0., 2)
+        self.assertEqual(result['metrics']['target_name'], 'target')
+
+    def test_different_labels_2(self):
+        df1 = DataFrame({
+            'target': ['c'] * 10
+        })
+        df2 = DataFrame({
+            'target': ['a', 'b'] * 10
+        })
+        analyzer = CatTargetDriftAnalyzer()
+
+        # FIXME: RuntimeWarning: divide by zero encountered in true_divide
+        result = analyzer.calculate(df1, df2, ColumnMapping())
+        self._assert_result_structure(result)
+        self.assertAlmostEqual(result['metrics']['target_drift'], 0., 2)
+        self.assertEqual(result['metrics']['target_name'], 'target')
+
+    def test_computation_of_categories_as_numbers(self):
+        df1 = DataFrame({
+            'target': [0, 1] * 10
+        })
+        df2 = DataFrame({
+            'target': [1] * 5
+        })
+        analyzer = CatTargetDriftAnalyzer()
+
+        # FIXME: RuntimeWarning: divide by zero encountered in true_divide
+        result = analyzer.calculate(df1, df2, ColumnMapping())
+        self._assert_result_structure(result)
+        self.assertAlmostEqual(result['metrics']['target_drift'], 0.04122, 3)
         self.assertEqual(result['metrics']['target_name'], 'target')
         print(result)
+
+    def test_computing_of_target_and_prediction(self):
+        df1 = DataFrame({
+            'target': ['a', 'b'] * 10,
+            'prediction': ['b', 'c'] * 10
+        })
+        df2 = DataFrame({
+            'target': ['b', 'c'] * 5,
+            'prediction': ['a', 'b'] * 5
+        })
+        analyzer = CatTargetDriftAnalyzer()
+        result = analyzer.calculate(df1, df2, ColumnMapping())
+        self._assert_result_structure(result)
+        self.assertAlmostEqual(result['metrics']['target_drift'], 0., 3)
+        self.assertEqual(result['metrics']['prediction_name'], 'prediction')
+        self.assertEqual(result['metrics']['prediction_type'], 'cat')
