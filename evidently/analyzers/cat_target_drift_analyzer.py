cd8e4a6b0237240109a28dd3d6679788cc4f6fee
burkovae <AndreyMail@gmx.net>
2021-12-08 20:30:56 +0100

    remove duplicate code

--- evidently/analyzers/cat_target_drift_analyzer.py
+++ evidently/analyzers/cat_target_drift_analyzer.py
@@ -19,6 +19,36 @@ def _remove_nans_and_infinities(dataframe):
     return dataframe
 
 
+def _compute_data_stats(reference_data: pd.DataFrame, current_data: pd.DataFrame, column_name: str):
+    ref_feature_vc = reference_data[column_name].value_counts()
+    current_feature_vc = current_data[column_name].value_counts()
+
+    keys = set(list(reference_data[column_name].unique()) +
+               list(current_data[column_name].unique()))
+
+    ref_feature_dict = dict.fromkeys(keys, 0)
+    for key, item in zip(ref_feature_vc.index, ref_feature_vc.values):
+        ref_feature_dict[key] = item
+
+    current_feature_dict = dict.fromkeys(keys, 0)
+    for key, item in zip(current_feature_vc.index, current_feature_vc.values):
+        current_feature_dict[key] = item
+
+    if len(keys) > 2:
+        f_exp = [value[1] for value in sorted(ref_feature_dict.items())]
+        f_obs = [value[1] for value in sorted(current_feature_dict.items())]
+        target_p_value = chisquare(f_exp, f_obs)[1]
+    else:
+        ordered_keys = sorted(list(keys))
+        target_p_value = proportions_diff_z_test(
+            proportions_diff_z_stat_ind(
+                reference_data[column_name].apply(lambda x: 0 if x == ordered_keys[0] else 1),
+                current_data[column_name].apply(lambda x: 0 if x == ordered_keys[0] else 1)
+            )
+        )
+    return target_p_value
+
+
 class CatTargetDriftAnalyzer(Analyzer):
     def calculate(self, reference_data: pd.DataFrame, current_data: pd.DataFrame, column_mapping) -> dict:
         columns = process_columns(reference_data, column_mapping)
@@ -34,68 +64,16 @@ class CatTargetDriftAnalyzer(Analyzer):
         result['metrics'] = {}
         # target drift
         if target_column is not None:
-            ref_feature_vc = reference_data[target_column].value_counts()
-            current_feature_vc = current_data[target_column].value_counts()
-
-            keys = set(list(reference_data[target_column].unique()) +
-                       list(current_data[target_column].unique()))
-
-            ref_feature_dict = dict.fromkeys(keys, 0)
-            for key, item in zip(ref_feature_vc.index, ref_feature_vc.values):
-                ref_feature_dict[key] = item
-
-            current_feature_dict = dict.fromkeys(keys, 0)
-            for key, item in zip(current_feature_vc.index, current_feature_vc.values):
-                current_feature_dict[key] = item
-
-            if len(keys) > 2:
-                f_exp = [value[1] for value in sorted(ref_feature_dict.items())]
-                f_obs = [value[1] for value in sorted(current_feature_dict.items())]
-                target_p_value = chisquare(f_exp, f_obs)[1]
-            else:
-                ordered_keys = sorted(list(keys))
-                target_p_value = proportions_diff_z_test(
-                    proportions_diff_z_stat_ind(
-                        reference_data[target_column].apply(lambda x: 0 if x == ordered_keys[0] else 1),
-                        current_data[target_column].apply(lambda x: 0 if x == ordered_keys[0] else 1)
-                    )
-                )
-
+            p_value = _compute_data_stats(reference_data, current_data, target_column)
             result['metrics']["target_name"] = target_column
             result['metrics']["target_type"] = 'cat'
-            result['metrics']["target_drift"] = target_p_value
+            result['metrics']["target_drift"] = p_value
 
         # prediction drift
         if prediction_column is not None:
-            ref_feature_vc = reference_data[prediction_column].value_counts()
-            current_feature_vc = current_data[prediction_column].value_counts()
-
-            keys = set(list(reference_data[prediction_column].unique()) +
-                       list(current_data[prediction_column].unique()))
-
-            ref_feature_dict = dict.fromkeys(keys, 0)
-            for key, item in zip(ref_feature_vc.index, ref_feature_vc.values):
-                ref_feature_dict[key] = item
-
-            current_feature_dict = dict.fromkeys(keys, 0)
-            for key, item in zip(current_feature_vc.index, current_feature_vc.values):
-                current_feature_dict[key] = item
-
-            if len(keys) > 2:
-                f_exp = [value[1] for value in sorted(ref_feature_dict.items())]
-                f_obs = [value[1] for value in sorted(current_feature_dict.items())]
-                pred_p_value = chisquare(f_exp, f_obs)[1]
-            else:
-                ordered_keys = sorted(list(keys))
-                pred_p_value = proportions_diff_z_test(
-                    proportions_diff_z_stat_ind(
-                        reference_data[prediction_column].apply(lambda x: 0 if x == ordered_keys[0] else 1),
-                        current_data[prediction_column].apply(lambda x: 0 if x == ordered_keys[0] else 1)
-                    )
-                )
-
+            p_value = _compute_data_stats(reference_data, current_data, prediction_column)
             result['metrics']["prediction_name"] = prediction_column
             result['metrics']["prediction_type"] = 'cat'
-            result['metrics']["prediction_drift"] = pred_p_value
+            result['metrics']["prediction_drift"] = p_value
 
         return result
