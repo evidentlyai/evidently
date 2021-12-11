e4157be4b4433bb1fab0ee053cc142d89c99f7d4
burkovae <AndreyMail@gmx.net>
2021-12-11 12:19:09 +0100

    first simplifications

--- evidently/analyzers/num_target_drift_analyzer.py
+++ evidently/analyzers/num_target_drift_analyzer.py
@@ -9,6 +9,21 @@ from evidently.analyzers.stattests import ks_stat_test
 from evidently.analyzers.utils import process_columns
 
 
+def _compute_correlation(reference_data: pd.DataFrame, current_data: pd.DataFrame, prefix,
+                         main_column, num_columns, stats_fun):
+    target_p_value = stats_fun(reference_data[main_column], current_data[main_column])
+    metrics = {
+        prefix + '_name': main_column,
+        prefix + '_type': 'num',
+        prefix + '_drift': target_p_value
+    }
+    ref_target_corr = reference_data[num_columns + [main_column]].corr()[main_column]
+    curr_target_corr = current_data[num_columns + [main_column]].corr()[main_column]
+    target_corr = {'reference': ref_target_corr.to_dict(), 'current': curr_target_corr.to_dict()}
+    metrics[prefix + '_correlations'] = target_corr
+    return metrics
+
+
 class NumTargetDriftAnalyzer(Analyzer):
     def calculate(self, reference_data: pd.DataFrame, current_data: pd.DataFrame, column_mapping) -> dict:
         options = self.options_provider.get(DataDriftOptions)
@@ -26,30 +41,12 @@ class NumTargetDriftAnalyzer(Analyzer):
         func = ks_stat_test if func is None else func
         # target
         if target_column is not None:
-            # drift
-            target_p_value = func(reference_data[target_column], current_data[target_column])
-            result['metrics']["target_name"] = target_column
-            result['metrics']["target_type"] = 'num'
-            result['metrics']["target_drift"] = target_p_value
-
-            # corr
-            ref_target_corr = reference_data[num_feature_names + [target_column]].corr()[target_column]
-            curr_target_corr = current_data[num_feature_names + [target_column]].corr()[target_column]
-            target_corr = {'reference': ref_target_corr.to_dict(), 'current': curr_target_corr.to_dict()}
-            result['metrics']['target_correlations'] = target_corr
+            metrics = _compute_correlation(reference_data, current_data, 'target', target_column, num_feature_names, func)
+            result['metrics'] = dict(**result['metrics'], **metrics)
 
         # prediction
         if prediction_column is not None:
-            # drift
-            pred_p_value = func(reference_data[prediction_column], current_data[prediction_column])
-            result['metrics']["prediction_name"] = prediction_column
-            result['metrics']["prediction_type"] = 'num'
-            result['metrics']["prediction_drift"] = pred_p_value
-
-            # corr
-            ref_pred_corr = reference_data[num_feature_names + [prediction_column]].corr()[prediction_column]
-            curr_pred_corr = current_data[num_feature_names + [prediction_column]].corr()[prediction_column]
-            prediction_corr = {'reference': ref_pred_corr.to_dict(), 'current': curr_pred_corr.to_dict()}
-            result['metrics']['prediction_correlations'] = prediction_corr
+            metrics = _compute_correlation(reference_data, current_data, 'prediction', prediction_column, num_feature_names, func)
+            result['metrics'] = dict(**result['metrics'], **metrics)
 
         return result
