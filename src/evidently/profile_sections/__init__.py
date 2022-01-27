4650e051af5e0536f9189efd948b49d7d855c8d3
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2022-01-27 18:22:11 +0300

    Ignore typing errors in __init__.py for backward compatibility parts.

--- src/evidently/profile_sections/__init__.py
+++ src/evidently/profile_sections/__init__.py
@@ -3,6 +3,6 @@ import warnings
 import evidently.model_profile.sections
 from evidently.model_profile.sections import *
 
-__path__ = evidently.model_profile.sections.__path__
+__path__ = evidently.model_profile.sections.__path__  # type: ignore
 
 warnings.warn("'import evidently.profile_sections' is deprecated, use 'import evidently.model_profile.sections'")
