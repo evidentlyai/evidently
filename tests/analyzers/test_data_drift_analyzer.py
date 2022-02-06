26143411cb3e83a73ecc003fbff89d2c4a797bce
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2022-02-07 00:22:10 +0300

    Add tests for cat feature in DataDrift.

--- tests/analyzers/test_data_drift_analyzer.py
+++ tests/analyzers/test_data_drift_analyzer.py
@@ -15,6 +15,22 @@ def data_drift_analyzer() -> DataDriftAnalyzer:
     return analyzer
 
 
+def sample_data(feature1, feature2, feature3):
+    return [{'feature1': t[0], 'feature2': t[1], 'feature3': t[2]} for t in zip(feature1, feature2, feature3)]
+
+
+@pytest.mark.parametrize(["reference", "current", "column_mapping"],
+                         [
+                             (sample_data([1], [1], [1]), sample_data([1], [1], [1]), ColumnMapping()),
+                             (sample_data(["1"], [1], [1]), sample_data(["1"], [1], [1]), ColumnMapping()),
+                             (sample_data([True], [1], [1]), sample_data([True], [1], [1]), ColumnMapping()),
+                         ])
+def test_data_drift_analyzer_no_exceptions(reference, current, column_mapping):
+    analyzer = DataDriftAnalyzer()
+    analyzer.options_provider = OptionsProvider()
+    analyzer.calculate(DataFrame(reference), DataFrame(current), column_mapping)
+
+
 def test_data_drift_analyzer_as_dict_format(data_drift_analyzer: DataDriftAnalyzer) -> None:
     test_data = DataFrame({
         'target': [1, 2, 3, 4],
