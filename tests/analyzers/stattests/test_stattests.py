5fb8b8d9ac118bb21fd26547dac7e8fc29fc0728
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2022-02-09 21:08:50 +0300

    Add ignoring NaN values in stat tests for cat features.

--- tests/analyzers/stattests/test_stattests.py
+++ tests/analyzers/stattests/test_stattests.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 from pytest import approx
 
+from evidently.analyzers.stattests import z_stat_test
 from evidently.analyzers.stattests.chisquare_stattest import chi_stat_test
 
 
@@ -9,6 +11,24 @@ def test_freq_obs_eq_freq_exp() -> None:
     reference = pd.Series([1, 2, 3, 4, 5, 6]).repeat([16, 18, 16, 14, 12, 12])
     current = pd.Series([1, 2, 3, 4, 5, 6]).repeat([16, 16, 16, 16, 16, 8])
     assert chi_stat_test(reference, current) == approx(0.62338, abs=1e-5)
+
+
+def test_chi_stat_test_cat_feature() -> None:
+    reference = pd.Series(["a", "b", "c"]).repeat([10, 10, 10])
+    current = pd.Series(["a", "b", "c"]).repeat([10, 10, 10])
+    assert chi_stat_test(reference, current) == approx(1.0, abs=1e-5)
+
+
+def test_z_stat_test_cat_feature() -> None:
+    reference = pd.Series(["a", "b"]).repeat([10, 10])
+    current = pd.Series(["a", "b"]).repeat([10, 10])
+    assert z_stat_test(reference, current) == approx(1.0, abs=1e-5)
+
+
+def test_cat_feature_with_nans() -> None:
+    reference = pd.Series(["a", "b", np.nan]).repeat([10, 10, 10])
+    current = pd.Series(["a", "b", np.nan]).repeat([10, 10, 10])
+    assert chi_stat_test(reference, current) == approx(1.0, abs=1e-5)
 
 
 def test_freq_obs_not_eq_freq_exp() -> None:
