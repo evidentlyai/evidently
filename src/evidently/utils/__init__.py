aef95ad2f7f09c92018373fd7e00b83c773c4b7f
Tapot <novakche@yandex.ru>
2022-02-19 23:51:40 +0300

    update NumpyEncoder and move it to a separate module

--- src/evidently/utils/__init__.py
+++ src/evidently/utils/__init__.py
@@ -1,13 +1,1 @@
-import json
-import numpy as np
-
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, np.void):
-            return None
-        if isinstance(o, (np.generic, np.bool_)):
-            return o.item()
-        if isinstance(o, np.ndarray):
-            return o.tolist()
-        return o
+from .numpy_encoder import NumpyEncoder
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,42 +1,65 @@
+import json
+from datetime import datetime
 import pytest
 
 import numpy as np
+import pandas as pd
 
-from evidently.utils import NumpyEncoder
-
-
-@pytest.fixture
-def numpy_encoder() -> NumpyEncoder:
-    return NumpyEncoder()
+from evidently.utils.numpy_encoder import NumpyEncoder
 
 
-def test_int_convert(numpy_encoder: NumpyEncoder) -> None:
-    for _type in (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32,
-                  np.int64, np.uint8, np.uint16, np.uint32, np.uint64):
-        assert numpy_encoder.default(_type(55)) == 55
+@pytest.mark.parametrize(
+    "test_object, expected_json",
+    (
+        (
+            [np.nan, np.inf, -np.inf],
+            "[NaN, Infinity, -Infinity]",
+        ),
+        (
+            [
+                _type(42)
+                for _type in (
+                    np.int_,
+                    np.intc,
+                    np.intp,
+                    np.int8,
+                    np.int16,
+                    np.int32,
+                    np.int64,
+                    np.uint8,
+                    np.uint16,
+                    np.uint32,
+                    np.uint64,
+                )
+            ],
+            "[42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42]",
+        ),
+        (
+            [_type(0.5) for _type in (np.float, np.float_, np.float16, np.float32, np.float64)],
+            "[0.5, 0.5, 0.5, 0.5, 0.5]",
+        ),
+        ([(_type(1), _type(0)) for _type in (np.bool, np.bool_)], "[[true, false], [true, false]]"),
+        (np.array([0, 1, 2.1, np.nan, np.inf, pd.NaT]), "[0, 1, 2.1, NaN, Infinity, null]"),
+        (np.empty((0, 0)), "[]"),
+        (np.array([[0, 1, 2.1], [0, 1, 2.1], [0, 1, 2.1]]), "[[0.0, 1.0, 2.1], [0.0, 1.0, 2.1], [0.0, 1.0, 2.1]]"),
+        (np.ones((2, 3)), "[[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]"),
+        (np.void(3), "null"),
+        # python types
+        (
+            [321123321123, 0.56, True, False, None, "test", {1: 4}],
+            '[321123321123, 0.56, true, false, null, "test", {"1": 4}]',
+        ),
+    ),
+)
+def test_numpy_encoder_compatible_types(test_object, expected_json: str) -> None:
+    assert json.dumps(test_object, cls=NumpyEncoder) == expected_json, test_object
 
 
-def test_float_convert(numpy_encoder: NumpyEncoder) -> None:
-    np.testing.assert_almost_equal(numpy_encoder.default(np.float16(1.5)), 1.5)
-    for _type in (np.float, np.float_, np.float32, np.float64):
-        np.testing.assert_almost_equal(numpy_encoder.default(_type(.2)), .2)
-
-
-def test_bool_covert(numpy_encoder: NumpyEncoder) -> None:
-    assert numpy_encoder.default(np.bool(1)) is True
-    assert numpy_encoder.default(np.bool(0)) is False
-    assert numpy_encoder.default(np.bool_(1)) is True
-    assert numpy_encoder.default(np.bool_(0)) is False
-
-
-def test_array_covert(numpy_encoder: NumpyEncoder) -> None:
-    assert numpy_encoder.default(np.array([0, 1, 2.1])) == [0, 1, 2.1]
-    assert numpy_encoder.default(np.empty((0, 0))) == []
-    assert numpy_encoder.default(
-        np.array([[0, 1, 2.1], [0, 1, 2.1], [0, 1, 2.1]])
-    ) == [[0, 1, 2.1], [0, 1, 2.1], [0, 1, 2.1]]
-    assert numpy_encoder.default(np.ones((2, 3))) == [[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]
-
-
-def test_none_covert(numpy_encoder: NumpyEncoder) -> None:
-    assert numpy_encoder.default(np.void(3)) is None
+@pytest.mark.parametrize(
+    "test_object, type_name_in_error",
+    ((datetime(year=2022, month=1, day=13, hour=12, minute=45, second=32), "datetime"), ({1, 2, 3}, "set")),
+)
+def test_numpy_encoder_incompatible_types(test_object, type_name_in_error: str) -> None:
+    with pytest.raises(TypeError) as error:
+        json.dumps(test_object, cls=NumpyEncoder)
+        assert type_name_in_error in str(error)
