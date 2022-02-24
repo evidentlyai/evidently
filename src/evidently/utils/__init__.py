a0262c0844bc26d9143f233ebbb49b0daf6691e1
Tapot <novakche@yandex.ru>
2022-02-24 15:09:50 +0300

    update NumpyEncoder and move it to a separate module (#161)
    * update NumpyEncoder and move it to a separate module
    
    * style fix with black
    
    * style fix

--- src/evidently/utils/__init__.py
+++ src/evidently/utils/__init__.py
@@ -1,14 +1,1 @@
-import json
-import numpy as np
-import pandas as pd
-
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, (np.void, type(pd.NaT))):
-            return None
-        if isinstance(o, (np.generic, np.bool_)):
-            return o.item()
-        if isinstance(o, np.ndarray):
-            return o.tolist()
-        return o
+from .numpy_encoder import NumpyEncoder
---
+++ src/evidently/utils/numpy_encoder.py
@@ -0,0 +1,32 @@
+import json
+
+import numpy as np
+import pandas as pd
+
+
+_TYPES_MAPPING = (
+    (
+        (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64),
+        int,
+    ),
+    ((np.float_, np.float16, np.float32, np.float64), float),
+    ((np.ndarray,), lambda obj: obj.tolist()),
+    ((np.bool, np.bool_), bool),
+    ((np.void, type(pd.NaT)), lambda obj: None),
+)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """Numpy and Pandas data types to JSON types encoder"""
+
+    def default(self, obj):
+        """JSON converter calls the method when it cannot convert an object to a Python type
+        Convert the object to a Python type
+
+        If we cannot convert the object, leave the default `JSONEncoder` behaviour - raise a TypeError exception.
+        """
+        for types_list, python_type in _TYPES_MAPPING:
+            if isinstance(obj, types_list):
+                return python_type(obj)
+
+        return json.JSONEncoder.default(self, obj)
