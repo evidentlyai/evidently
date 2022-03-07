e736ad3b0b492a3e6367a3e6f4ca5ccf7dab6d53
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2022-03-07 21:22:23 +0300

    Fix flake8 errors.

--- src/evidently/dashboard/widgets/expandable_list_widget.py
+++ src/evidently/dashboard/widgets/expandable_list_widget.py
@@ -29,229 +29,229 @@ class ExpandableListWidget(Widget):
             size=2,
             type="list",
             widgets=[BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "Header",
-                "description": "optional description",
-                "metricsValuesHeaders": ["reference", "current"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6]},
-                    {"label": "average", "values": [0.3, 0.6]},
-                    {"label": "something", "values": [0.3, 0.6]},
-                    {"label": "some string", "values": ["absc", "gar"]},
-                ],
-                "graph": {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout']
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "Header",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["reference", "current"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6]},
+                        {"label": "average", "values": [0.3, 0.6]},
+                        {"label": "something", "values": [0.3, 0.6]},
+                        {"label": "some string", "values": ["absc", "gar"]},
+                    ],
+                    "graph": {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout']
+                    },
+                    "details": {
+                        "parts": [{
+                            "title": "First Tab",
+                            "id": "first_tab"
+                        }, {
+                            "title": "Second Tab",
+                            "id": "second_tab"
+                        }],
+                        "insights": []
+                    }
                 },
-                "details": {
-                    "parts": [{
-                        "title": "First Tab",
-                        "id": "first_tab"
-                    }, {
-                        "title": "Second Tab",
-                        "id": "second_tab"
-                    }],
-                    "insights": []
-                }
-            },
-            additionalGraphs=[
-                AdditionalGraphInfo("first_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                }),
-                AdditionalGraphInfo("second_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                })
-            ]
-        ), BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "Without Graph",
-                "description": "optional description",
-                "metricsValuesHeaders": ["reference", "current"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6]},
-                    {"label": "average", "values": [0.3, 0.6]},
-                    {"label": "something", "values": [0.3, 0.6]},
-                    {"label": "some string", "values": ["absc", "gar"]},
-                ],
-                "details": {
-                    "parts": [{
-                        "title": "First Tab",
-                        "id": "first_tab"
-                    }, {
-                        "title": "Second Tab",
-                        "id": "second_tab"
-                    }],
-                    "insights": []
-                }
-            },
-            additionalGraphs=[
-                AdditionalGraphInfo("first_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                }),
-                AdditionalGraphInfo("second_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                })
-            ]
-        ), BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "Without details",
-                "description": "optional description",
-                "metricsValuesHeaders": ["reference", "current"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6]},
-                    {"label": "average", "values": [0.3, 0.6]},
-                    {"label": "something", "values": [0.3, 0.6]},
-                    {"label": "some string", "values": ["absc", "gar"]},
-                ],
-                "graph": {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout']
+                additionalGraphs=[
+                    AdditionalGraphInfo("first_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    }),
+                    AdditionalGraphInfo("second_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    })
+                ]
+            ), BaseWidgetInfo(
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "Without Graph",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["reference", "current"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6]},
+                        {"label": "average", "values": [0.3, 0.6]},
+                        {"label": "something", "values": [0.3, 0.6]},
+                        {"label": "some string", "values": ["absc", "gar"]},
+                    ],
+                    "details": {
+                        "parts": [{
+                            "title": "First Tab",
+                            "id": "first_tab"
+                        }, {
+                            "title": "Second Tab",
+                            "id": "second_tab"
+                        }],
+                        "insights": []
+                    }
                 },
-            },
-        ), BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "Without graph and details",
-                "description": "optional description",
-                "metricsValuesHeaders": ["reference", "current"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6]},
-                    {"label": "average", "values": [0.3, 0.6]},
-                    {"label": "something", "values": [0.3, 0.6]},
-                    {"label": "some string", "values": ["absc", "gar"]},
-                ],
-            },
-        ), BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "Header",
-                "description": "optional description",
-                "metricsValuesHeaders": ["reference", "current"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6]},
-                    {"label": "average", "values": [0.3, 0.6]},
-                    {"label": "something", "values": [0.3, 0.6]},
-                    {"label": "some string", "values": ["absc", "gar"]},
-                ],
-                "graph": {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout']
+                additionalGraphs=[
+                    AdditionalGraphInfo("first_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    }),
+                    AdditionalGraphInfo("second_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    })
+                ]
+            ), BaseWidgetInfo(
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "Without details",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["reference", "current"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6]},
+                        {"label": "average", "values": [0.3, 0.6]},
+                        {"label": "something", "values": [0.3, 0.6]},
+                        {"label": "some string", "values": ["absc", "gar"]},
+                    ],
+                    "graph": {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout']
+                    },
                 },
-                "details": {
-                    "parts": [{
-                        "title": "First Tab",
-                        "id": "first_tab"
-                    }, {
-                        "title": "Second Tab",
-                        "id": "second_tab"
-                    }],
-                    "insights": []
-                }
-            },
-            additionalGraphs=[
-                AdditionalGraphInfo("first_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                }),
-                AdditionalGraphInfo("second_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                })
-            ]
-        ), BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "More values",
-                "description": "optional description",
-                "metricsValuesHeaders": ["first", "second", "third"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6, 0.9]},
-                    {"label": "average", "values": [0.3, 0.6, 0.7]},
-                    {"label": "something", "values": [0.3, 0.6, 0.9]},
-                    {"label": "some string", "values": ["absc", "gar", "par"]},
-                ],
-                "graph": {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout']
+            ), BaseWidgetInfo(
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "Without graph and details",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["reference", "current"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6]},
+                        {"label": "average", "values": [0.3, 0.6]},
+                        {"label": "something", "values": [0.3, 0.6]},
+                        {"label": "some string", "values": ["absc", "gar"]},
+                    ],
                 },
-                "details": {
-                    "parts": [{
-                        "title": "First Tab",
-                        "id": "first_tab"
-                    }, {
-                        "title": "Second Tab",
-                        "id": "second_tab"
-                    }],
-                }
-            },
-            additionalGraphs=[
-                AdditionalGraphInfo("first_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                }),
-                AdditionalGraphInfo("second_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                })
-            ]
-        ), BaseWidgetInfo(
-            type="rich_data",
-            title="",
-            size=2,
-            params={
-                "header": "With insight",
-                "description": "optional description",
-                "metricsValuesHeaders": ["reference", "current"],
-                "metrics": [
-                    {"label": "f1", "values": [0.3, 0.6]},
-                    {"label": "average", "values": [0.3, 0.6]},
-                    {"label": "something", "values": [0.3, 0.6]},
-                    {"label": "some string", "values": ["absc", "gar"]},
-                ],
-                "graph": {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout']
+            ), BaseWidgetInfo(
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "Header",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["reference", "current"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6]},
+                        {"label": "average", "values": [0.3, 0.6]},
+                        {"label": "something", "values": [0.3, 0.6]},
+                        {"label": "some string", "values": ["absc", "gar"]},
+                    ],
+                    "graph": {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout']
+                    },
+                    "details": {
+                        "parts": [{
+                            "title": "First Tab",
+                            "id": "first_tab"
+                        }, {
+                            "title": "Second Tab",
+                            "id": "second_tab"
+                        }],
+                        "insights": []
+                    }
                 },
-                "details": {
-                    "parts": [{
-                        "title": "First Tab",
-                        "id": "first_tab"
-                    }, {
-                        "title": "Second Tab",
-                        "id": "second_tab"
-                    }],
-                    "insights": [Insight("Some notification", "warning", "additional text for insight")]
-                }
-            },
-            additionalGraphs=[
-                AdditionalGraphInfo("first_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                }),
-                AdditionalGraphInfo("second_tab", {
-                    "data": fig_dict['data'],
-                    "layout": fig_dict['layout'],
-                })
-            ]
-        )],
+                additionalGraphs=[
+                    AdditionalGraphInfo("first_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    }),
+                    AdditionalGraphInfo("second_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    })
+                ]
+            ), BaseWidgetInfo(
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "More values",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["first", "second", "third"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6, 0.9]},
+                        {"label": "average", "values": [0.3, 0.6, 0.7]},
+                        {"label": "something", "values": [0.3, 0.6, 0.9]},
+                        {"label": "some string", "values": ["absc", "gar", "par"]},
+                    ],
+                    "graph": {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout']
+                    },
+                    "details": {
+                        "parts": [{
+                            "title": "First Tab",
+                            "id": "first_tab"
+                        }, {
+                            "title": "Second Tab",
+                            "id": "second_tab"
+                        }],
+                    }
+                },
+                additionalGraphs=[
+                    AdditionalGraphInfo("first_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    }),
+                    AdditionalGraphInfo("second_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    })
+                ]
+            ), BaseWidgetInfo(
+                type="rich_data",
+                title="",
+                size=2,
+                params={
+                    "header": "With insight",
+                    "description": "optional description",
+                    "metricsValuesHeaders": ["reference", "current"],
+                    "metrics": [
+                        {"label": "f1", "values": [0.3, 0.6]},
+                        {"label": "average", "values": [0.3, 0.6]},
+                        {"label": "something", "values": [0.3, 0.6]},
+                        {"label": "some string", "values": ["absc", "gar"]},
+                    ],
+                    "graph": {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout']
+                    },
+                    "details": {
+                        "parts": [{
+                            "title": "First Tab",
+                            "id": "first_tab"
+                        }, {
+                            "title": "Second Tab",
+                            "id": "second_tab"
+                        }],
+                        "insights": [Insight("Some notification", "warning", "additional text for insight")]
+                    }
+                },
+                additionalGraphs=[
+                    AdditionalGraphInfo("first_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    }),
+                    AdditionalGraphInfo("second_tab", {
+                        "data": fig_dict['data'],
+                        "layout": fig_dict['layout'],
+                    })
+                ]
+            )],
             pageSize=2,
         )
