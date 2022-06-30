8341ab781b0d24a31d2b3d103871ecd6d5b70386
Vyacheslav Morov <v.morov@corp.mail.ru>
2022-06-30 23:20:27 +0300

    Rewrite classification performance metrics.

--- src/evidently/v2/metrics/classification_performance_metrics.py
+++ src/evidently/v2/metrics/classification_performance_metrics.py
@@ -1,11 +1,12 @@
 import dataclasses
-from typing import Optional
+from typing import Optional, Tuple
 
+import numpy as np
 import pandas as pd
+from numpy import dtype
 
-from evidently.analyzers import classification_performance_analyzer as cpa
-from evidently.analyzers.classification_performance_analyzer import ClassificationPerformanceAnalyzer, \
-    ClassificationPerformanceAnalyzerResults, classification_performance_metrics
+from evidently import ColumnMapping
+from evidently.analyzers.classification_performance_analyzer import classification_performance_metrics
 from evidently.analyzers import prob_classification_performance_analyzer as pcpa
 from evidently.options import QualityMetricsOptions
 from evidently.options import OptionsProvider
@@ -14,40 +15,60 @@ from evidently.v2.metrics.base_metric import Metric
 
 
 @dataclasses.dataclass
-class ClassificationPerformanceMetricsResults(ClassificationPerformanceAnalyzerResults):
-    dummy_metrics: Optional[cpa.ClassificationPerformanceMetrics] = None
+class ClassificationPerformanceMetricsResults:
+    reference_metrics: Optional[pcpa.ProbClassificationPerformanceMetrics] = None
+    current_metrics: Optional[pcpa.ProbClassificationPerformanceMetrics] = None
+    dummy_metrics: Optional[pcpa.ProbClassificationPerformanceMetrics] = None
 
 
 class ClassificationPerformanceMetrics(Metric[ClassificationPerformanceMetricsResults]):
     def __init__(self):
-        self.analyzer = ClassificationPerformanceAnalyzer()
+        pass
 
     def calculate(self, data: InputData, metrics: dict) -> ClassificationPerformanceMetricsResults:
         if data.current_data is None:
             raise ValueError("current dataset should be present")
 
-        if data.reference_data is None:
-            analyzer_results = self.analyzer.calculate(
-                reference_data=data.current_data,
-                current_data=None,
-                column_mapping=data.column_mapping
-            )
-            analyzer_results.current_metrics = analyzer_results.reference_metrics
-            analyzer_results.reference_metrics = None
-        else:
-            analyzer_results = self.analyzer.calculate(
-                reference_data=data.reference_data,
-                current_data=data.current_data,
-                column_mapping=data.column_mapping
-            )
-        results = ClassificationPerformanceMetricsResults(**analyzer_results.__dict__)
-        target_data = data.current_data[data.column_mapping.target]
+        results = ClassificationPerformanceMetricsResults()
+        current_data = _cleanup_data(data.current_data)
+        target_data = current_data[data.column_mapping.target]
+        prediction_data, pred_probas = get_prediction_data(current_data, data.column_mapping)
+        results.current_metrics = classification_performance_metrics(target_data,
+                                                                     prediction_data,
+                                                                     data.column_mapping.target_names)
+
+        if data.reference_data:
+            reference_data = _cleanup_data(data.reference_data)
+            ref_prediction_data, ref_pred_probas = get_prediction_data(reference_data, data.column_mapping)
+            results.reference_metrics = classification_performance_metrics(
+                reference_data[data.column_mapping.target],
+                ref_prediction_data,
+                data.column_mapping.target_names)
+
         dummy_preds = pd.Series([target_data.value_counts().argmax()] * len(target_data))
         results.dummy_metrics = classification_performance_metrics(target_data,
                                                                    dummy_preds,
                                                                    data.column_mapping.target_names)
         return results
 
+
+def _cleanup_data(data: pd.DataFrame) -> pd.DataFrame:
+    return data.replace([np.inf, -np.inf], np.nan).dropna(axis=0, how="any")
+
+
+def get_prediction_data(data: pd.DataFrame, mapping: ColumnMapping) -> Tuple[pd.Series, Optional[pd.DataFrame]]:
+    if isinstance(mapping.prediction, list):
+        # list of columns with prediction probas, should be same as target labels
+        return data[mapping.prediction].idxmax(axis=1), data[mapping.prediction]
+    if isinstance(mapping.prediction, str) and data[mapping.prediction].dtype == dtype('float64'):
+        predictions = data[mapping.prediction].apply(lambda x:
+                                                     mapping.target_names[1] if x > 0.5 else mapping.target_names[0])
+        prediction_probas = pd.DataFrame.from_dict({
+            mapping.target_names[1]: data[mapping.prediction],
+            mapping.target_names[0]: data[mapping.prediction].apply(lambda x: 1. - x)
+        })
+        return predictions, prediction_probas
+    return data[mapping.prediction], None
 
 ProbClassificationPerformanceMetricsResults = pcpa.ProbClassificationPerformanceMetrics
 
--- tests/v2/metrics/test_classification_performance_metrics.py
+++ tests/v2/metrics/test_classification_performance_metrics.py
@@ -1,10 +1,39 @@
+from typing import Dict, Optional
+
 import numpy as np
 import pandas as pd
+import pytest
 
 from evidently.pipeline.column_mapping import ColumnMapping
 from evidently.v2.metrics.base_metric import InputData
 from evidently.v2.metrics import ClassificationPerformanceMetrics
-from evidently.v2.metrics import ProbClassificationPerformanceMetrics
+from evidently.v2.metrics.classification_performance_metrics import get_prediction_data
+
+
+@pytest.mark.parametrize("data,mapping,expected_predictions,expected_probas", [
+    (
+            pd.DataFrame([dict(target='a', prediction='a')]),
+            ColumnMapping(prediction='prediction'),
+            pd.Series(['a']),
+            None,
+    ),
+    (
+            pd.DataFrame([dict(target='a', pos_proba=0.9)]),
+            ColumnMapping(prediction='pos_proba', target_names=['b', 'a']),
+            pd.Series(['a']),
+            pd.DataFrame([dict(a=0.9, b=0.1)]),
+    ),
+])
+def test_prediction_data(data: pd.DataFrame,
+                         mapping: ColumnMapping,
+                         expected_predictions: pd.Series,
+                         expected_probas: Optional[pd.DataFrame]):
+    predictions, predictions_probas = get_prediction_data(data, mapping)
+    assert predictions.equals(expected_predictions)
+    if predictions_probas is None:
+        assert expected_probas is None
+    else:
+        assert np.isclose(predictions_probas, expected_probas).all()
 
 
 def test_classification_performance_metrics() -> None:
@@ -27,30 +56,3 @@ def test_classification_performance_metrics() -> None:
     assert result.current_metrics.f1 == 1
     assert result.current_metrics.precision == 1
     assert result.current_metrics.recall == 1
-
-
-def test_prob_classification_performance_metrics() -> None:
-    test_dataset = pd.DataFrame(
-        {
-            "target": ["label_a", "label_a", "label_b"],
-            "label_a": [0.2, 0.1, 0.5],
-            "label_b": [0.8, 0.9, 0.5],
-        }
-    )
-    data_mapping = ColumnMapping(
-        target="target",
-        prediction=["label_a", "label_b"],
-    )
-    metric = ProbClassificationPerformanceMetrics()
-    result = metric.calculate(
-        data=InputData(current_data=test_dataset, reference_data=None, column_mapping=data_mapping),
-        metrics={}
-    )
-    assert result is not None
-    assert result.accuracy == 0
-    assert result.f1 == 0
-    assert result.precision == 0
-    assert result.recall == 0
-    assert result.roc_auc == 0
-    assert result.roc_aucs is None
-    assert result.log_loss > 1.5
