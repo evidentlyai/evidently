3117416571a884e786e95998205afa963a9c5b93
0lgaF <olgafilippova@MacBook-Air-Olga.local>
2022-07-06 14:07:55 +0300

    Bug fix

--- src/evidently/v2/metrics/data_quality_metrics.py
+++ src/evidently/v2/metrics/data_quality_metrics.py
@@ -352,13 +352,14 @@ class DataQualityCorrelationMetrics(Metric[DataQualityCorrelationMetricsResults]
         if data.reference_data is not None:
             reference_correlations = data.reference_data.corr(method=self.method)
             reference_correlations_for_plot = reference_correlations.copy()
+            np.fill_diagonal(reference_correlations.values, 0)
 
         if num_features is None:
             num_features = [i for i in current_correlations if i not in [target_name, prediction_name]]
 
         # we will get 1 for all column/column correlation in the diagonal, fill it with 0
         np.fill_diagonal(current_correlations.values, 0)
-        np.fill_diagonal(reference_correlations.values, 0)
+        
         target_prediction_correlation = current_correlations.loc[prediction_name, target_name]
 
         if reference_correlations is not None:
