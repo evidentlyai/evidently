b2e24cc22984b8708fefad6198c2f66d96f556e2
Vyacheslav Morov <Liraim@users.noreply.github.com>
2022-07-28 23:31:22 +0300

    Classification and regression presets (#285)

--- src/evidently/metrics/utils.py
+++ src/evidently/metrics/utils.py
@@ -44,25 +44,31 @@ def make_target_bins_for_reg_plots(
 ) -> pd.DataFrame:
     df_for_bins = pd.DataFrame({"data": "curr", target_column: curr[target_column], preds_column: curr[preds_column]})
     if ref is not None:
-        df_for_bins = df_for_bins.append(
+        df_for_bins = pd.concat([
+            df_for_bins,
             pd.DataFrame({"data": "ref", target_column: ref[target_column], preds_column: ref[preds_column]})
-        )
+        ])
     df_for_bins["target_binned"] = pd.cut(df_for_bins[target_column], min(df_for_bins[target_column].nunique(), 10))
     return df_for_bins
 
 
 def apply_func_to_binned_data(df_for_bins, func, target_column, preds_column, is_ref_data=False):
     result = {}
+
+    def _apply(x):
+        if x.shape[0] == 0:
+            return None
+        return func(x[target_column], x[preds_column])
     result["current"] = (
         df_for_bins[df_for_bins.data == "curr"]
         .groupby("target_binned")
-        .apply(lambda x: func(x[target_column], x[preds_column]))
+        .apply(_apply)
     )
 
     if is_ref_data:
         result["reference"] = (
             df_for_bins[df_for_bins.data == "ref"]
             .groupby("target_binned")
-            .apply(lambda x: func(x[target_column], x[preds_column]))
+            .apply(_apply)
         )
     return result
