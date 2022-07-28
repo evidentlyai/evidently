4ca83614851e503e86ceeaf645cb2da7128b6b82
Tapot <novakche@yandex.ru>
2022-07-28 16:58:03 +0300

    add unittests for logloss and rocaux tests

--- tests/tests/test_classification_performance_tests.py
+++ tests/tests/test_classification_performance_tests.py
@@ -2,6 +2,8 @@ import json
 
 import pandas as pd
 
+from pytest import approx
+
 from evidently.pipeline.column_mapping import ColumnMapping
 from evidently.tests import TestAccuracyScore
 from evidently.tests import TestPrecisionScore
@@ -159,6 +161,50 @@ def test_recall_score_test_render_json() -> None:
     }
 
 
+def test_log_loss_test() -> None:
+    test_dataset = pd.DataFrame(
+        {
+            "target": ["a", "a", "a", "b"],
+            "b": [0.2, 0.5, 0.3, 0.6],
+        }
+    )
+    column_mapping = ColumnMapping(prediction="b", pos_label="a")
+    suite = TestSuite(tests=[TestLogLoss(gte=0.8)])
+    suite.run(current_data=test_dataset, reference_data=None, column_mapping=column_mapping)
+    assert not suite
+
+    suite = TestSuite(tests=[TestLogLoss(lt=0.8)])
+    suite.run(current_data=test_dataset, reference_data=None, column_mapping=column_mapping)
+    assert suite
+
+
+def test_log_loss_test_json_render() -> None:
+    test_dataset = pd.DataFrame(
+        {
+            "target": ["a", "a", "a", "b"],
+            "b": [0.2, 0.5, 0.3, 0.6],
+        }
+    )
+    column_mapping = ColumnMapping(prediction="b", pos_label="a")
+    suite = TestSuite(tests=[TestLogLoss()])
+    suite.run(current_data=test_dataset, reference_data=test_dataset, column_mapping=column_mapping)
+    assert suite
+
+    result_from_json = json.loads(suite.json())
+    assert result_from_json["summary"]["all_passed"] is True
+    test_info = result_from_json["tests"][0]
+    assert test_info == {
+        "description": " Logarithmic Loss is 0.446. Test Threshold is eq=0.446 ± " "0.0892",
+        "group": "classification",
+        "name": "Logarithmic Loss",
+        "parameters": {
+            "condition": {"eq": {"absolute": 1e-12, "relative": 0.2, "value": approx(0.446, abs=0.0001)}},
+            "log_loss": approx(0.446, abs=0.0001),
+        },
+        "status": "SUCCESS",
+    }
+
+
 def test_log_loss_test_cannot_calculate_log_loss() -> None:
     test_dataset = pd.DataFrame(
         {
@@ -176,7 +222,50 @@ def test_log_loss_test_cannot_calculate_log_loss() -> None:
     assert test_info["status"] == "ERROR"
 
 
-def test_rec_auc_test_cannot_calculate_roc_auc() -> None:
+def test_roc_auc_test() -> None:
+    test_dataset = pd.DataFrame(
+        {
+            "target": ["a", "a", "a", "b"],
+            "a": [0.8, 0.5, 0.7, 0.3],
+            "b": [0.2, 0.5, 0.3, 0.6],
+        }
+    )
+    column_mapping = ColumnMapping(prediction=["a", "b"], pos_label="a")
+    suite = TestSuite(tests=[TestRocAuc(gte=0.8)])
+    suite.run(current_data=test_dataset, reference_data=None, column_mapping=column_mapping)
+    assert suite
+
+    suite = TestSuite(tests=[TestRocAuc(lt=0.8)])
+    suite.run(current_data=test_dataset, reference_data=None, column_mapping=column_mapping)
+    assert not suite
+
+
+def test_roc_auc_test_json_render() -> None:
+    test_dataset = pd.DataFrame(
+        {
+            "target": ["t", "f", "f", "t"],
+            "f": [0.8, 0.5, 0.7, 0.3],
+            "t": [0.2, 0.5, 0.3, 0.6],
+        }
+    )
+    column_mapping = ColumnMapping(prediction=["f", "t"], pos_label="t")
+    suite = TestSuite(tests=[TestRocAuc(lt=0.8)])
+    suite.run(current_data=test_dataset, reference_data=None, column_mapping=column_mapping)
+    assert suite
+
+    result_from_json = json.loads(suite.json())
+    assert result_from_json["summary"]["all_passed"] is True
+    test_info = result_from_json["tests"][0]
+    assert test_info == {
+        "description": "ROC AUC Score is 0.5. Test Threshold is lt=0.8",
+        "group": "classification",
+        "name": "ROC AUC Score",
+        "parameters": {"condition": {"lt": 0.8}, "roc_auc": 0.5},
+        "status": "SUCCESS",
+    }
+
+
+def test_roc_auc_test_cannot_calculate_roc_auc() -> None:
     test_dataset = pd.DataFrame(
         {
             "target": ["a", "a", "a", "b", "b", "b", "c", "c", "c", "c"],
