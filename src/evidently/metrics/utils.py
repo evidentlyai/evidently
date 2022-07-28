d2b71d6a2dabeda790509ca6779e97f2f17164ee
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2022-07-28 23:11:45 +0300

    Replace .append to pd.concat.

--- src/evidently/metrics/utils.py
+++ src/evidently/metrics/utils.py
@@ -44,9 +44,10 @@ def make_target_bins_for_reg_plots(
 ) -> pd.DataFrame:
     df_for_bins = pd.DataFrame({"data": "curr", target_column: curr[target_column], preds_column: curr[preds_column]})
     if ref is not None:
-        df_for_bins = df_for_bins.append(
+        df_for_bins = pd.concat([
+            df_for_bins,
             pd.DataFrame({"data": "ref", target_column: ref[target_column], preds_column: ref[preds_column]})
-        )
+        ])
     df_for_bins["target_binned"] = pd.cut(df_for_bins[target_column], min(df_for_bins[target_column].nunique(), 10))
     return df_for_bins
 
