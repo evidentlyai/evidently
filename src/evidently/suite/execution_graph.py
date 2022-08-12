256ed092a39fa0c9ad8ae023a0300abffa4196de
Vyacheslav Morov <Liraim@users.noreply.github.com>
2022-08-12 21:06:54 +0300

    Implement metrics deduplication based on parameters. (#294)

--- src/evidently/suite/execution_graph.py
+++ src/evidently/suite/execution_graph.py
@@ -1,5 +1,7 @@
 import abc
-from typing import List
+import functools
+import logging
+from typing import List, Tuple, Type, Dict
 
 from evidently.metrics.base_metric import Metric
 from evidently.tests.base_test import Test
@@ -7,7 +9,7 @@ from evidently.tests.base_test import Test
 
 class ExecutionGraph:
     @abc.abstractmethod
-    def get_metric_execution_iterator(self) -> List[Metric]:
+    def get_metric_execution_iterator(self) -> List[Tuple[Metric, Metric]]:
         raise NotImplementedError()
 
     @abc.abstractmethod
@@ -28,8 +30,28 @@ class SimpleExecutionGraph(ExecutionGraph):
         self.metrics = metrics
         self.tests = tests
 
-    def get_metric_execution_iterator(self) -> List[Metric]:
-        return self.metrics
+    def get_metric_execution_iterator(self) -> List[Tuple[Metric, Metric]]:
+        aggregated: Dict[Type[Metric], List[Metric]] = functools.reduce(_aggregate_metrics, self.metrics, {})
+        metric_to_calculations = {}
+        for metric_type, metrics in aggregated.items():
+            metrics_by_parameters: Dict[tuple, List[Metric]] = functools.reduce(_aggregate_by_parameters, metrics, {})
+            calculations = []
+            for params, params_metrics in metrics_by_parameters.items():
+                logging.debug(f"{metric_type.__name__} with params ({params}): {len(params_metrics)} combined")
+                base_metric = params_metrics[0]
+                calculations += [base_metric] * len(params_metrics)
+            metric_to_calculations.update(dict(zip(metrics, calculations)))
+        return [(metric, metric_to_calculations[metric]) for metric in self.metrics]
 
     def get_test_execution_iterator(self) -> List[Test]:
         return self.tests
+
+
+def _aggregate_metrics(agg, item):
+    agg[type(item)] = agg.get(type(item), []) + [item]
+    return agg
+
+
+def _aggregate_by_parameters(agg: dict, metric: Metric) -> dict:
+    agg[metric.get_parameters()] = agg.get(metric.get_parameters(), []) + [metric]
+    return agg
