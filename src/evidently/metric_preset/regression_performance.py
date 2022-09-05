7ace4a5ade7e52b9bcc9860208349640dd04cc2e
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2022-09-06 01:02:49 +0300

    Add plots to regression performance metric preset.

--- src/evidently/metric_preset/regression_performance.py
+++ src/evidently/metric_preset/regression_performance.py
@@ -2,8 +2,12 @@ from evidently.utils.data_operations import DatasetColumns
 from evidently.metrics.base_metric import InputData
 from evidently.metric_preset.metric_preset import MetricPreset
 from evidently.metrics.regression_performance_metrics import RegressionPerformanceMetrics
+from evidently.metrics.predicted_vs_actual import PredictedVsActualMetric
 
 
 class RegressionPerformance(MetricPreset):
     def generate_metrics(self, data: InputData, columns: DatasetColumns):
-        return [RegressionPerformanceMetrics()]
+        return [
+            RegressionPerformanceMetrics(),
+            PredictedVsActualMetric(),
+        ]
