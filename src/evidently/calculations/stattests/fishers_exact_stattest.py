e6c8e72bc37f639f56fc826793565cfacaf19059
SangamSwadiK <35230623+SangamSwadiK@users.noreply.github.com>
2022-10-10 17:44:00 +0530

    initial commit

---
+++ src/evidently/calculations/stattests/fishers_exact_stattest.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from scipy.stats import fisher_exact
+
+from evidently.calculations.stattests.registry import StatTest
+from evidently.calculations.stattests.registry import register_stattest
+
+
+def _fisher_exact():
+    pass
+
+
+# fisher_exact_test = StatTest()
+
+# register_stattest()
