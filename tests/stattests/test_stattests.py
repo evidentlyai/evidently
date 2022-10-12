1c3d9b8b308b7ae7ba9e918ad1a9ed4a6d93a583
AntonisCSt <a.x-stellas@hotmail.com>
2022-10-12 11:42:36 +0300

    isort fix

--- tests/stattests/test_stattests.py
+++ tests/stattests/test_stattests.py
@@ -5,9 +5,9 @@ from pytest import approx
 
 from evidently.calculations.stattests import z_stat_test
 from evidently.calculations.stattests.anderson_darling_stattest import anderson_darling_test
-from evidently.calculations.stattests.mann_whitney_urank_stattest import mann_whitney_u_stat_test
 from evidently.calculations.stattests.chisquare_stattest import chi_stat_test
 from evidently.calculations.stattests.cramer_von_mises_stattest import cramer_von_mises
+from evidently.calculations.stattests.mann_whitney_urank_stattest import mann_whitney_u_stat_test
 
 
 def test_freq_obs_eq_freq_exp() -> None:
@@ -73,4 +73,3 @@ def test_mann_whitney() -> None:
     reference = pd.Series([1, 2, 3, 4, 5, 6]).repeat([16, 18, 16, 14, 12, 12])
     current = pd.Series([1, 2, 3, 4, 5, 6]).repeat([16, 16, 16, 16, 16, 8])
     assert mann_whitney_u_stat_test.func(reference, current, "num", 0.05) == (approx(0.481, abs=1e-2), False)
-
