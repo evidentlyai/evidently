9f6f358741d7e53299f3fc87c7d2c5c2551f782d
AntonisCSt <a.x-stellas@hotmail.com>
2022-10-13 21:22:26 +0300

    added test on init

--- src/evidently/calculations/stattests/__init__.py
+++ src/evidently/calculations/stattests/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 from .anderson_darling_stattest import anderson_darling_test
+from .epps_singleton_stattest import epps_singleton_test
 from .chisquare_stattest import chi_stat_test
 from .cramer_von_mises_stattest import cramer_von_mises
 from .jensenshannon import jensenshannon_stat_test
