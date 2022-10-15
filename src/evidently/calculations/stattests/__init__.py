c2af2a0fe846f885487155037499b368aee36009
SangamSwadiK <35230623+SangamSwadiK@users.noreply.github.com>
2022-10-15 12:49:49 +0530

    update examples

--- src/evidently/calculations/stattests/__init__.py
+++ src/evidently/calculations/stattests/__init__.py
@@ -3,6 +3,7 @@
 from .anderson_darling_stattest import anderson_darling_test
 from .chisquare_stattest import chi_stat_test
 from .cramer_von_mises_stattest import cramer_von_mises
+from .g_stattest import g_test
 from .jensenshannon import jensenshannon_stat_test
 from .kl_div import kl_div_stat_test
 from .ks_stattest import ks_stat_test
