fd3e30de5473c213c60b5d23525b58b32d60549b
SangamSwadiK <35230623+SangamSwadiK@users.noreply.github.com>
2022-10-16 19:51:06 +0530

    fix lint,sort

--- src/evidently/calculations/stattests/__init__.py
+++ src/evidently/calculations/stattests/__init__.py
@@ -3,6 +3,7 @@
 from .anderson_darling_stattest import anderson_darling_test
 from .chisquare_stattest import chi_stat_test
 from .cramer_von_mises_stattest import cramer_von_mises
+from .fisher_exact_stattest import fisher_exact_test
 from .jensenshannon import jensenshannon_stat_test
 from .kl_div import kl_div_stat_test
 from .ks_stattest import ks_stat_test
@@ -14,4 +15,3 @@ from .registry import get_stattest
 from .registry import register_stattest
 from .wasserstein_distance_norm import wasserstein_stat_test
 from .z_stattest import z_stat_test
-from .fisher_stattest import fisher_exact_test
