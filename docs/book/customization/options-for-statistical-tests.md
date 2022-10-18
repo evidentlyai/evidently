9c36f30da786383ac32d1bfad45ba1d82e9da8ef
SangamSwadik <35230623+SangamSwadiK@users.noreply.github.com>
2022-10-18 15:23:38 +0530

    update G test doc and examples (#390)
    * update G test doc
    
    * update examples
    
    * add missing docstring

--- docs/book/customization/options-for-statistical-tests.md
+++ docs/book/customization/options-for-statistical-tests.md
@@ -168,6 +168,10 @@ example_stat_test = StatTest(
   - only for numerical features
   - returns `p-value`
   - drift detected when `p_value < threshold`
+- `g-test` - G-test
+  - only for categorical features
+  - returns `p-value`
+  - drift detected when `p_value < threshold`
 - `hellinger` - Hellinger Distance (normed)
   - for numerical and categorical features
   - returns `distance`
--- src/evidently/calculations/stattests/g_stattest.py
+++ src/evidently/calculations/stattests/g_stattest.py
@@ -16,6 +16,16 @@ def _g_stat_test(
     feature_type: str,
     threshold: float,
 ) -> Tuple[float, bool]:
+    """Compute the G test between two arrays
+    Args:
+        reference_data: reference data
+        current_data: current data
+        feature_type: feature type
+        threshold: all values above this threshold means data drift
+    Returns:
+        p_value: calculated p_value value
+        test_result: whether the drift is detected
+    """
     keys = get_unique_not_nan_values_list_from_series(current_data=current_data, reference_data=reference_data)
     ref_feature_dict = {**dict.fromkeys(keys, 0), **dict(reference_data.value_counts())}
     current_feature_dict = {**dict.fromkeys(keys, 0), **dict(current_data.value_counts())}
