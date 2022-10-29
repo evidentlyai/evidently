4dda2578933dbb57517b9af767dc91ce0c0589cb
Tapot <novakche@yandex.ru>
2022-10-29 16:23:21 +0300

    remove DataQuality preset

--- src/evidently/metric_preset/__init__.py
+++ src/evidently/metric_preset/__init__.py
@@ -1,7 +1,6 @@
 from .classification_performance import ClassificationPerformance
 from .classification_performance import ClassificationPreset
 from .data_drift import DataDriftPreset
-from .data_quality import DataQuality
 from .data_quality import DataQualityPreset
 from .regression_performance import RegressionPerformance
 from .regression_performance import RegressionPreset
