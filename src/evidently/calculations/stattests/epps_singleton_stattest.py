31ed6c91989a9f91c6537f94ebc2d90eebf67b80
AntonisCSt <a.x-stellas@hotmail.com>
2022-10-30 19:25:46 +0200

    added raised error in test when iqr is zero

--- src/evidently/calculations/stattests/epps_singleton_stattest.py
+++ src/evidently/calculations/stattests/epps_singleton_stattest.py
@@ -1,7 +1,9 @@
 from typing import Tuple
 
+import numpy as np
 import pandas as pd
 from scipy.stats import epps_singleton_2samp
+from scipy.stats import iqr
 
 from evidently.calculations.stattests.registry import StatTest
 from evidently.calculations.stattests.registry import register_stattest
@@ -22,6 +24,12 @@ def _epps_singleton(
         p_value: p-value based on the asymptotic chi2-distribution.
         test_result: whether the drift is detected
     """
+    # raised error if iqr is zero
+    iqr_value = iqr(np.hstack((reference_data, current_data)))
+    if iqr_value == 0:
+        raise ValueError(
+            f'{"Epps-Singleton testsinterquartile range (IQR) is zero. IQR of 0 means there is no variability in the middle 50% of your data, but the center of the distribution can be anywhere. Expected: your data sould have higher variability/variance."}'
+        )
     p_value = epps_singleton_2samp(reference_data, current_data)[1]
     return p_value, p_value < threshold
 
