5546be90371188d7134214de6bf4e557e1396c07
Natalia Tarasova <natalia.tarasova@mechanica.ai>
2022-12-12 18:05:00 +0000

    add text features - non-letter character share, OOV words percentage, trigger words presense detection

---
+++ tests/features/test_OOV_words_percentage_feature.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from evidently import ColumnMapping
+from evidently.features.OOV_words_percentage_feature import OOVWordsPercentage
+from evidently.utils.data_preprocessing import create_data_definition
+
+
+def test_oov_words_percentage():
+    feature_generator = OOVWordsPercentage("column_1", ignore_words=("foobar",))
+    data = pd.DataFrame(
+        dict(
+            column_1=["Who ate apples? Go iaehb!", "Who ate apples? Go foobar! ", "the"]
+        )
+    )
+    result = feature_generator.generate_feature(
+        data=data,
+        data_definition=create_data_definition(None, data, ColumnMapping()),
+    )
+    assert result.equals(pd.DataFrame(dict(column_1=[20.0, 0, 0])))
