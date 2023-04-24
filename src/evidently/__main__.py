4c623e91eed94a6580b4bfb02b46f761657322e8
Mikhail Sveshnikov <mike0sv@gmail.com>
2023-04-24 19:02:09 +0300

    remove yaml requirement (#582)

--- src/evidently/__main__.py
+++ src/evidently/__main__.py
@@ -1,21 +1,11 @@
 import argparse
-import json
 import logging
-import os
 import sys
 from dataclasses import dataclass
 from typing import Any
 from typing import Dict
 
-import yaml
-
-from evidently._config import TELEMETRY_ADDRESS
-from evidently._config import TELEMETRY_ENABLED
-from evidently.pipeline.column_mapping import ColumnMapping
 from evidently.runner.loader import SamplingOptions
-from evidently.runner.runner import DataOptions
-from evidently.runner.runner import parse_options
-from evidently.telemetry import TelemetrySender
 
 
 @dataclass
@@ -53,17 +43,6 @@ def __get_not_none(src: Dict, key, default):
     return default if src.get(key, None) is None else src.get(key)
 
 
-def __load_config_file(config_file: str):
-    with open(config_file, encoding="utf-8") as f_config:
-        if config_file.endswith(".yaml") or config_file.endswith(".yml"):
-            opts_data = yaml.load(f_config, Loader=yaml.SafeLoader)
-        elif config_file.endswith(".json"):
-            opts_data = json.load(f_config)
-        else:
-            raise Exception(f"config .{config_file.split('.')[-1]} not supported")
-    return opts_data
-
-
 def help_handler(**_kv):
     parser.print_help()
     sys.exit(1)
@@ -87,7 +66,6 @@ parsers = parser.add_subparsers()
 parser.set_defaults(handler=help_handler)
 calculate_parser = parsers.add_parser("calculate")
 calc_subparsers = calculate_parser.add_subparsers()
-
 
 parsed = parser.parse_args(sys.argv[1:])
 
