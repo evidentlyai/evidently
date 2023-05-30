3c75f1fedf1862cef740cfec55eaea69e98dea15
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2023-05-30 11:10:10 +0200

    Fix mypy.

--- src/evidently/report/report.py
+++ src/evidently/report/report.py
@@ -33,7 +33,7 @@ class Report(Display):
     _first_level_metrics: List[Union[Metric]]
     metrics: List[Union[Metric, MetricPreset, BaseGenerator]]
 
-    def __init__(self, metrics: List[Union[Metric, MetricPreset, BaseGenerator]], options: AnyOptions = None) -> object:
+    def __init__(self, metrics: List[Union[Metric, MetricPreset, BaseGenerator]], options: AnyOptions = None):
         super().__init__(options)
         # just save all metrics and metric presets
         self.metrics = metrics
