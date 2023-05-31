bb9436e4362f52db010feb483e0aeaf8afcff979
mike0sv <mike0sv@gmail.com>
2023-05-31 15:39:47 +0300

    separate metric result type for scatter from dataframe

--- src/evidently/metric_results.py
+++ src/evidently/metric_results.py
@@ -17,9 +17,14 @@ from evidently.core import IncludeTags
 from evidently.pipeline.column_mapping import TargetNames
 
 Label = Union[int, str]
-ScatterData = Union[pd.Series, List[float], pd.Index, pd.DataFrame]
+
+
+ScatterData = Union[pd.Series, List[float], pd.Index]
 ContourData = Tuple[np.ndarray, List[float], List[float]]
 ColumnScatter = Dict[Label, ScatterData]
+
+ScatterAggData = Union[pd.DataFrame]
+ColumnAggScatter = Dict[Label, ScatterAggData]
 
 
 class Distribution(MetricResult):
@@ -158,6 +163,19 @@ def column_scatter_from_df(df: Optional[pd.DataFrame], with_index: bool) -> Opti
     if with_index:
         data["index"] = df.index
     return data
+
+
+class ScatterAggField(MetricResult):
+    class Config:
+        smart_union = True
+        dict_include = False
+        pd_include = False
+
+        tags = {IncludeTags.Render}
+
+    scatter: ColumnAggScatter
+    x_name: str
+    plot_shape: Dict[str, float]
 
 
 class ScatterField(MetricResult):
