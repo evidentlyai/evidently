2c52d03f93a31ff74251b433671dcc82d3e6f723
Emeli Dral <emeli.dral@gmail.com>
2023-06-30 15:30:08 +0100

    Updated example dashboard for ui service

--- src/evidently/ui/generate_workspace.py
+++ src/evidently/ui/generate_workspace.py
@@ -4,13 +4,11 @@ import numpy as np
 from sklearn import datasets
 
 from evidently.metric_preset import DataDriftPreset
-from evidently.metrics import ColumnDriftMetric
-from evidently.metrics import ColumnMissingValuesMetric
-from evidently.metrics import DatasetCorrelationsMetric
+from evidently.metrics import DatasetDriftMetric, DatasetMissingValuesMetric, ColumnDriftMetric, ColumnMissingValuesMetric, ColumnQuantileMetric
 from evidently.report import Report
 from evidently.test_suite import TestSuite
-from evidently.tests import TestNumberOfDriftedColumns
-from evidently.tests import TestShareOfDriftedColumns
+from evidently.test_preset import DataDriftTestPreset
+
 from evidently.ui.dashboards import CounterAgg
 from evidently.ui.dashboards import DashboardPanelCounter
 from evidently.ui.dashboards import DashboardPanelPlot
@@ -19,115 +17,139 @@ from evidently.ui.dashboards import PlotType
 from evidently.ui.dashboards import ReportFilter
 from evidently.ui.workspace import Workspace
 
+
 adult_data = datasets.fetch_openml(name="adult", version=2, as_frame="auto")
 adult = adult_data.frame
 
 adult_ref = adult[~adult.education.isin(["Some-college", "HS-grad", "Bachelors"])]
 adult_cur = adult[adult.education.isin(["Some-college", "HS-grad", "Bachelors"])]
 
-adult_cur.iloc[:2000, 3:5] = np.nan
-
 WORKSPACE = "workspace"
 
-
-def create_report(metric, date: datetime.datetime, tag: str):
+def create_report(i: int, tags=[]):
     data_drift_report = Report(
-        metrics=[metric, ColumnDriftMetric(column_name="age"), ColumnMissingValuesMetric(column_name="education")],
-        options={"render": {"raw_data": True}},
-        metadata={"type": metric.__class__.__name__},
-        tags=[tag],
-        timestamp=date,
-        dataset_id="adult",
-    ).set_batch_size("1")
+        metrics=[
+            DatasetDriftMetric(),
+            DatasetMissingValuesMetric(),
+            ColumnDriftMetric(column_name="age"),
+            ColumnQuantileMetric(column_name="age", quantile=0.5),
+            ColumnDriftMetric(column_name="education-num"),
+            ColumnQuantileMetric(column_name="education-num", quantile=0.5),
+            ], 
+        metadata={"type": "data_quality"}, 
+        tags=tags, 
+        timestamp=datetime.datetime.now() + datetime.timedelta(days=i), 
+    )
 
-    data_drift_report.run(reference_data=adult_ref, current_data=adult_cur)
+    data_drift_report.set_batch_size("daily")
+    data_drift_report.set_dataset_id("adult")
+
+    data_drift_report.run(reference_data=adult_ref, current_data=adult_cur.iloc[100*i:100*(i+1), :])
     return data_drift_report
 
 
-def create_test_suite():
-    data_drift_dataset_tests = TestSuite(
+def create_test_suite(i: int, tags=[]):
+    data_drift_test_suite = TestSuite(
         tests=[
-            TestNumberOfDriftedColumns(),
-            TestShareOfDriftedColumns(),
-        ]
+            DataDriftTestPreset()
+            ], 
+        timestamp=datetime.datetime.now() + datetime.timedelta(days=i), 
     )
 
-    data_drift_dataset_tests.run(reference_data=adult_ref, current_data=adult_cur)
-    return data_drift_dataset_tests
+    data_drift_test_suite.run(reference_data=adult_ref, current_data=adult_cur.iloc[100*i:100*(i+1), :])
+    return data_drift_test_suite
 
 
 def create_project(workspace: Workspace):
-    project = workspace.create_project("project1")
+    project = workspace.create_project("Example Project")
     project.add_panel(
         DashboardPanelCounter(
-            filter=ReportFilter(metadata_values={}, tag_values=["drift"]),
+            filter=ReportFilter(metadata_values={}, tag_values=[]),
             agg=CounterAgg.NONE,
-            title="My beatifule panels",
-        )
-    )
-    project.add_panel(
-        DashboardPanelCounter(
-            filter=ReportFilter(metadata_values={"type": "DataDriftPreset"}, tag_values=["drift"]),
-            agg=CounterAgg.SUM,
-            value=PanelValue(metric_id="DatasetDriftMetric", field_path="number_of_columns"),
-            title="sum of number_of_columns",
+            title="Census Income Dataset (Adult)",
         )
     )
     project.add_panel(
         DashboardPanelPlot(
-            title="sample_panel",
-            filter=ReportFilter(metadata_values={"type": "DataDriftPreset"}, tag_values=["drift"]),
+            title="Dataset Quality",
+            filter=ReportFilter(metadata_values={"type": "data_quality"}, tag_values=[ ]),
             values=[
-                PanelValue(
-                    metric_id="DatasetDriftMetric",
-                    metric_args={"num_stattest": "ks"},
-                    field_path="share_of_drifted_columns",
-                    legend="Share",
-                ),
-                PanelValue(
-                    metric_id="DatasetDriftMetric",
-                    metric_args={},
-                    field_path="number_of_drifted_columns",
-                    legend="Count",
-                ),
+                PanelValue(metric_id="DatasetDriftMetric", field_path="share_of_drifted_columns", legend="Drift Share"),
+                PanelValue(metric_id="DatasetMissingValuesMetric", field_path="current.share_of_missing_values", legend="Missing Values Share"),
             ],
             plot_type=PlotType.LINE,
         )
     )
+    project.add_panel(
+        DashboardPanelPlot(
+            title="Age: Wasserstein drift distance",
+            filter=ReportFilter(metadata_values={"type": "data_quality"}, tag_values=[]),
+            values=[
+                PanelValue(metric_id="ColumnDriftMetric",
+                           metric_args={"column_name.name": "age"},
+                           field_path="drift_score", 
+                           legend="Drift Score"),
+            ],
+            plot_type=PlotType.LINE,
+            size=1
+        )
+    )
+    project.add_panel(
+        DashboardPanelPlot(
+            title="Age: quantile=0.5",
+            filter=ReportFilter(metadata_values={"type": "data_quality"}, tag_values=[]),
+            values=[
+                PanelValue(metric_id="ColumnQuantileMetric", 
+                           metric_args={"column_name.name": "age", "quantile":0.5},
+                           field_path="current.value", 
+                           legend="Quantile"),
+            ],
+            plot_type=PlotType.LINE,
+            size=1
+        )
+    ) 
+    project.add_panel(
+        DashboardPanelPlot(
+            title="Education-num: Wasserstein drift distance",
+            filter=ReportFilter(metadata_values={"type": "data_quality"}, tag_values=[]),
+            values=[
+                PanelValue(metric_id="ColumnDriftMetric",
+                           metric_args={"column_name.name": "education-num"},
+                           field_path="drift_score", 
+                           legend="Drift Score"),
+            ],
+            plot_type=PlotType.LINE,
+            size=1
+        )
+    )
+    project.add_panel(
+        DashboardPanelPlot(
+            title="Education-num: quantile=0.5",
+            filter=ReportFilter(metadata_values={"type": "data_quality"}, tag_values=[]),
+            values=[
+                PanelValue(metric_id="ColumnQuantileMetric", 
+                           metric_args={"column_name.name": "education-num", "quantile":0.5},
+                           field_path="current.value", 
+                           legend="Quantile"),
+            ],
+            plot_type=PlotType.LINE,
+            size=1
+        )
+    )     
     return project
 
 
 def main(workspace: str):
-    ws = Workspace.create(workspace)
+    ws = Workspace.create("workspace")
     project = create_project(ws)
     project.save()
 
-    for d in range(1, 3):
-        ts = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days=d), datetime.time())
-        report = create_report(
-            DataDriftPreset(
-                num_stattest="ks", cat_stattest="psi", num_stattest_threshold=0.2, cat_stattest_threshold=0.2
-            ),
-            ts,
-            "drift",
-        )
+    for i in range(0, 19):
+        report = create_report(i=i)
         ws.add_report(project.id, report)
-        report = create_report(
-            DataDriftPreset(
-                num_stattest="cramer_von_mises",
-                cat_stattest="psi",
-                num_stattest_threshold=0.5,
-                cat_stattest_threshold=0.2,
-            ),
-            ts,
-            "drift",
-        )
-        ws.add_report(project.id, report)
-        corr_report = create_report(DatasetCorrelationsMetric(), ts, "correlation")
-        ws.add_report(project.id, corr_report)
-
-    test_suite = create_test_suite()
-    ws.add_test_suite(project.id, test_suite)
+        
+        test_suite = create_test_suite(i=i)
+        ws.add_report(project.id, test_suite)
 
 
 if __name__ == "__main__":
