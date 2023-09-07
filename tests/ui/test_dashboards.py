94fa96198f13a4bf13871e557dbb5ceab4da5123
Mikhail Sveshnikov <mike0sv@gmail.com>
2023-09-07 18:59:27 +0300

    Fix metric args plot values (#750)
    * fix metric_args plot values
    
    * fix metric_args plot values

--- tests/ui/test_dashboards.py
+++ tests/ui/test_dashboards.py
@@ -1,8 +1,12 @@
+import json
 from typing import Dict
 
 import pytest
+from pydantic import parse_obj_as
 
 from evidently.base_metric import MetricResult
+from evidently.descriptors import OOV
+from evidently.ui.dashboards import PanelValue
 from evidently.ui.dashboards import getattr_nested
 
 
@@ -21,3 +25,12 @@ class B(MetricResult):
 )
 def test_getattr_nested(obj, path: str, value):
     assert getattr_nested(obj, path.split(".")) == value
+
+
+def test_panel_value_metric_args_ser():
+    pv = PanelValue(field_path="", metric_args={"col": OOV(display_name="OOV").for_column("Review_Text")})
+
+    pl = json.dumps(pv.dict())
+    pv2 = parse_obj_as(PanelValue, json.loads(pl))
+
+    assert pv2 == pv
