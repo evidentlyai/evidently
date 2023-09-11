12442df17fc1a34ba0d8927bb7363beefc2321eb
0lgaF <olgafilippova@MacBook-Air-Olga.local>
2023-09-11 16:32:51 +0300

    Linters

--- src/evidently/ui/demo_project_text.py
+++ src/evidently/ui/demo_project_text.py
@@ -27,29 +27,34 @@ warnings.simplefilter("ignore")
 WORKSPACE = "workspace"
 DEMO_PROJECT_NAME = "Demo project - Reviews"
 
+
 def create_data():
-    reviews_data = datasets.fetch_openml(name='Womens-E-Commerce-Clothing-Reviews', version=2, as_frame='auto')
+    reviews_data = datasets.fetch_openml(name="Womens-E-Commerce-Clothing-Reviews", version=2, as_frame="auto")
     reviews = reviews_data.frame
     for name, rs in (("TheOtherStore", 0), ("AMajorCompetitor", 42), ("AwesomeShop", 100)):
         np.random.seed(rs)
         random_index = np.random.choice(reviews.index, 300, replace=False)
-        reviews.loc[random_index, 'Review_Text'] = reviews.loc[random_index, 'Review_Text'] + f' mention competitor {name}'
+        reviews.loc[random_index, "Review_Text"] = (
+            reviews.loc[random_index, "Review_Text"] + f" mention competitor {name}"
+        )
 
     np.random.seed(13)
     random_index = np.random.choice(reviews.index, 1000, replace=False)
-    reviews.loc[random_index, 'Review_Text'] = reviews.loc[random_index, 'Review_Text'] + ' mention www.someurl.someurl '
-    reviews['prediction'] = reviews['Rating']
+    reviews.loc[random_index, "Review_Text"] = (
+        reviews.loc[random_index, "Review_Text"] + " mention www.someurl.someurl "
+    )
+    reviews["prediction"] = reviews["Rating"]
     np.random.seed(0)
     random_index = np.random.choice(reviews.index, 2000, replace=False)
-    reviews.loc[random_index, 'prediction'] = 1
+    reviews.loc[random_index, "prediction"] = 1
     reference = reviews.sample(n=5000, replace=True, ignore_index=True, random_state=42)
     current = reviews
     column_mapping = ColumnMapping(
-        target='Rating',
-        prediction='prediction',
-        numerical_features=['Age', 'Positive_Feedback_Count'],
-        categorical_features=['Division_Name', 'Department_Name', 'Class_Name'],
-        text_features=['Review_Text', 'Title']
+        target="Rating",
+        prediction="prediction",
+        numerical_features=["Age", "Positive_Feedback_Count"],
+        categorical_features=["Division_Name", "Department_Name", "Class_Name"],
+        text_features=["Review_Text", "Title"],
     )
     return current, reference, column_mapping
 
@@ -60,41 +65,45 @@ def create_report(i: int, data):
         metrics=[
             metrics.DatasetSummaryMetric(),
             metrics.DatasetDriftMetric(),
-            metrics.ColumnDriftMetric(column_name='prediction'),
-            metrics.ColumnDriftMetric(column_name='Rating'),
-            metrics.ColumnDriftMetric(column_name='Age'),
-            metrics.ColumnDriftMetric(column_name='Positive_Feedback_Count'),
-            metrics.ColumnDriftMetric(column_name='Division_Name'),
-            metrics.ColumnDriftMetric(column_name='Department_Name'),
-            metrics.ColumnDriftMetric(column_name='Class_Name'),
+            metrics.ColumnDriftMetric(column_name="prediction"),
+            metrics.ColumnDriftMetric(column_name="Rating"),
+            metrics.ColumnDriftMetric(column_name="Age"),
+            metrics.ColumnDriftMetric(column_name="Positive_Feedback_Count"),
+            metrics.ColumnDriftMetric(column_name="Division_Name"),
+            metrics.ColumnDriftMetric(column_name="Department_Name"),
+            metrics.ColumnDriftMetric(column_name="Class_Name"),
             metrics.ColumnDriftMetric(column_name="Review_Text"),
-            metrics.ColumnDriftMetric(column_name='Title'),
+            metrics.ColumnDriftMetric(column_name="Title"),
             metrics.ClassificationQualityMetric(),
             metrics.ColumnSummaryMetric(column_name=descriptors.OOV(display_name="OOV").for_column("Review_Text")),
-            metrics.ColumnSummaryMetric(column_name=descriptors.NonLetterCharacterPercentage(
-                display_name="NonLetterCharacterPercentage"
-            ).for_column("Review_Text")),
+            metrics.ColumnSummaryMetric(
+                column_name=descriptors.NonLetterCharacterPercentage(
+                    display_name="NonLetterCharacterPercentage"
+                ).for_column("Review_Text")
+            ),
             metrics.ColumnSummaryMetric(
                 column_name=descriptors.Sentiment(display_name="Sentiment").for_column("Review_Text")
             ),
-            metrics.ColumnSummaryMetric(column_name=descriptors.RegExp(
-                display_name="urls", reg_exp='.*(http|www)\S+.*').for_column("Review_Text")
+            metrics.ColumnSummaryMetric(
+                column_name=descriptors.RegExp(display_name="urls", reg_exp=r".*(http|www)\S+.*").for_column(
+                    "Review_Text"
+                )
             ),
             metrics.ColumnValueRangeMetric(
                 column_name=descriptors.TextLength(display_name="TextLength in the Range").for_column("Review_Text"),
                 left=1,
-                right=1000
+                right=1000,
             ),
             metrics.ColumnCategoryMetric(
                 column_name=descriptors.TriggerWordsPresence(
                     display_name="competitors",
                     words_list=["theotherstore", "amajorcompetitor", "awesomeshop"],
-                    lemmatize=False
+                    lemmatize=False,
                 ).for_column("Review_Text"),
-                category=1
+                category=1,
             ),
-            metrics.ColumnCategoryMetric(column_name='Rating', category=1),
-            metrics.ColumnCategoryMetric(column_name='Rating', category=5),
+            metrics.ColumnCategoryMetric(column_name="Rating", category=1),
+            metrics.ColumnCategoryMetric(column_name="Rating", category=5),
         ],
         timestamp=datetime(2023, 1, 29) + timedelta(days=i + 1),
     )
@@ -102,15 +111,13 @@ def create_report(i: int, data):
 
     if i < 17:
         text_report.run(
-          reference_data=reference,
-          current_data=current.iloc[1000 * i : 1000 * (i + 1), :],
-          column_mapping=column_mapping
+            reference_data=reference,
+            current_data=current.iloc[1000 * i : 1000 * (i + 1), :],
+            column_mapping=column_mapping,
         )
     else:
         text_report.run(
-            reference_data=reference,
-            current_data=current[(current.Rating < 5)],
-            column_mapping=column_mapping
+            reference_data=reference, current_data=current[(current.Rating < 5)], column_mapping=column_mapping
         )
 
     return text_report
@@ -198,15 +205,15 @@ def create_project(workspace: WorkspaceBase):
     # features drift
     # text
     values = []
-    for col in ['Title', 'Review_Text']:
-     values.append(
-        PanelValue(
-            metric_id="ColumnDriftMetric",
-            metric_args={"column_name.name": col},
-            field_path=metrics.ColumnDriftMetric.fields.drift_score,
-            legend=col,
-        ),
-     )
+    for col in ["Title", "Review_Text"]:
+        values.append(
+            PanelValue(
+                metric_id="ColumnDriftMetric",
+                metric_args={"column_name.name": col},
+                field_path=metrics.ColumnDriftMetric.fields.drift_score,
+                legend=col,
+            ),
+        )
     project.dashboard.add_panel(
         DashboardPanelPlot(
             title="Data Drift: review texts (domain classifier ROC AUC) ",
@@ -218,15 +225,15 @@ def create_project(workspace: WorkspaceBase):
     )
     # numerical
     values = []
-    for col in ['Age', 'Positive_Feedback_Count']:
-     values.append(
-        PanelValue(
-            metric_id="ColumnDriftMetric",
-            metric_args={"column_name.name": col},
-            field_path=metrics.ColumnDriftMetric.fields.drift_score,
-            legend=f"{col}",
-        ),
-     )
+    for col in ["Age", "Positive_Feedback_Count"]:
+        values.append(
+            PanelValue(
+                metric_id="ColumnDriftMetric",
+                metric_args={"column_name.name": col},
+                field_path=metrics.ColumnDriftMetric.fields.drift_score,
+                legend=f"{col}",
+            ),
+        )
     project.dashboard.add_panel(
         DashboardPanelPlot(
             title="Data Drift: numerical features (Wasserstein distance)",
@@ -238,15 +245,15 @@ def create_project(workspace: WorkspaceBase):
     )
     # categorical
     values = []
-    for col in ['Division_Name', 'Department_Name', 'Class_Name']:
-     values.append(
-        PanelValue(
-            metric_id="ColumnDriftMetric",
-            metric_args={"column_name.name": col},
-            field_path=metrics.ColumnDriftMetric.fields.drift_score,
-            legend=col,
-        ),
-     )
+    for col in ["Division_Name", "Department_Name", "Class_Name"]:
+        values.append(
+            PanelValue(
+                metric_id="ColumnDriftMetric",
+                metric_args={"column_name.name": col},
+                field_path=metrics.ColumnDriftMetric.fields.drift_score,
+                legend=col,
+            ),
+        )
     project.dashboard.add_panel(
         DashboardPanelPlot(
             title="Data Drift: categorical features (Jensen-Shannon distance)",
@@ -322,7 +329,7 @@ def create_project(workspace: WorkspaceBase):
                     field_path="current_characteristics.mean",
                     legend="sentiment (mean)",
                 ),
-                ],
+            ],
             plot_type=PlotType.LINE,
             size=1,
         )
@@ -339,14 +346,14 @@ def create_project(workspace: WorkspaceBase):
                         "column_name": descriptors.TriggerWordsPresence(
                             display_name="competitors",
                             words_list=["theotherstore", "amajorcompetitor", "awesomeshop"],
-                            lemmatize=False
+                            lemmatize=False,
                         ).for_column("Review_Text"),
-                        "category": 1
+                        "category": 1,
                     },
                     field_path="current.category_ratio",
                     legend="reviews with competitors",
                 ),
-                ],
+            ],
             plot_type=PlotType.LINE,
             size=1,
         )
@@ -360,15 +367,14 @@ def create_project(workspace: WorkspaceBase):
                 PanelValue(
                     metric_id="ColumnSummaryMetric",
                     metric_args={
-                        "column_name": descriptors.RegExp(
-                            display_name="urls",
-                            reg_exp='.*(http|www)\S+.*'
-                        ).for_column("Review_Text")
+                        "column_name": descriptors.RegExp(display_name="urls", reg_exp=r".*(http|www)\S+.*").for_column(
+                            "Review_Text"
+                        )
                     },
                     field_path="current_characteristics.mean",
                     legend="reviews with URLs",
                 ),
-                ],
+            ],
             plot_type=PlotType.LINE,
             size=1,
         )
@@ -380,14 +386,11 @@ def create_project(workspace: WorkspaceBase):
             filter=ReportFilter(metadata_values={}, tag_values=[]),
             values=[
                 PanelValue(
-                  metric_id="ColumnCategoryMetric",
-                  metric_args={
-                      "column_name.name": 'Rating',
-                      "category": 1
-                  },
-                  field_path="current.category_ratio",
-                  legend='share of "1"',
-              ),
+                    metric_id="ColumnCategoryMetric",
+                    metric_args={"column_name.name": "Rating", "category": 1},
+                    field_path="current.category_ratio",
+                    legend='share of "1"',
+                ),
             ],
             plot_type=PlotType.LINE,
             size=1,
@@ -399,20 +402,17 @@ def create_project(workspace: WorkspaceBase):
             filter=ReportFilter(metadata_values={}, tag_values=[]),
             values=[
                 PanelValue(
-                  metric_id="ColumnCategoryMetric",
-                  metric_args={
-                      "column_name.name": 'Rating',
-                      "category": 5
-                  },
-                  field_path="current.category_ratio",
-                  legend='share of "5"',
-              ),
+                    metric_id="ColumnCategoryMetric",
+                    metric_args={"column_name.name": "Rating", "category": 5},
+                    field_path="current.category_ratio",
+                    legend='share of "5"',
+                ),
             ],
             plot_type=PlotType.LINE,
             size=1,
         )
     )
-    
+
     project.save()
     return project
 
