80fdf0cb0fe5cfaa7f5f580d8b428144cc104962
mike0sv <mike0sv@gmail.com>
2023-09-12 18:44:09 +0300

    fix period serialization

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,23 +3,32 @@ import pandas as pd
 from pydantic import BaseModel
 
 
-def smart_assert_equal(actual, expected):
+def smart_assert_equal(actual, expected, path=""):
     if isinstance(actual, BaseModel) and isinstance(expected, BaseModel) and actual.__class__ is expected.__class__:
         for field in actual.__fields__.values():
-            smart_assert_equal(getattr(actual, field.name), getattr(expected, field.name))
+            smart_assert_equal(getattr(actual, field.name), getattr(expected, field.name), path=f"{path}.{field.name}")
         return
     if isinstance(actual, pd.Series):
-        pd.testing.assert_series_equal(actual, expected)
+        try:
+            pd.testing.assert_series_equal(actual, expected)
+        except AssertionError as e:
+            raise AssertionError(e.args[0] + f"\npath: {path}")
         return
     if isinstance(actual, pd.DataFrame):
-        pd.testing.assert_frame_equal(actual, expected)
+        try:
+            pd.testing.assert_frame_equal(actual, expected)
+        except AssertionError as e:
+            raise AssertionError(e.args[0] + f"\npath: {path}")
         return
     if isinstance(actual, list) and isinstance(expected, list):
         for idx in range(len(actual)):
-            smart_assert_equal(actual[idx], expected[idx])
+            smart_assert_equal(actual[idx], expected[idx], path=f"{path}[{idx}]")
         return
     if isinstance(actual, dict) and isinstance(expected, dict):
         for key in set(actual.keys()) | set(expected.keys()):
-            smart_assert_equal(actual.get(key), expected.get(key))
+            smart_assert_equal(actual.get(key), expected.get(key), path=f"{path}[{key}]")
         return
-    np.testing.assert_equal(actual, expected)
+    try:
+        np.testing.assert_equal(actual, expected, f"path: {path}")
+    except AssertionError:
+        raise
--- tests/multitest/metrics/conftest.py
+++ tests/multitest/metrics/conftest.py
@@ -108,7 +108,7 @@ def load_test_metrics():
 
 # for debugging
 metric_type_filter = []
-metric_name_filter = []
+metric_name_filter = ["column_summary_metric_period"]
 
 
 def generate_metric_dataset_outcome():
