4e070fd7a7d08636c30e2b796a5873e7048e4336
mike0sv <mike0sv@gmail.com>
2023-09-14 19:11:34 +0300

    example2

---
+++ src/evidently2/example2.py
@@ -0,0 +1,148 @@
+from pprint import pprint
+
+import pandas as pd
+
+from evidently import ColumnMapping
+from evidently2.core.calculation import Context
+from evidently2.core.calculation import InputValue
+from evidently2.core.calculation import get_all_calculations
+from evidently2.core.calculation import partial_calculations
+from evidently2.core.compat import InputData2
+from evidently2.core.spark import create_data_definition_spark
+from evidently.base_metric import ColumnName
+from evidently.base_metric import MetricResult
+from evidently.utils.data_preprocessing import create_data_definition
+
+ref_pd = pd.DataFrame([{"a": 0}, {"a": 1}, {"a": 2}])
+cur_pd = pd.DataFrame([{"a": 0}, {"a": 0}, {"a": 0}])
+
+
+def old_evidently():
+    from evidently.metrics import ColumnDriftMetric
+    from evidently.options import DataDriftOptions
+    from evidently.report import Report
+
+    DataDriftOptions.__fields__["nbinsx"].default = 2
+    report = Report(metrics=[ColumnDriftMetric("a")])
+
+    ref = ref_pd
+    cur = cur_pd
+    report.run(reference_data=ref, current_data=cur, column_mapping=ColumnMapping(numerical_features=["a"]))
+
+    pprint(report.as_dict()["metrics"][0]["result"])
+    result = {
+        "column_name": "a",
+        "column_type": "num",
+        "current": {"small_distribution": {"x": [-0.5, 0.0, 0.5], "y": [0.0, 2.0]}},
+        "drift_detected": True,
+        "drift_score": 0.04978706836786395,
+        "reference": {"small_distribution": {"x": [0.0, 1.0, 2.0], "y": [0.3333333333333333, 0.6666666666666666]}},
+        "stattest_name": "chi-square p_value",
+        "stattest_threshold": 0.05,
+    }
+
+
+def new():
+    from evidently2.metrics.drift.column_drift_metric import ColumnDriftMetric
+    from evidently.options import DataDriftOptions
+
+    DataDriftOptions.__fields__["nbinsx"].default = 2
+
+    metric = ColumnDriftMetric(column_name=ColumnName.from_any("a"))
+
+    ref = ref_pd
+    cur = cur_pd
+    from evidently2.core.calculation import InputData
+
+    data = InputData(
+        current_data=cur,
+        reference_data=ref,
+        data_definition=create_data_definition(ref, cur, ColumnMapping(numerical_features=["a"])),
+    )
+
+    with Context.new():
+        result = metric.calculate(data)
+    # pprint(result.get_result().dict())
+    result_dict = {
+        "column_name": "a",
+        "column_type": "Numerical",
+        "current_small_distribution": {
+            "type": "evidently.metric_results.Distribution",
+            "x": [-0.5, 0.0, 0.5],
+            "y": [0.0, 2.0],
+        },
+        "drift_detected": True,
+        "drift_score": 0.04978706836786395,
+        "reference_small_distribution": {
+            "type": "evidently.metric_results.Distribution",
+            "x": [0.0, 1.0, 2.0],
+            "y": [0.3333333333333333, 0.6666666666666666],
+        },
+        "stattest_name": "chi-square p_value",
+        "stattest_threshold": 0.05,
+        "type": "evidently2.metrics.drift.column_drift_metric.ColumnDriftResultCalculation",
+    }
+    column_mapping = ColumnMapping(numerical_features=["a"])
+    from evidently2.core.suite import Report
+
+    report = Report(metrics=[metric])
+    report.run(cur, ref, column_mapping)
+    profile = report.create_reference_profile(ref, column_mapping)
+
+    report2 = profile.run(cur)
+
+    pprint(report2.as_dict())
+
+
+def new2():
+    from evidently2.metrics.drift.column_drift_metric2 import ColumnDriftMetric
+    from evidently.options import DataDriftOptions
+
+    DataDriftOptions.__fields__["nbinsx"].default = 2
+
+    metric = ColumnDriftMetric(column_name=ColumnName.from_any("a"))
+
+    ref = ref_pd
+    cur = cur_pd
+
+    dd = create_data_definition(ref, cur, ColumnMapping(numerical_features=["a"]))
+
+    ref_result = metric.calculate_reference(dd, ref)
+
+    result = metric.calculate(dd, ref_result, cur)
+
+    pprint(result.dict())
+    result_dict = {
+        'column_name': 'a',
+        'column_type': 'Numerical',
+        'current_small_distribution': {'type': 'evidently.metric_results.DistributionIncluded',
+                                       'x': [-0.5, 0.0, 0.5],
+                                       'y': [0.0, 2.0]},
+        'drift_detected': True,
+        'drift_score': 0.04978706836786395,
+        'reference_small_distribution': {'type': 'evidently.metric_results.DistributionIncluded',
+                                         'x': [0.0, 1.0, 2.0],
+                                         'y': [0.3333333333333333,
+                                               0.6666666666666666]},
+        'stattest_name': 'chi-square p_value',
+        'stattest_threshold': 0.05,
+        'type': 'evidently2.metrics.drift.column_drift_metric.ColumnDriftResult'
+    }
+    # column_mapping = ColumnMapping(numerical_features=["a"])
+    # from evidently2.core.suite import Report
+    #
+    # report = Report(metrics=[metric])
+    # report.run(cur, ref, column_mapping)
+    # profile = report.create_reference_profile(ref, column_mapping)
+    #
+    # report2 = profile.run(cur)
+    #
+    # pprint(report2.as_dict())
+
+
+if __name__ == "__main__":
+    # old_evidently()
+    new2()
+    # clean_spark()
+    # new_spark()
+    # groupby()
--- src/evidently2/metrics/drift/column_drift_metric2.py
+++ src/evidently2/metrics/drift/column_drift_metric2.py
@@ -1,18 +1,29 @@
-from typing import Any, Dict, Generic, Optional, TypeVar
+import dataclasses
+from typing import Any, Callable, Dict, Generic, List, Optional, Tuple, TypeVar
+
+import numpy as np
+import pandas as pd
+from scipy.stats import chisquare
 
 from evidently.base_metric import ColumnName, MetricResult
+from evidently.core import ColumnType
+from evidently.metric_results import DistributionIncluded
+from evidently.options import DataDriftOptions
 from evidently.pydantic_utils import EvidentlyBaseModel
+from evidently.utils.data_preprocessing import DataDefinition
 from evidently2.core.calculation import DataType
-from evidently2.metrics.drift.column_drift_metric import ColumnDriftResult
+from evidently2.metrics.drift.column_drift_metric import ColumnDriftResult, StatTest
 
 ReferenceResultType = TypeVar("ReferenceResultType", bound=MetricResult)
 ResultType = TypeVar("ResultType", bound=MetricResult)
 
+ColumnDataType = pd.Series
+
 class Metric3(EvidentlyBaseModel, Generic[ReferenceResultType, ResultType]):
-    def calculate_reference(self, reference: DataType) -> ReferenceResultType:
+    def calculate_reference(self, data_definition: DataDefinition, reference: DataType) -> ReferenceResultType:
         raise NotImplementedError
 
-    def calculate(self, ref: ReferenceResultType, current: DataType) -> ResultType:
+    def calculate(self, data_definition: DataDefinition, ref: ReferenceResultType, current: DataType) -> ResultType:
         raise NotImplementedError
 
 
@@ -26,16 +37,168 @@ class ChiSquareReferenceResult(StatTestReferenceResult):
 class ColumnDriftReferenceResult(MetricResult):
     stat_test: str
     stat_test_result: StatTestReferenceResult
+    small_hist: Optional[DistributionIncluded]
 
+
+@dataclasses.dataclass
+class StatTestResult:
+    drift_score: float
+    drifted: bool
+    actual_threshold: float
+
+
+StatTestFuncType = Callable[[StatTestReferenceResult, ColumnDataType, str, float], Tuple[float, bool]]
+StatTestReferenceFuncType = Callable[[ColumnDataType, str, float], StatTestReferenceResult]
+
+@dataclasses.dataclass
+class StatTest:
+    name: str
+    display_name: str
+    func: StatTestFuncType
+    get_reference: StatTestReferenceFuncType
+    allowed_feature_types: List[str]
+    default_threshold: float = 0.05
+
+
+    def __call__(
+        self, reference_data: StatTestReferenceResult, current_data: ColumnDataType, feature_type: str, threshold: Optional[float]
+    ) -> StatTestResult:
+        actual_threshold = self.default_threshold if threshold is None else threshold
+        p = self.func(reference_data, current_data, feature_type, actual_threshold)
+        drift_score, drifted = p
+        return StatTestResult(drift_score=drift_score, drifted=drifted, actual_threshold=actual_threshold)
+
+    def __hash__(self):
+        # hash by name, so stattests with same name would be the same.
+        return hash(self.name)
+
+def _chi_stat_reference(reference_data: ColumnDataType, feature_type: str, threshold: float) -> ChiSquareReferenceResult:
+    return ChiSquareReferenceResult(
+        value_counts=reference_data.value_counts().to_dict(),
+        size=reference_data.shape[0]
+    )
+
+def _chi_stat_test(
+    reference_data: ChiSquareReferenceResult, current_data: ColumnDataType, feature_type: str, threshold: float
+) -> Tuple[float, bool]:
+    k_norm = current_data.shape[0] / reference_data.size
+    ref_feature_dict = reference_data.value_counts
+    current_feature_dict = current_data.value_counts().to_dict()
+
+    keys = set(ref_feature_dict.keys()) | set(current_feature_dict.keys())
+    f_exp = [ref_feature_dict.get(key, 0) * k_norm for key in keys]
+    f_obs = [current_feature_dict.get(key, 0) for key in keys]
+    p_value = chisquare(f_obs, f_exp)[1]
+    return p_value, p_value < threshold
+
+
+chi_stat_test = StatTest(
+    name="chisquare", display_name="chi-square p_value", func=_chi_stat_test, get_reference=_chi_stat_reference, allowed_feature_types=["cat"]
+)
+
+z_stat_test = wasserstein_stat_test = ks_stat_test = jensenshannon_stat_test = chi_stat_test
+
+def _get_default_stattest(reference_data: ColumnDataType, feature_type: str) -> StatTest:
+    if feature_type != "num":
+        raise NotImplementedError
+
+    # todo: we can make this lazy too
+    n_values = reference_data.nunique()
+    size = reference_data.shape[0]
+    if size <= 1000:
+        if n_values <= 5:
+            return chi_stat_test if n_values > 2 else z_stat_test
+        elif n_values > 5:
+            return ks_stat_test
+    elif size > 1000:
+        if n_values <= 5:
+            return jensenshannon_stat_test
+        elif n_values > 5:
+            return wasserstein_stat_test
+
+    raise ValueError(f"Unexpected feature_type {feature_type}")
+
+
+def get_stattest(reference_data: ColumnDataType, feature_type: str, stattest_func: Optional[str]) -> StatTest:
+    if stattest_func is None:
+        return _get_default_stattest(reference_data, feature_type)
 
 class ColumnDriftMetric(Metric3[ColumnDriftReferenceResult, ColumnDriftResult]):
     column_name: ColumnName
     stattest: Optional[str]
     stattest_threshold: Optional[float]
 
-    def calculate_reference(self, reference: DataType) -> ColumnDriftReferenceResult:
-        pass
+    def calculate_reference(self, data_definition: DataDefinition, reference: DataType) -> ColumnDriftReferenceResult:
+        column = self.column_name
+        reference_column = reference[self.column_name.name]
+        reference_column = reference_column.replace([-np.inf, np.inf], np.nan).dropna()
 
-    def calculate(self, ref: ColumnDriftReferenceResult, current: DataType) -> ColumnDriftResult:
-        pass
+        if reference_column.empty:
+            raise ValueError(
+                f"An empty column '{column.name}' was provided for drift calculation in the reference dataset."
+            )
+
+        column_type = data_definition.get_column(self.column_name.name).column_type
+        stat_test = get_stattest(reference_column, column_type.value, self.stattest)
+
+        options = DataDriftOptions(all_features_stattest=self.stattest, threshold=self.stattest_threshold)
+
+        reference_small_distribution = [
+            t.tolist()
+            for t in np.histogram(
+                reference_column[np.isfinite(reference_column)],
+                bins=options.get_nbinsx(self.column_name.name),
+                density=True,
+            )
+        ]
+        return ColumnDriftReferenceResult(
+            stat_test=stat_test.name,
+            stat_test_result=stat_test.get_reference(reference_column, column_type.value, self.stattest_threshold),
+            small_hist=DistributionIncluded(
+                x=reference_small_distribution[1], y=reference_small_distribution[0]
+            )
+            if reference_small_distribution
+            else None,
+        )
+
+    def calculate(self, data_definition: DataDefinition, ref: ColumnDriftReferenceResult, current: DataType) -> ColumnDriftResult:
+        options = DataDriftOptions(all_features_stattest=self.stattest, threshold=self.stattest_threshold)
+
+        # todo: get it from registry useing ref.stat_test
+        drift_test_function = chi_stat_test
+        assert drift_test_function.name == ref.stat_test
+
+        column_type = data_definition.get_column(self.column_name.name).column_type
+
+        current_column = current[self.column_name.name]
+        current_column = current_column.replace([-np.inf, np.inf], np.nan).dropna()
+        drift_result = drift_test_function(ref.stat_test_result, current_column, column_type.value, self.stattest_threshold)
+        column = self.column_name
+
+        current_small_distribution: Optional[List[List[float]]] = None
+
+        if column_type == ColumnType.Numerical:
+            current_nbinsx = options.get_nbinsx(column.name)
+
+            current_small_distribution = [
+                t.tolist()
+                for t in np.histogram(
+                    current_column[np.isfinite(current_column)],
+                    bins=current_nbinsx,
+                    density=True,
+                )
+            ]
+
+
+        return ColumnDriftResult(
+            column_name=column.name,
+            column_type=column_type.name,
+            stattest_name=drift_test_function.display_name,
+            stattest_threshold=drift_result.actual_threshold,
+            drift_score=drift_result.drift_score,
+            drift_detected=drift_result.drifted,
+            current_small_distribution=DistributionIncluded(x=current_small_distribution[1], y=current_small_distribution[0])         if current_small_distribution
+            else None,
+            reference_small_distribution=ref.small_hist,
+    )
 
