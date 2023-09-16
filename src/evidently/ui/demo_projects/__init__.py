a64615bce65480422ad912e9a7c8b0881276b6e7
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2023-09-16 11:28:01 +0200

    Example changes

--- src/evidently/ui/demo_projects/__init__.py
+++ src/evidently/ui/demo_projects/__init__.py
@@ -2,6 +2,11 @@ from typing import Dict
 
 from .base import DemoProject
 from .bikes import bikes_demo_project
+from .other_bikes import other_bikes_demo_project
 from .reviews import reviews_demo_project
 
-DEMO_PROJECTS: Dict[str, DemoProject] = {"bikes": bikes_demo_project, "reviews": reviews_demo_project}
+DEMO_PROJECTS: Dict[str, DemoProject] = {
+    "other_bikes": other_bikes_demo_project,
+    "bikes": bikes_demo_project,
+    "reviews": reviews_demo_project,
+}
