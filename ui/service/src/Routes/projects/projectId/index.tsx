30d102ea97d05a3535063cab2d684d86c5bf19e6
DimaAmega <dima.amega1@gmail.com>
2023-10-03 18:22:22 +0400

    added redirect `test_suites/*` to `test-suites/*`

--- ui/service/src/Routes/projects/projectId/index.tsx
+++ ui/service/src/Routes/projects/projectId/index.tsx
@@ -8,11 +8,12 @@ import { Component, handle, loader } from './Component'
 import DashboardRoute from './dashboard'
 import ReportsRoute from './reports'
 import TestSuitesRoute from './test-suites'
+import TestSuitesOldRoute from './test_suites'
 
 export default {
   path: 'projects/:projectId',
   loader,
   Component,
   handle,
-  children: [DashboardRoute, ReportsRoute, TestSuitesRoute]
+  children: [DashboardRoute, ReportsRoute, TestSuitesRoute, TestSuitesOldRoute]
 } satisfies RouteObject
