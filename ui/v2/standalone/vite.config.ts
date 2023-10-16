f6043397166995c03edb175445a31f885b982ef0
DimaAmega <dima.amega1@gmail.com>
2023-10-16 15:19:47 +0300

    update build scripts

--- ui/v2/standalone/vite.config.ts
+++ ui/v2/standalone/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js'
+      }
+    }
+  }
 })
