2eca7ee434bc2f6c4596e86e3963883ce0afafc1
mike0sv <mike0sv@gmail.com>
2023-10-26 14:57:34 +0300

    Adult dataset demo

--- tests/ui/test_demo_project.py
+++ tests/ui/test_demo_project.py
@@ -5,7 +5,7 @@ from evidently.ui.workspace import Workspace
 
 
 @pytest.mark.parametrize("demo_project", list(DEMO_PROJECTS.keys()))
-def test_create_demo_proejct(demo_project, tmp_path):
+def test_create_demo_project(demo_project, tmp_path):
     dp = DEMO_PROJECTS[demo_project]
     dp.create(str(tmp_path))
 
