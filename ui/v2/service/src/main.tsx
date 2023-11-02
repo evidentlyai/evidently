813a2cacf85e180bb25d0276adbc541b9703c584
DimaAmega <dima.amega1@gmail.com>
2023-11-03 00:59:35 +0400

    wip

--- ui/v2/service/src/main.tsx
+++ ui/v2/service/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Routes } from 'routes'
+import { Routes } from 'Routes'
 
 const router = createBrowserRouter(Routes)
 
