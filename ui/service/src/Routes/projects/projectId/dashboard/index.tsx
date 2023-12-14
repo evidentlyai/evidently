bda11fddbafb2c6e70141b1ce9e82e739019db34
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2023-12-14 18:04:32 +0400

    fix bug: bad code splitting (#897)
    * fix bug: bad code splitting
    
    * commit UI assets

--- ui/service/src/Routes/projects/projectId/dashboard/index.tsx
+++ ui/service/src/Routes/projects/projectId/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { RouteObject } from 'react-router-dom'
 import { injectAPI } from 'evidently-ui-lib/routes-components/dashboard/data'
-import { DashboardWidgets } from 'evidently-ui-lib/components/DashboardWidgets'
 import { api } from 'api/RemoteApi'
 
 const { loader } = injectAPI({ api })
@@ -9,9 +8,10 @@ export default {
   index: true,
   id: 'dashboard',
   lazy: async () => {
-    const { DashboardComponentTemplate } = await import(
-      'evidently-ui-lib/routes-components/dashboard'
-    )
+    const [{ DashboardComponentTemplate }, { DashboardWidgets }] = await Promise.all([
+      import('evidently-ui-lib/routes-components/dashboard'),
+      import('evidently-ui-lib/components/DashboardWidgets')
+    ])
 
     return {
       Component: () => (
