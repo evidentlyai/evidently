e8cb6b5c347eed3f09953e7447b2d760f1af8d8a
Vyacheslav Morov <Liraim@users.noreply.github.com>
2024-01-12 17:42:22 +0100

    Add support for size parameter in CustomValueMetric. (#944)
    * Add support for size parameter in CustomValueMetric.
    
    * Fix linter.
    
    * Make WidgetSize json serializable.

--- tests/multitest/metrics/custom.py
+++ tests/multitest/metrics/custom.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from evidently.base_metric import InputData
-from evidently.metrics.custom_metric import CustomCallableMetric
+from evidently.metrics.custom_metric import CustomValueMetric
 from tests.multitest.conftest import AssertResultFields
 from tests.multitest.datasets import TestDataset
 from tests.multitest.metrics.conftest import TestMetric
@@ -18,7 +18,7 @@ def custom_callable_metric():
 
     return TestMetric(
         "custom_callable_metric",
-        CustomCallableMetric(func=custom_func, title="aaa"),
+        CustomValueMetric(func=custom_func, title="aaa"),
         AssertResultFields({"value": 0.3}),
         datasets=[
             TestDataset("custom_callable_metric_data", current=current_data, reference=reference_data, tags=[]),
