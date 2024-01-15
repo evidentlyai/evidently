fdf16f65df0984cf4834f2988b012a4213a5af12
DimaAmega <dima.amega1@gmail.com>
2024-01-15 17:56:21 +0400

    wip

--- ui/service/tests/visual.spec.ts
+++ ui/service/tests/visual.spec.ts
@@ -1,35 +1,67 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
+
+const goToFirstSnapshotAndExpanSomeWidgets = async ({
+  projectName,
+  page,
+  isTestSuite
+}: {
+  page: Page
+  isTestSuite: boolean
+  projectName: string
+}) => {
+  await page.getByRole('link', { name: projectName }).click()
+  await page.getByRole('tab', { name: isTestSuite ? 'Test Suites' : 'Reports' }).click()
+  await page.getByRole('button', { name: 'View' }).first().click()
+  await page.waitForLoadState('networkidle')
+  await page.getByRole('button', { name: 'Details' }).first().click()
+  await page.getByRole('button', { name: 'Details' }).nth(1).click()
+  await page.getByRole('button', { name: 'Details' }).last().click()
+  await page.waitForLoadState('networkidle')
+}
+
+const VisualTestSnapshot = async ({
+  page,
+  projectName,
+  isTestSuite
+}: {
+  page: Page
+  projectName: string
+  isTestSuite: boolean
+}) => {
+  await page.goto('/')
+  await goToFirstSnapshotAndExpanSomeWidgets({
+    page,
+    projectName,
+    isTestSuite
+  })
+  await expect(page).toHaveScreenshot({ fullPage: true })
+}
+
+const VisualTestDashboard = async ({ page, projectName }: { page: Page; projectName: string }) => {
+  await page.goto('/')
+  await page.getByRole('link', { name: projectName }).click()
+  await page.waitForLoadState('networkidle')
+  await expect(page).toHaveScreenshot({ fullPage: true })
+}
 
 test.skip('Home screen', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveScreenshot({ fullPage: true })
 })
 
-test('Bikes Dashboard', async ({ page }) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
-  await page.waitForLoadState('networkidle')
-  await expect(page).toHaveScreenshot({ fullPage: true })
+/////////////////////
+///     BIKES
+/////////////////////
+const BikesDemoProjectName = 'Demo project - Bikes'
+
+test(`${BikesDemoProjectName}: Dashboard`, async ({ page }) => {
+  await VisualTestDashboard({ page, projectName: BikesDemoProjectName })
 })
 
-test('Bikes Report', async ({ page }) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
-  await page.getByRole('tab', { name: 'Reports' }).click()
-  await page.getByRole('button', { name: 'View' }).first().click()
-  await page.waitForLoadState('networkidle')
-  await page.getByRole('button', { name: 'Details' }).first().click()
-  await page.waitForLoadState('networkidle')
-  await expect(page).toHaveScreenshot({ fullPage: true })
+test(`${BikesDemoProjectName}: Report`, async ({ page }) => {
+  await VisualTestSnapshot({ page, projectName: BikesDemoProjectName, isTestSuite: false })
 })
 
-test('Bikes Test Suite', async ({ page }) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
-  await page.getByRole('tab', { name: 'Test Suites' }).click()
-  await page.getByRole('button', { name: 'View' }).first().click()
-  await page.waitForLoadState('networkidle')
-  await page.getByRole('button', { name: 'Details' }).first().click()
-  await page.waitForLoadState('networkidle')
-  await expect(page).toHaveScreenshot({ fullPage: true })
+test(`${BikesDemoProjectName}: Test Suite`, async ({ page }) => {
+  await VisualTestSnapshot({ page, projectName: BikesDemoProjectName, isTestSuite: true })
 })
