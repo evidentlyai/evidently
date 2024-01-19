45944af2a36367a9e7925ea883a89c9792599aa8
DimaAmega <dima.amega1@gmail.com>
2024-01-19 09:15:41 +0400

    Global Progress even fetchers

--- ui/packages/evidently-ui-lib/src/components/NavigationProgress.tsx
+++ ui/packages/evidently-ui-lib/src/components/NavigationProgress.tsx
@@ -1,9 +1,10 @@
-import { useNavigation } from 'react-router-dom'
+import { useFetchers, useNavigation } from 'react-router-dom'
 import { Box, LinearProgress } from '@mui/material'
 
 export const NavigationProgress = () => {
   const navigation = useNavigation()
-  const isNavigation = navigation.state !== 'idle' || fetchers.some((f) => f.state !== 'idle')
+  const fetchers = useFetchers()
+  const isNavigation = navigation.state !== 'idle' || fetchers.some(({ state }) => state !== 'idle')
 
   if (isNavigation) {
     return <LinearProgress sx={{ height: 4, position: 'sticky', top: 0, left: 0, zIndex: 999 }} />
