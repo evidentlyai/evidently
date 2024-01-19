3a9a3efcf334e4f85292d348c38461542b67cdc7
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2024-01-19 09:06:11 +0400

    Update NavigationProgress.tsx

--- ui/packages/evidently-ui-lib/src/components/NavigationProgress.tsx
+++ ui/packages/evidently-ui-lib/src/components/NavigationProgress.tsx
@@ -3,7 +3,7 @@ import { Box, LinearProgress } from '@mui/material'
 
 export const NavigationProgress = () => {
   const navigation = useNavigation()
-  const isNavigation = navigation.state !== 'idle'
+  const isNavigation = navigation.state !== 'idle' || fetchers.some((f) => f.state !== 'idle')
 
   if (isNavigation) {
     return <LinearProgress sx={{ height: 4, position: 'sticky', top: 0, left: 0, zIndex: 999 }} />
