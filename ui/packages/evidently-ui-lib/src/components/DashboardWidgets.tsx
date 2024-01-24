4cebed2c4ab8903189dcd1b558692d1f5d1c027d
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2024-01-24 22:16:18 +0400

    Fix React.Fragment for DashboardContentWidgets (#958)
    * fix React.Fragment
    * Update NavigationProgress.tsx
    * Global Progress even fetchers

--- ui/packages/evidently-ui-lib/src/components/DashboardWidgets.tsx
+++ ui/packages/evidently-ui-lib/src/components/DashboardWidgets.tsx
@@ -2,11 +2,17 @@ import { Grid } from '@mui/material'
 import { WidgetInfo } from '~/api'
 import { DashboardContentWidgets } from '~/components/DashboardContent'
 
-export const DashboardWidgets = ({ widgets }: { widgets: WidgetInfo[] }) => {
+export const DashboardWidgets = ({
+  widgets,
+  ItemWrapper
+}: {
+  widgets: WidgetInfo[]
+  ItemWrapper?: ({ id, children }: { id: string; children: React.ReactNode }) => React.ReactNode
+}) => {
   return (
     <>
       <Grid container spacing={3} direction="row" alignItems="stretch">
-        <DashboardContentWidgets widgets={widgets} />
+        <DashboardContentWidgets widgets={widgets} ItemWrapper={ItemWrapper} />
       </Grid>
     </>
   )
