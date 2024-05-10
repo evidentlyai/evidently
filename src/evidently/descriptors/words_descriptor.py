71a2829570053105f2aeb8c81cab0720b53bec02
Vyacheslav Morov <Liraim@users.noreply.github.com>
2024-05-10 13:14:51 +0200

    New text descriptors and features. (#1105)
    * Add text contains / text startswith / endswith features and descriptors.
    
    * Split TextPart to TextBegins / TextEnds
    Split TextContains to TextContains / TextNotContains
    
    * Rename Begins / Ends to BeginsWith / EndsWith
    
    * Remove Text from new features names.
    
    * Fix tests after all refactoring.
    
    * Rename NotContains to DoesNotContain.
    
    * Set feature_type for new features to Categorical.
    
    * Add IncludesWords and ExcludesWords features.
    
    * Default value for mode in Contains/Includes descriptors
    
    ---------
    
    Co-authored-by: Emeli Dral <emeli.dral@gmail.com>

--- src/evidently/descriptors/words_descriptor.py
+++ src/evidently/descriptors/words_descriptor.py
@@ -1,26 +1,35 @@
 from typing import List
 
-from evidently.features import trigger_words_presence_feature
+from evidently.features import words_feature
 from evidently.features.generated_features import FeatureDescriptor
 from evidently.features.generated_features import GeneratedFeature
 
 
-class TriggerWordsPresence(FeatureDescriptor):
+class ExcludesWords(FeatureDescriptor):
     words_list: List[str]
+    mode: str = "all"
     lemmatize: bool = True
 
     def feature(self, column_name: str) -> GeneratedFeature:
-        return trigger_words_presence_feature.TriggerWordsPresent(
+        return words_feature.ExcludesWords(
             column_name,
             self.words_list,
+            self.mode,
             self.lemmatize,
             self.display_name,
         )
 
-    def for_column(self, column_name: str):
-        return trigger_words_presence_feature.TriggerWordsPresent(
+
+class IncludesWords(FeatureDescriptor):
+    words_list: List[str]
+    mode: str = "any"
+    lemmatize: bool = True
+
+    def feature(self, column_name: str) -> GeneratedFeature:
+        return words_feature.IncludesWords(
             column_name,
             self.words_list,
+            self.mode,
             self.lemmatize,
             self.display_name,
-        ).feature_name()
+        )
