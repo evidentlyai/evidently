9f2a42aac8ac8ed3ec7a2b0d763046876c530c8a
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2024-05-17 15:51:51 +0200

    Rework test_overview.

--- src/evidently/metric_preset/text_overview.py
+++ src/evidently/metric_preset/text_overview.py
@@ -1,14 +1,18 @@
+import itertools
 from typing import Any
 from typing import Dict
+from typing import List
 from typing import Optional
 
+from evidently.descriptors import OOV
+from evidently.descriptors import NonLetterCharacterPercentage
+from evidently.descriptors import SemanticSimilarity
+from evidently.descriptors import SentenceCount
+from evidently.descriptors import Sentiment
+from evidently.descriptors import TextLength
 from evidently.features.generated_features import FeatureDescriptor
 from evidently.metric_preset.metric_preset import MetricPreset
-from evidently.metrics import ColumnDriftMetric
 from evidently.metrics import ColumnSummaryMetric
-from evidently.metrics import TextDescriptorsCorrelationMetric
-from evidently.metrics import TextDescriptorsDistribution
-from evidently.metrics import TextDescriptorsDriftMetric
 from evidently.utils.data_preprocessing import DataDefinition
 
 
@@ -26,24 +30,41 @@ class TextOverviewPreset(MetricPreset):
         column_name: text column name.
     """
 
-    column_name: str
+    columns: List[str]
 
-    def __init__(self, column_name: str, descriptors: Optional[Dict[str, FeatureDescriptor]] = None):
+    def __init__(
+        self,
+        column_name: Optional[str] = None,
+        columns: Optional[List[str]] = None,
+        descriptors: Optional[Dict[str, FeatureDescriptor]] = None,
+    ):
         super().__init__()
-        self.column_name = column_name
+        if column_name is not None and columns is not None:
+            raise ValueError("Cannot specify both `columns` and `columns`.")
+        if columns is None and columns is None:
+            raise ValueError("Must specify either `columns` or `columns`.")
+        self.columns = columns or [column_name]
         self.descriptors = descriptors
 
     def generate_metrics(self, data_definition: DataDefinition, additional_data: Optional[Dict[str, Any]]):
-        result = [
-            ColumnSummaryMetric(column_name=self.column_name),
-            TextDescriptorsDistribution(column_name=self.column_name, descriptors=self.descriptors),
-            TextDescriptorsCorrelationMetric(column_name=self.column_name, descriptors=self.descriptors),
+        result = []
+        descriptors = self.descriptors or [
+            TextLength(),
+            SentenceCount(),
+            Sentiment(),
+            OOV(),
+            NonLetterCharacterPercentage(),
         ]
-        if data_definition.reference_present is not None:
-            result.extend(
-                [
-                    ColumnDriftMetric(column_name=self.column_name),
-                    TextDescriptorsDriftMetric(column_name=self.column_name, descriptors=self.descriptors),
-                ]
-            )
+        for column in self.columns:
+            result.append(ColumnSummaryMetric(column_name=column))
+            for descriptor in descriptors:
+                feature = descriptor.on(column)
+                result.append(ColumnSummaryMetric(feature))
+
+        if len(self.columns) > 1:
+            column_x_column = itertools.product(self.columns, self.columns)
+            for pair in column_x_column:
+                if pair[0] == pair[1]:
+                    continue
+                result.append(ColumnSummaryMetric(SemanticSimilarity(columns=pair).as_column()))
         return result
