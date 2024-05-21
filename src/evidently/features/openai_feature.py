0fc27a05d8a9f398dcba525ee8096a6e8c8ea589
Vyacheslav Morov <Liraim@users.noreply.github.com>
2024-05-21 22:37:09 +0100

    Fix SemanticSimilarity / CustomDescriptor index values. (#1120)
    Co-authored-by: Emeli Dral <emeli.dral@gmail.com>

--- src/evidently/features/openai_feature.py
+++ src/evidently/features/openai_feature.py
@@ -1,4 +1,5 @@
 import uuid
+from itertools import repeat
 from typing import List
 from typing import Optional
 from typing import Union
@@ -11,13 +12,16 @@ from evidently.core import ColumnType
 from evidently.features.generated_features import GeneratedFeature
 from evidently.utils.data_preprocessing import DataDefinition
 
+_legacy_models = ["gpt-3.5-turbo-instruct", "babbage-002", "davinci-002"]
+
 
 class OpenAIFeature(GeneratedFeature):
     column_name: str
     feature_id: str
     prompt: str
     prompt_replace_string: str
-    context: str
+    context: Optional[str]
+    context_column: Optional[str]
     context_replace_string: str
     openai_params: dict
     model: str
@@ -30,7 +34,8 @@ class OpenAIFeature(GeneratedFeature):
         model: str,
         prompt: str,
         feature_type: str,
-        context: str = "",
+        context: Optional[str] = None,
+        context_column: Optional[str] = None,
         prompt_replace_string: str = "REPLACE",
         context_replace_string: str = "CONTEXT",
         check_mode: str = "any_line",
@@ -41,7 +46,10 @@ class OpenAIFeature(GeneratedFeature):
         self.feature_id = str(uuid.uuid4())
         self.prompt = prompt
         self.prompt_replace_string = prompt_replace_string
+        if context is not None and context_column is not None:
+            raise ValueError("Context and context_column are mutually exclusive")
         self.context = context
+        self.context_column = context_column
         self.context_replace_string = context_replace_string
         self.openai_params = openai_params or {}
         self.model = model
@@ -58,12 +66,31 @@ class OpenAIFeature(GeneratedFeature):
         column_data = data[self.column_name].values.tolist()
         client = OpenAI()
         result: List[Union[str, float, None]] = []
-        prompt = self.prompt.replace(self.context_replace_string, self.context)
-        for value in column_data:
-            prompt = prompt.replace(self.prompt_replace_string, value)
-            prompt_answer = client.completions.create(model=self.model, prompt=prompt, **self.openai_params)
+
+        if self.model in _legacy_models:
+            func = _completions_openai_call
+        else:
+            func = _chat_completions_openai_call
+
+        if self.context_column is not None:
+            context_column = data[self.context_column].values.tolist()
+        else:
+            context_column = repeat(self.context)
+
+        for message, context in zip(column_data, context_column):
+            context = "" if context is None else context
+            prompt_answer = func(
+                client,
+                model=self.model,
+                prompt=self.prompt,
+                prompt_replace_string=self.prompt_replace_string,
+                context_replace_string=self.context_replace_string,
+                prompt_message=message,
+                context=context,
+                params=self.openai_params,
+            )
             processed_response = _postprocess_response(
-                prompt_answer.choices[0].text,
+                prompt_answer,
                 self.check_mode,
                 self.possible_values,
             )
@@ -88,6 +115,43 @@ class OpenAIFeature(GeneratedFeature):
         if self.display_name:
             return self.display_name.replace(" ", "_").lower()
         return self.column_name + "_" + self.feature_id
+
+
+def _completions_openai_call(
+    client,
+    model: str,
+    prompt: str,
+    prompt_replace_string: str,
+    context_replace_string: str,
+    prompt_message: str,
+    context: str,
+    params: dict,
+):
+    final_prompt = prompt.replace(context_replace_string, context).replace(prompt_replace_string, prompt_message)
+    prompt_answer = client.completions.create(model=model, prompt=final_prompt, **params)
+    return prompt_answer.choices[0].text
+
+
+def _chat_completions_openai_call(
+    client,
+    model: str,
+    prompt: str,
+    prompt_replace_string: str,
+    context_replace_string: str,
+    prompt_message: str,
+    context: str,
+    params: dict,
+):
+    final_prompt = prompt.replace(prompt_replace_string, prompt_message)
+    prompt_answer = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": context},
+            {"role": "user", "content": final_prompt},
+        ],
+        **params,
+    )
+    return prompt_answer.choices[0].message.content
 
 
 def _postprocess_response(response: str, check_mode: str, possible_values: Optional[List[str]]) -> Optional[str]:
