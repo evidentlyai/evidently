ee47f2ab485fd06a9e0f8680872a54a4088d0b20
Emeli Dral <emeli.dral@gmail.com>
2024-06-11 17:55:56 +0100

    fix linter

--- src/evidently/features/semantic_similarity_feature.py
+++ src/evidently/features/semantic_similarity_feature.py
@@ -1,7 +1,7 @@
 from typing import List
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from evidently.base_metric import ColumnName
 from evidently.base_metric import additional_feature
