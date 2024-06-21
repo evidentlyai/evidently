43085a8432adfb1dca6de9441b6cc007e52c74ae
DimaAmega <dima.amega1@gmail.com>
2024-06-21 18:20:36 +0400

    UI: feature: monitoring by time switch

--- ui/packages/evidently-ui-lib/src/routes-components/dashboard/index.tsx
+++ ui/packages/evidently-ui-lib/src/routes-components/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import { useLoaderData, useParams } from 'react-router-dom'
 import invariant from 'tiny-invariant'
-import { DashboardDateFilter } from '~/components/DashboardDateFilter'
+import { DashboardParams } from '~/components/DashboardDateFilter'
 import { loaderData } from './data'
+import { DashboardViewParamsContext } from '~/contexts/DashboardViewParams'
+import { useLocalStorage } from '~/hooks'
 import dayjs from 'dayjs'
 
 interface Props {
@@ -17,10 +19,20 @@ export const DashboardComponentTemplate = ({ Dashboard }: Props) => {
   const isShowDateFilter = data.min_timestamp !== null && data.max_timestamp !== null
   const dataRanges = { minDate: dayjs(data.min_timestamp), maxDate: dayjs(data.max_timestamp) }
 
+  const [isMonitoringByTime, setIsMonitoringByTime] = useLocalStorage('is-monitoring-by-time', true)
+
   return (
     <>
-      {isShowDateFilter && <DashboardDateFilter dataRanges={dataRanges} />}
-      <Dashboard data={data} />
+      <DashboardParams
+        dataRanges={dataRanges}
+        isShowDateFilter={isShowDateFilter}
+        isMonitoringByTime={isMonitoringByTime}
+        setIsMonitoringByTime={setIsMonitoringByTime}
+      />
+
+      <DashboardViewParamsContext.Provider value={{ isXaxisAsCategorical: !isMonitoringByTime }}>
+        <Dashboard data={data} />
+      </DashboardViewParamsContext.Provider>
     </>
   )
 }
