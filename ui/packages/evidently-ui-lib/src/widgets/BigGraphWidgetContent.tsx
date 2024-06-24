43fb8f0efde27d8348466b2bd9754efb06ab4114
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2024-06-24 20:03:04 +0400

    UI(feature): dashboard filter - hide dates (#1166)

--- ui/packages/evidently-ui-lib/src/widgets/BigGraphWidgetContent.tsx
+++ ui/packages/evidently-ui-lib/src/widgets/BigGraphWidgetContent.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import { BigGraphWidgetParams } from '~/api'
 import Plot from '~/components/Plot'
+import { useDashboardViewParams } from '~/contexts/DashboardViewParams'
 
 interface BigGraphWidgetProps extends BigGraphWidgetParams {
   widgetSize: number
 }
 
 const BigGraphWidgetContent: React.FunctionComponent<BigGraphWidgetProps> = (props) => {
+  const viewParams = useDashboardViewParams()
+
   return (
     <div>
       <Plot
@@ -15,10 +18,17 @@ const BigGraphWidgetContent: React.FunctionComponent<BigGraphWidgetProps> = (pro
         layout={{
           ...props.layout,
           title: undefined,
-          // width: (props.size.width ? props.size.width - 20 : undefined)
+          xaxis: {
+            ...props.layout?.xaxis,
+            type: viewParams?.isXaxisAsCategorical ? 'category' : undefined
+          }
         }}
         config={{ responsive: true }}
-        style={{ width: '100%', minHeight: 300 + 100 * (1 + props.widgetSize / 2), maxHeight: 400 }}
+        style={{
+          width: '100%',
+          minHeight: 300 + 100 * (1 + props.widgetSize / 2),
+          maxHeight: 400
+        }}
       />
     </div>
   )
