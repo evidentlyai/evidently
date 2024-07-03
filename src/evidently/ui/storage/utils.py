f9a524dd2415f82eeb32c8a0c8497c7999f3ab10
Mikhail Sveshnikov <mike0sv@gmail.com>
2024-07-03 18:34:15 +0400

    Pretty panels (#1182)
    * better metrics hover
    
    * add tests
    fix errors
    
    * test descriptions
    
    * small fixes
    
    * add \n for descriptions
    
    * fix \n br
    
    * add snapshot not found

--- src/evidently/ui/storage/utils.py
+++ src/evidently/ui/storage/utils.py
@@ -10,26 +10,34 @@ from evidently.core import BaseResult
 from evidently.utils import NumpyEncoder
 
 
-def iterate_obj_fields(obj: Any, paths: List[str]) -> Iterator[Tuple[str, str]]:
+def iterate_obj_fields(obj: Any, paths: List[str]) -> Iterator[Tuple[str, Any]]:
     if isinstance(obj, list):
         return
     if isinstance(obj, dict):
-        yield from (r for key, value in obj.items() for r in iterate_obj_fields(value, paths + [key]))
+        yield from (r for key, value in obj.items() for r in iterate_obj_fields(value, paths + [str(key)]))
         return
     if isinstance(obj, BaseResult) and obj.__config__.extract_as_obj:
-        yield ".".join(paths), json.dumps(obj.dict(), cls=NumpyEncoder)
+        yield ".".join(paths), obj
         return
     if isinstance(obj, BaseModel):
         yield from (
             r for name, field in obj.__fields__.items() for r in iterate_obj_fields(getattr(obj, name), paths + [name])
         )
         return
-    try:
-        value = str(float(obj))
-        yield ".".join(paths), value
-    except (TypeError, ValueError):
-        return
+    yield ".".join(paths), obj
+
+
+def iterate_obj_float_fields(obj: Any, paths: List[str]) -> Iterator[Tuple[str, str]]:
+    for path, value in iterate_obj_fields(obj, paths):
+        if isinstance(value, BaseResult) and value.__config__.extract_as_obj:
+            yield path, json.dumps(value.dict(), cls=NumpyEncoder)
+            continue
+        try:
+            value = str(float(value))
+            yield path, value
+        except (TypeError, ValueError):
+            continue
 
 
 def iterate_metric_results_fields(metric_result: MetricResult) -> Iterator[Tuple[str, str]]:
-    yield from iterate_obj_fields(metric_result, [])
+    yield from iterate_obj_float_fields(metric_result, [])
