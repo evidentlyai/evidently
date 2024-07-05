f681cb66ce03f86dd0b68e7671dd96e04171df03
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2024-07-05 14:25:31 +0200

    Support for api key in tracing init. Minor fix for tracing registrations.

--- src/evidently/tracing/__init__.py
+++ src/evidently/tracing/__init__.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 from functools import wraps
 from typing import Any
 from typing import Callable
@@ -9,8 +10,9 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-_TRACE_COLLECTOR_ADDRESS = os.getenv("EVIDENTLY_TRACE_COLLECTOR", "https://app.evidently.cloud/traces")
-_TRACE_COLLECTOR_TYPE = os.getenv("EVIDENTLY_TRACE_COLLECTOR_TYPE", "grpc")
+_TRACE_COLLECTOR_ADDRESS = os.getenv("EVIDENTLY_TRACE_COLLECTOR", "https://app.evidently.cloud")
+_TRACE_COLLECTOR_TYPE = os.getenv("EVIDENTLY_TRACE_COLLECTOR_TYPE", "http")
+_TRACE_COLLECTOR_API_KEY = os.getenv("EVIDENTLY_TRACE_COLLECTOR_API_KEY", "")
 
 
 _tracer: Optional[trace.TracerProvider] = None
@@ -25,19 +27,18 @@ def trace_event(track_args: Optional[List[str]] = None, ignore_args: Optional[Li
                     if set to [] do not capture any arguments
         ignore_args: list of arguments to ignore, if set to None - do not ignore any arguments.
     """
-    tracer = _tracer
 
     def wrapper(f: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(f)
         def func(*args, **kwargs):
             import inspect
 
-            if tracer is None:
+            if _tracer is None:
                 raise ValueError("TracerProvider not initialized, use init_tracer() or register_span_processor")
 
             sign = inspect.signature(f)
             bind = sign.bind(*args, **kwargs)
-            with tracer.start_as_current_span(f"{f.__name__}") as span:
+            with _tracer.start_as_current_span(f"{f.__name__}") as span:
                 final_args = track_args
                 if track_args is None:
                     final_args = list(sign.parameters.keys())
@@ -58,6 +59,7 @@ def register_span_processor(
     tracer_provider: TracerProvider,
     address: Optional[str] = None,
     exporter_type: Optional[str] = None,
+    api_key: Optional[str] = None,
 ) -> None:
     """
     Register Evidently telemetry tracing span processor in existing tracer provider.
@@ -65,20 +67,25 @@ def register_span_processor(
         tracer_provider: provider to register tracing in
         address: address of collector service
         exporter_type: type of exporter to use "grpc" or "http"
+        api_key: authorization api key for Evidently tracing
     """
     global _tracer  # noqa: PLW0603
 
     _address = address or _TRACE_COLLECTOR_ADDRESS
     _exporter_type = exporter_type or _TRACE_COLLECTOR_TYPE
+    _api_key = api_key or _TRACE_COLLECTOR_API_KEY
     exporter = None
     if _exporter_type == "grpc":
         from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
-        exporter = OTLPSpanExporter(_address)
+        exporter = OTLPSpanExporter(_address, headers=[] if _api_key is None else [("authorization", _api_key)])
     elif _exporter_type == "http":
         from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 
-        exporter = OTLPSpanExporter(_address)
+        exporter = OTLPSpanExporter(
+            urllib.parse.urljoin(_address, "/v1/traces"),
+            headers=dict([] if _api_key is None else [("authorization", _api_key)]),
+        )
 
     tracer_provider.add_span_processor(BatchSpanProcessor(exporter))
     _tracer = tracer_provider.get_tracer("evidently")
@@ -87,6 +94,7 @@ def register_span_processor(
 def init_tracing(
     address: Optional[str] = None,
     exporter_type: Optional[str] = None,
+    api_key: Optional[str] = None,
     *,
     as_global: bool = True,
 ) -> None:
@@ -95,6 +103,7 @@ def init_tracing(
     Args:
         address: address of collector service
         exporter_type: type of exporter to use "grpc" or "http"
+        api_key: authorization api key for Evidently tracing
         as_global: indicated when to register provider globally for opentelemetry of use local one
                    Can be useful when you don't want to mix already existing OpenTelemetry tracing with Evidently one,
                    but may require additional configuration
@@ -102,7 +111,7 @@ def init_tracing(
     global _tracer  # noqa: PLW0603
 
     _provider = TracerProvider()
-    register_span_processor(_provider, address, exporter_type)
+    register_span_processor(_provider, address, exporter_type, api_key)
 
     if as_global:
         trace.set_tracer_provider(_provider)
