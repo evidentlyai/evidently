312ba953334f53a2ca8b92464262bdb9ba1790d4
mike0sv <mike0sv@gmail.com>
2024-07-19 15:59:54 +0400

    change datasets name

--- src/evidently/ui/workspace/base.py
+++ src/evidently/ui/workspace/base.py
@@ -6,6 +6,7 @@ from typing import Union
 import pandas as pd
 
 from evidently.report import Report
+from evidently.suite.base_suite import ReportBase
 from evidently.suite.base_suite import Snapshot
 from evidently.test_suite import TestSuite
 from evidently.ui.base import Project
@@ -36,31 +37,24 @@ class WorkspaceBase(abc.ABC):
     def list_projects(self, team_id: Optional[TeamID] = None, org_id: Optional[OrgID] = None) -> List[Project]:
         raise NotImplementedError
 
-    def add_report(self, project_id: STR_UUID, report: Report, include_data: bool = False):
+    def _add_report_base(self, project_id: STR_UUID, report: ReportBase, include_data: bool = False):
         snapshot = report.to_snapshot()
         if include_data:
+            prefix = "report" if snapshot.is_report else "testsuite"
             reference, current = report.datasets()
             snapshot.links.datasets.output.current = self.add_dataset(
-                current, f"snapshot_{snapshot.id}_output_current", project_id
+                current, f"{prefix}-output-current-{snapshot.id}", project_id
             )
             snapshot.links.datasets.output.reference = self.add_dataset(
-                reference, f"snapshot_{snapshot.id}_output_reference", project_id
+                reference, f"{prefix}-output-reference-{snapshot.id}", project_id
             )
         self.add_snapshot(project_id, snapshot)
 
+    def add_report(self, project_id: STR_UUID, report: Report, include_data: bool = False):
+        self._add_report_base(project_id, report, include_data)
+
     def add_test_suite(self, project_id: STR_UUID, test_suite: TestSuite, include_data: bool = False):
-        snapshot = test_suite.to_snapshot()
-        if include_data:
-            reference, current = test_suite.datasets()
-            if current is not None:
-                snapshot.links.datasets.output.current = self.add_dataset(
-                    current, f"snapshot_{snapshot.id}_output_current", project_id
-                )
-            if reference is not None:
-                snapshot.links.datasets.output.reference = self.add_dataset(
-                    reference, f"snapshot_{snapshot.id}_output_reference", project_id
-                )
-        self.add_snapshot(project_id, snapshot)
+        self._add_report_base(project_id, test_suite, include_data)
 
     @abc.abstractmethod
     def add_snapshot(self, project_id: STR_UUID, snapshot: Snapshot):
