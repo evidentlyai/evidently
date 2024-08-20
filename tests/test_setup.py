7f3ef02367150597b0ac9954481002416485791b
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2024-08-20 13:42:48 +0200

    Support limited version in tests.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -7,9 +7,16 @@ def test_minimal_requirements():
     path = Path(__file__).parent.parent
     with open(path / "requirements.min.txt") as f:
         lines = {line.strip().split("#")[0] for line in f.readlines()}
-        min_reqs = {k.split("[")[0]: v for line in lines if line.strip() for k, v in (line.strip().split("=="),)}
+        min_reqs = {
+            k.split("[")[0]: _get_min_version(v)
+            for line in lines
+            if line.strip()
+            for k, v in (line.strip().split("=="),)
+        }
 
-    install_reqs = {k.split("[")[0]: v for r in setup_args["install_requires"] for k, v in (r.split(">="),)}
+    install_reqs = {
+        k.split("[")[0]: _get_min_version(v) for r in setup_args["install_requires"] for k, v in (r.split(">="),)
+    }
     extra = []
     wrong_version = []
     for m, v in install_reqs.items():
@@ -23,3 +30,9 @@ def test_minimal_requirements():
     assert (
         len(extra) == 0 and len(wrong_version) == 0
     ), f"install_requires has extra reqs {extra} and wrong versions of {wrong_version}"
+
+
+def _get_min_version(value):
+    if "," in value:
+        return value.split(",")[0]
+    return value
