26b1d201b247a0e5f9d58c5d053754d66daf129b
mike0sv <mike0sv@gmail.com>
2024-08-30 16:36:36 +0400

    change aliases to base uuid type

--- src/evidently/ui/type_aliases.py
+++ src/evidently/ui/type_aliases.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 from typing import Dict
 from typing import List
 from typing import NamedTuple
@@ -13,17 +14,17 @@ from evidently.tests.base_test import Test
 from evidently.tests.base_test import TestStatus
 
 BlobID = str
-UserID = uuid6.UUID
-TeamID = uuid6.UUID
-OrgID = uuid6.UUID
+UserID = uuid.UUID
+TeamID = uuid.UUID
+OrgID = uuid.UUID
 RoleID = int
-ProjectID = uuid6.UUID
-EntityID = uuid6.UUID
-SnapshotID = uuid6.UUID
-STR_UUID = Union[str, uuid6.UUID]
-PanelID = uuid6.UUID
-TabID = uuid6.UUID
-DatasetID = uuid6.UUID
+ProjectID = uuid.UUID
+EntityID = uuid.UUID
+SnapshotID = uuid.UUID
+STR_UUID = Union[str, uuid.UUID]
+PanelID = uuid.UUID
+TabID = uuid.UUID
+DatasetID = uuid.UUID
 ZERO_UUID = uuid6.UUID(int=0, version=7)
 
 
