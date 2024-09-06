2bb5e6d9fe576b6203b21617197e37867a559950
Mikhail Sveshnikov <mike0sv@gmail.com>
2024-09-06 23:16:19 +0400

    rename semantic_similarity (#1286)

--- src/evidently/descriptors/semantic_similarity.py
+++ src/evidently/descriptors/semantic_similarity.py
@@ -1,18 +1,9 @@
-from typing import List
-
 from evidently.features.generated_features import FeatureDescriptor
-from evidently.features.generated_features import GeneratedFeature
 from evidently.features.generated_features import GeneratedFeatures
-from evidently.features.generated_features import MultiColumnFeatureDescriptor
 from evidently.features.semantic_similarity_feature import SemanticSimilarityFeature
 
 
-class SemanticSimilarity(MultiColumnFeatureDescriptor):
-    def feature(self, columns: List[str]) -> GeneratedFeature:
-        return SemanticSimilarityFeature(columns=columns, display_name=self.display_name)
-
-
-class SemanticSimilarityDescriptor(FeatureDescriptor):
+class SemanticSimilarity(FeatureDescriptor):
     with_column: str
 
     def feature(self, column_name: str) -> GeneratedFeatures:
