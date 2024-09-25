9133cf6bca11b69c947c0888e873e4f1fb164d3c
Mikhail Sveshnikov <mike0sv@gmail.com>
2024-09-25 19:13:42 +0400

    make ui/base async (#1307)

--- src/evidently/ui/workspace/view.py
+++ src/evidently/ui/workspace/view.py
@@ -9,6 +9,7 @@ from evidently import ColumnMapping
 from evidently.suite.base_suite import Snapshot
 from evidently.ui.base import Project
 from evidently.ui.base import ProjectManager
+from evidently.ui.base import async_to_sync
 from evidently.ui.type_aliases import STR_UUID
 from evidently.ui.type_aliases import ZERO_UUID
 from evidently.ui.type_aliases import DatasetID
@@ -28,44 +29,50 @@ class WorkspaceView(WorkspaceBase):
         self.user_id = user_id or ZERO_UUID
 
     def create_project(self, name: str, description: Optional[str] = None, team_id: TeamID = None) -> Project:
-        return self.project_manager.create_project(
-            name, user_id=self.user_id, team_id=team_id or ZERO_UUID, description=description
+        return async_to_sync(
+            self.project_manager.create_project(
+                name, user_id=self.user_id, team_id=team_id or ZERO_UUID, description=description
+            )
         )
 
     def add_project(self, project: Project, team_id: TeamID = None) -> Project:
-        project = self.project_manager.add_project(project, user_id=self.user_id, team_id=team_id or ZERO_UUID)
+        project = async_to_sync(
+            self.project_manager.add_project(project, user_id=self.user_id, team_id=team_id or ZERO_UUID)
+        )
         return project
 
     def get_project(self, project_id: STR_UUID) -> Optional[Project]:
         if isinstance(project_id, str):
             project_id = uuid6.UUID(project_id)
-        return self.project_manager.get_project(self.user_id, project_id)
+        return async_to_sync(self.project_manager.get_project(self.user_id, project_id))
 
     def delete_project(self, project_id: STR_UUID):
         if isinstance(project_id, str):
             project_id = uuid6.UUID(project_id)
-        self.project_manager.delete_project(self.user_id, project_id)
+        async_to_sync(self.project_manager.delete_project(self.user_id, project_id))
 
     def list_projects(self, team_id: Optional[TeamID] = None, org_id: Optional[OrgID] = None) -> List[Project]:
-        return self.project_manager.list_projects(self.user_id, team_id or ZERO_UUID, org_id or ZERO_UUID)
+        return async_to_sync(
+            self.project_manager.list_projects(self.user_id, team_id or ZERO_UUID, org_id or ZERO_UUID)
+        )
 
     def add_snapshot(self, project_id: STR_UUID, snapshot: Snapshot):
         if isinstance(project_id, str):
             project_id = uuid6.UUID(project_id)
-        self.project_manager.add_snapshot(self.user_id, project_id, snapshot)
+        async_to_sync(self.project_manager.add_snapshot(self.user_id, project_id, snapshot))
 
     def delete_snapshot(self, project_id: STR_UUID, snapshot_id: STR_UUID):
         if isinstance(project_id, str):
             project_id = uuid6.UUID(project_id)
         if isinstance(snapshot_id, str):
             snapshot_id = uuid6.UUID(snapshot_id)
-        self.project_manager.delete_snapshot(self.user_id, project_id, snapshot_id)
+        async_to_sync(self.project_manager.delete_snapshot(self.user_id, project_id, snapshot_id))
 
     def search_project(
         self, project_name: str, team_id: Optional[TeamID] = None, org_id: Optional[OrgID] = None
     ) -> List[Project]:
-        return self.project_manager.search_project(
-            self.user_id, project_name, team_id or ZERO_UUID, org_id or ZERO_UUID
+        return async_to_sync(
+            self.project_manager.search_project(self.user_id, project_name, team_id or ZERO_UUID, org_id or ZERO_UUID)
         )
 
     def add_dataset(
