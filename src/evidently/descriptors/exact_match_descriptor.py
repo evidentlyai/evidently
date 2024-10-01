4f771a469fba0e3b4d8aded0e49379e2e7149fd0
Gagan <g.deepsingh1@gmail.com>
2024-10-01 01:35:33 -0600

    test cases

--- src/evidently/descriptors/exact_match_descriptor.py
+++ src/evidently/descriptors/exact_match_descriptor.py
@@ -1,3 +1,4 @@
+from evidently.features.exact_match_feature import ExactMatchFeature
 from evidently.features.generated_features import FeatureDescriptor
 from evidently.features.generated_features import GeneratedFeatures
 
@@ -9,4 +10,4 @@ class ExactMatch(FeatureDescriptor):
     with_column: str
 
     def feature(self, column_name: str) -> GeneratedFeatures:
-        return ExactMatch(columns=[column_name, self.with_column], display_name=self.display_name)
+        return ExactMatchFeature(columns=[column_name, self.with_column], display_name=self.display_name)
