0e51d686536f0a9c689bff72641fc3dda32a7d44
Jonathan Bown <Jonathan.k.bown@gmail.com>
2024-10-03 23:55:13 -0600

    Initial unit tests

--- tests/features/test_words_feature.py
+++ tests/features/test_words_feature.py
@@ -5,6 +5,7 @@ import pytest
 
 from evidently.features.words_feature import ExcludesWords
 from evidently.features.words_feature import IncludesWords
+from evidently.features.words_feature import WordMatchFeature
 from evidently.pipeline.column_mapping import ColumnMapping
 from evidently.utils.data_preprocessing import create_data_definition
 
@@ -51,3 +52,44 @@ def test_excludes_words(words: List[str], mode: str, lemmatize: bool, expected: 
         data_definition=create_data_definition(None, data, ColumnMapping()),
     )
     assert result.equals(pd.DataFrame(dict([(feature_generator._feature_column_name(), expected)])))
+
+
+input_data = [
+    "Who are you and where are my apples and grapes?",
+    "Apple is red",
+    "Grape is blue",
+]
+
+
+@pytest.mark.parametrize(
+    ["mode", "lemmatize", "expected"],
+    [
+        ("any", False, [True, True, False, False]),
+        ("all", False, [False, True, False, False]),
+        ("all", True, [False, True, True, False]),
+        ("all", False, [False, True, False, False]),
+    ],
+)
+def test_word_match(mode: str, lemmatize: bool, expected: List[bool]):
+    data = {
+        "generated": [
+            "I love eating apples and grapes.",
+            "I love apples, grapes, and oranges",
+            "Grapes, oranges, apples.",
+            "Oranges are more sour than grapes.",
+        ],
+        "expected": [
+            ["apples", "grapes", "oranges"],
+            ["grapes", "apples", "oranges"],
+            ["apple", "orange", "grape"],
+            ["orange", "sweet", "grape"],
+        ],
+    }
+    df = pd.DataFrame(data)
+    df["expected"] = df["expected"].apply(tuple)
+    feature_generator = WordMatchFeature(columns=["generated", "expected"], mode=mode, lemmatize=lemmatize)
+    result = feature_generator.generate_feature(
+        data=df,
+        data_definition=create_data_definition(None, df, ColumnMapping()),
+    )
+    assert result.equals(pd.DataFrame(dict([(feature_generator._feature_name(), expected)])))
