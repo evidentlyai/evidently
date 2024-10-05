783f75fb32bfe74e2bac7fa56a7c7b2533338473
Jonathan Bown <Jonathan.k.bown@gmail.com>
2024-10-04 22:03:08 -0600

    Update test_words_feature.py

--- tests/features/test_words_feature.py
+++ tests/features/test_words_feature.py
@@ -5,7 +5,7 @@ import pytest
 
 from evidently.features.words_feature import ExcludesWords
 from evidently.features.words_feature import IncludesWords
-from evidently.features.words_feature import WordMatchFeature
+from evidently.features.words_feature import WordMatch
 from evidently.pipeline.column_mapping import ColumnMapping
 from evidently.utils.data_preprocessing import create_data_definition
 
@@ -64,32 +64,40 @@ input_data = [
 @pytest.mark.parametrize(
     ["mode", "lemmatize", "expected"],
     [
-        ("any", False, [True, True, False, False]),
-        ("all", False, [False, True, False, False]),
-        ("all", True, [False, True, True, False]),
-        ("all", False, [False, True, False, False]),
+        ("any", False, [True, True, False, False, False]),
+        ("all", False, [False, True, False, False, False]),
+        ("any", True, [False, False, True, True, False]),
+        ("all", True, [False, False, True, False, False]),
     ],
 )
 def test_word_match(mode: str, lemmatize: bool, expected: List[bool]):
     data = {
         "generated": [
             "I love eating apples and grapes.",
-            "I love apples, grapes, and oranges",
+            "I eat apples, grapes, and oranges",
             "Grapes, oranges, apples.",
             "Oranges are more sour than grapes.",
+            "This test doesn't have the words.",
         ],
         "expected": [
             ["apples", "grapes", "oranges"],
             ["grapes", "apples", "oranges"],
             ["apple", "orange", "grape"],
             ["orange", "sweet", "grape"],
+            ["none", "of", "these"],
         ],
     }
     df = pd.DataFrame(data)
     df["expected"] = df["expected"].apply(tuple)
-    feature_generator = WordMatchFeature(columns=["generated", "expected"], mode=mode, lemmatize=lemmatize)
+    feature_generator = WordMatch(columns=["generated", "expected"], mode=mode, lemmatize=lemmatize)
     result = feature_generator.generate_feature(
         data=df,
         data_definition=create_data_definition(None, df, ColumnMapping()),
     )
     assert result.equals(pd.DataFrame(dict([(feature_generator._feature_name(), expected)])))
+
+
+def test_word_match_mode_error():
+    with pytest.raises(ValueError) as e:
+        WordMatch(columns=["generated", "expected"], mode="mode", lemmatize=True)
+        e.match("mode must be either 'all' or 'any'")
