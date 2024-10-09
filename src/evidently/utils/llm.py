79d58b0e51579b882cd71f674321ae27bd6b57f5
mike0sv <mike0sv@gmail.com>
2024-10-09 15:57:28 +0200

    wip

--- src/evidently/utils/llm.py
+++ src/evidently/utils/llm.py
@@ -1,22 +1,34 @@
+import dataclasses
 from abc import ABC
 from abc import abstractmethod
+from typing import Any
 from typing import Callable
 from typing import ClassVar
 from typing import Dict
+from typing import Iterator
 from typing import List
 from typing import Optional
+from typing import Sequence
 from typing import Tuple
 from typing import Type
 from typing import Union
 
-from evidently._pydantic_compat import PrivateAttr
+from evidently._pydantic_compat import Field
 from evidently._pydantic_compat import SecretStr
 from evidently.errors import EvidentlyError
+from evidently.features.llm_judge import Uncertainty
 from evidently.options.base import Options
 from evidently.options.option import Option
+from evidently.pydantic_utils import EvidentlyBaseModel
 
-LLMMessage = Tuple[str, str]
-LLMResponse = Dict[str, Union[str, float]]
+
+@dataclasses.dataclass
+class LLMMessage:
+    role: str
+    content: str
+
+
+LLMResponse = Dict[str, Any]
 
 
 class EvidentlyLLMError(EvidentlyError):
@@ -66,17 +78,6 @@ def get_llm_wrapper(provider: LLMProvider, model: LLMModel, options: Options) ->
     raise ValueError(f"LLM wrapper for provider {provider} model {model} not found")
 
 
-class WithLLMWrapper:
-    provider: str
-    model: str
-    _llm_wrapper: Optional[LLMWrapper] = PrivateAttr(None)
-
-    def get_llm_wrapper(self, options: Options) -> LLMWrapper:
-        if self._llm_wrapper is None:
-            self._llm_wrapper = get_llm_wrapper(self.provider, self.model, options)
-        return self._llm_wrapper
-
-
 class OpenAIKey(Option):
     api_key: Optional[SecretStr] = None
 
@@ -122,3 +123,131 @@ class LiteLLMWrapper(LLMWrapper):
         from litellm import completion
 
         return completion(model=self.model, messages=messages).choices[0].message.content
+
+
+class PromptBlock(EvidentlyBaseModel):
+    class Config:
+        alias_required = False  # fixme
+
+    def render(self) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def simple(cls, value: str):
+        return SimpleBlock(value=value)
+
+    @classmethod
+    def input(cls, placeholder_name: str = "input"):
+        return SimpleBlock(value=f"{{{placeholder_name}}}")
+
+    def anchored(self, start: str, end: str):
+        return Anchor(start=start, block=self, end=end)
+
+
+class Anchor(PromptBlock):
+    start: str
+    block: PromptBlock
+    end: str
+
+    def render(self) -> str:
+        return f"{self.start}\n{self.block.render()}\n{self.end}"
+
+
+class SimpleBlock(PromptBlock):
+    value: str
+
+    def render(self) -> str:
+        return self.value
+
+
+class OutputFormatBlock(PromptBlock):
+    def parse_response(self, response: str) -> Dict[str, str]:
+        raise NotImplementedError
+
+
+class JsonOutputFormatBlock(PromptBlock):
+    fields: Dict[str, Union[Tuple[str, str], str]]
+
+    def render(self) -> str:
+        values = []
+        example_rows = []
+        for field, descr in self.fields.items():
+            if isinstance(descr, tuple):
+                descr, field_key = descr
+            else:
+                field_key = field
+            values.append(field)
+            example_rows.append(f'"{field_key}": "{descr}"')
+
+        example_rows_str = "\n".join(example_rows)
+        return f"Return {', '.join(values)} formatted as json without formatting as follows:\n{{{{\n{example_rows_str}\n}}}}"
+
+
+class PromptTemplate(EvidentlyBaseModel):
+    class Config:
+        alias_required = False  # fixme
+
+    def get_blocks(self) -> Sequence[PromptBlock]:
+        raise NotImplementedError
+
+    def iterate(self, values: Sequence[Dict[str, str]]) -> Iterator[str]:
+        template = self.render()
+        for vals in values:
+            yield template.format(**vals)
+
+    def render(self) -> str:
+        return "\n".join(block.render() for block in self.get_blocks())
+
+    def parse(self, response: str) -> Dict[str, str]:
+        output = next((b for b in self.get_blocks() if isinstance(b, OutputFormatBlock)), None)
+        if output is None:
+            return {"": response}
+        return output.parse_response(response)
+
+
+class BinaryClassificationPromtTemplate(PromptTemplate):
+    def get_blocks(self) -> Sequence[PromptBlock]:
+        fields = {}
+        if self.include_category:
+            cat = f"{self.target_category} or {self.non_target_category}"
+            if self.uncertainty == Uncertainty.UNKNOWN:
+                cat += " or UNKNOWN"
+            fields["category"] = (cat, self.output_column)
+        if self.include_score:
+            fields["score"] = ("<score here>", self.output_score_column)
+        if self.include_reasoning:
+            fields["reasoning"] = ('"<reasoning here>"', self.output_reasoning_column)
+        return [
+            PromptBlock.simple(self.criteria),
+            PromptBlock.simple(
+                f"Classify text between {self.anchor_start} and {self.anchor_end} "
+                f"into two categories: {self.target_category} and {self.non_target_category}."
+            ),
+            PromptBlock.input().anchored(self.anchor_start, self.anchor_end),
+            PromptBlock.func(self._instructions),
+            JsonOutputFormatBlock(fields=fields),
+        ]
+
+    criteria: str = ""
+    instructions_template: str = (
+        "Use the following categories for classification:\n{__categories__}\n{__scoring__}\nThink step by step."
+    )
+    anchor_start: str = "___text_starts_here___"
+    anchor_end: str = "___text_ends_here___"
+
+    placeholders: Dict[str, str] = {}
+    target_category: str
+    non_target_category: str
+
+    uncertainty: Uncertainty = Uncertainty.UNKNOWN
+
+    include_category: bool = True
+    include_reasoning: bool = False
+    include_score: bool = False
+    score_range: Tuple[float, float] = (0.0, 1.0)
+
+    output_column: str = "category"
+    output_reasoning_column: str = "reasoning"
+    output_score_column: str = "score"
+
+    pre_messages: List[LLMMessage] = Field(default_factory=list)
