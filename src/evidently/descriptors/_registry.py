aaf64238232971639d6cf55f2a77bcddeb7998a8
Gagan Bhullar <g.deepsingh1@gmail.com>
2024-10-17 06:50:57 -0600

    ExactMatch descriptor (#1326)

--- src/evidently/descriptors/_registry.py
+++ src/evidently/descriptors/_registry.py
@@ -112,5 +112,8 @@ register_type_alias(
     "evidently:descriptor:CustomPairColumnEval",
 )
 register_type_alias(
+    FeatureDescriptor, "evidently.descriptors.exact_match_descriptor.ExactMatch", "evidently:descriptor:ExactMatch"
+)
+register_type_alias(
     FeatureDescriptor, "evidently.descriptors.is_valid_json_descriptor.IsValidJSON", "evidently:descriptor:IsValidJSON"
 )
