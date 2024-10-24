866492aa16e2c8efdd484923707edebdaceb7dc3
DimaAmega <dima.amega1@gmail.com>
2024-10-24 17:04:36 +0400

    using `@uiw/react-json-view`

---
+++ ui/packages/evidently-ui-lib/src/components/JsonView.tsx
@@ -0,0 +1,21 @@
+import JsonView, { type JsonViewProps } from '@uiw/react-json-view'
+import { githubDarkTheme } from '@uiw/react-json-view/githubDark'
+import { githubLightTheme } from '@uiw/react-json-view/githubLight'
+import { useMemo } from 'react'
+import { useThemeMode } from '~/hooks/theme'
+
+export const JsonViewThemed: React.FC<
+  React.PropsWithRef<Omit<JsonViewProps<object>, 'style' | 'displayObjectSize'>>
+> = ({ ...props }) => {
+  const mode = useThemeMode()
+
+  const theme = useMemo(
+    () => ({
+      ...(mode === 'light' ? githubLightTheme : githubDarkTheme),
+      '--w-rjv-background-color': 'transparent'
+    }),
+    [mode]
+  )
+
+  return <JsonView {...props} style={theme} displayObjectSize={false} />
+}
