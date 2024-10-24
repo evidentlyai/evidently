38132b9397f166a7731c1e8aee36192f5b7fff26
Svetlana Popova <svetleo@evidentlyai.com>
2024-10-24 13:46:26 +0200

    OpenAI error message

--- src/evidently/utils/llm/wrapper.py
+++ src/evidently/utils/llm/wrapper.py
@@ -194,8 +194,8 @@ class OpenAIWrapper(LLMWrapper):
         messages = [{"role": msg.role, "content": msg.content} for msg in messages]
         try:
             response = await self.client.chat.completions.create(model=self.model, messages=messages)  # type: ignore[arg-type]
-        except openai.OpenAIError as e:
-            raise LLMRequestError("Failed to call OpenAI complete API") from e
+        except openai.APIError as e:
+            raise LLMRequestError(f"Failed to call OpenAI complete API: {e.message}") from e
         content = response.choices[0].message.content
         assert content is not None  # todo: better error
         return content
