7b89f10b228538331fdd574cae7f861b21b8c105
Svetlana Popova <svetleo@evidentlyai.com>
2024-10-29 13:45:20 +0100

    event loop exp

--- src/evidently/utils/sync.py
+++ src/evidently/utils/sync.py
@@ -11,44 +11,46 @@ _thr = threading.Thread(target=_loop.run_forever, name="Async Runner", daemon=Tr
 
 TA = TypeVar("TA")
 
-# def async_to_sync(awaitable: Awaitable[TA]) -> TA:
-#     try:
-#         loop = asyncio.get_running_loop()
-#         # we are in sync context but inside a running loop
-#         logging.info("loop", loop)
-#         if not _thr.is_alive():
-#             logging.info("start loop", loop)
-#             _thr.start()
-#         future = asyncio.run_coroutine_threadsafe(awaitable, _loop)
-#         return future.result()
-#     except RuntimeError as e:
-#         logging.info("RuntimeError", e)
-#         logging.info("creating new loop")
-#         new_loop = asyncio.new_event_loop()
-#         asyncio.set_event_loop(new_loop)
-#         try:
-#             return new_loop.run_until_complete(awaitable)
-#         finally:
-#             new_loop.close()
-
 
 def async_to_sync(awaitable: Awaitable[TA]) -> TA:
     try:
         loop = asyncio.get_running_loop()
         # we are in sync context but inside a running loop
-        print("loop", loop)
-
-        if not loop.is_running():
-            print("loop is not running", loop)
-            new_loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(new_loop)
-            try:
-                return new_loop.run_until_complete(awaitable)
-            finally:
-                new_loop.close()
-
+        print(
+            f"loop {loop}",
+        )
+        if not _thr.is_alive():
+            print("start loop", loop)
+            # _thr.start()
         future = asyncio.run_coroutine_threadsafe(awaitable, _loop)
         return future.result()
+    except RuntimeError as e:
+        print("RuntimeError", e)
+        print("creating new loop")
+        new_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(new_loop)
+        try:
+            return new_loop.run_until_complete(awaitable)
+        finally:
+            new_loop.close()
+
+    # def async_to_sync(awaitable: Awaitable[TA]) -> TA:
+    #     try:
+    #         loop = asyncio.get_running_loop()
+    #         # we are in sync context but inside a running loop
+    #         print("loop", loop)
+    #
+    #         if not loop.is_running():
+    #             print("loop is not running", loop)
+    #             new_loop = asyncio.new_event_loop()
+    #             asyncio.set_event_loop(new_loop)
+    #             try:
+    #                 return new_loop.run_until_complete(awaitable)
+    #             finally:
+    #                 new_loop.close()
+    #
+    #         future = asyncio.run_coroutine_threadsafe(awaitable, _loop)
+    #         return future.result()
 
     except RuntimeError as e:
         print("RuntimeError, creating new_event_loop", e)
