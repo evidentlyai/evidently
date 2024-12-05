72dc7bcf605a3521fdf023c817b53364442b2fb6
DimaAmega <dima.amega1@gmail.com>
2024-12-05 06:55:20 +0400

    wip

--- ui/service/vite.config.ts
+++ ui/service/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://demo.evidentlyai.com'
+      '/api': 'https://demo.evidentlyai.com'
     }
   },
   build: {
