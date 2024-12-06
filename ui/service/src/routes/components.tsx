0aed65d59e1d84580ff98ea2009f922e7cea5308
DimaAmega <dima.amega1@gmail.com>
2024-12-06 04:53:10 +0400

    wip

--- ui/service/src/routes/components.tsx
+++ ui/service/src/routes/components.tsx
@@ -8,17 +8,28 @@ import type { GetParams } from 'evidently-ui-lib/router-utils/types'
 import { useMatch } from 'evidently-ui-lib/shared-dependencies/react-router-dom'
 import type { GetRouteByPath, Routes } from './types'
 
+type Paths = Routes['path']
+
 type RouterLinkProps<K extends Paths> = RouterLinkTemplateComponentProps & {
   to: K
   paramsToReplace: GetParams<K>
   query?: GetRouteByPath<K>['loader']['query']
 }
 
-type Paths = Routes['path']
+export const RouterLink = <K extends Paths>({ ...props }: RouterLinkProps<K>) => {
+  const searchParams =
+    props.query &&
+    new URLSearchParams(
+      Object.fromEntries(Object.entries(props.query).filter(([_, v]) => v)) as Record<
+        string,
+        string
+      >
+    )
 
-export const RouterLink = <K extends Paths>({ ...props }: RouterLinkProps<K>) => (
-  <RouterLinkTemplate {...props} to={replaceParamsInLink(props.paramsToReplace, props.to)} />
-)
+  const to = [replaceParamsInLink(props.paramsToReplace, props.to), searchParams].join('?')
+
+  return <RouterLinkTemplate {...props} to={to} />
+}
 
 export const useMatchRouter = <K extends Paths>({ path }: { path: K }) => {
   return Boolean(useMatch({ path, end: false }))
