afebfa4688723c5cd037d5ad8f9d15203f44ffd0
mike0sv <mike0sv@gmail.com>
2024-12-13 12:45:42 +0000

    imports

--- src/evidently/v2/scorers/__init__.py
+++ src/evidently/v2/scorers/__init__.py
@@ -2,20 +2,17 @@ from ._custom_scorers import CustomColumnScorer
 from ._custom_scorers import CustomScorer
 from ._text_length import TextLength
 from .generated_scorers import begins_with
-from .generated_scorers import bert_score_feature
+from .generated_scorers import bert_score
 from .generated_scorers import contains
 from .generated_scorers import contains_link
-from .generated_scorers import custom_feature
-from .generated_scorers import custom_pair_column_feature
-from .generated_scorers import custom_single_column_feature
 from .generated_scorers import does_not_contain
 from .generated_scorers import ends_with
-from .generated_scorers import exact_match_feature
+from .generated_scorers import exact_match
 from .generated_scorers import excludes_words
-from .generated_scorers import hugging_face_feature
-from .generated_scorers import hugging_face_toxicity_feature
+from .generated_scorers import hugging_face
+from .generated_scorers import hugging_face_toxicity
 from .generated_scorers import includes_words
-from .generated_scorers import is_valid_j_s_o_n
+from .generated_scorers import is_valid_json
 from .generated_scorers import is_valid_python
 from .generated_scorers import item_match
 from .generated_scorers import item_no_match
@@ -24,9 +21,9 @@ from .generated_scorers import json_schema_match
 from .generated_scorers import llm_judge
 from .generated_scorers import non_letter_character_percentage
 from .generated_scorers import oov_words_percentage
-from .generated_scorers import open_a_i_feature
+from .generated_scorers import openai
 from .generated_scorers import reg_exp
-from .generated_scorers import semantic_similarity_feature
+from .generated_scorers import semantic_similarity
 from .generated_scorers import sentence_count
 from .generated_scorers import sentiment
 from .generated_scorers import text_length
@@ -40,21 +37,18 @@ __all__ = [
     "CustomColumnScorer",
     "CustomScorer",
     "TextLength",
-    "bert_score_feature",
+    "bert_score",
     "begins_with",
     "contains",
     "contains_link",
-    "custom_feature",
-    "custom_pair_column_feature",
-    "custom_single_column_feature",
     "does_not_contain",
     "ends_with",
-    "exact_match_feature",
+    "exact_match",
     "excludes_words",
-    "hugging_face_feature",
-    "hugging_face_toxicity_feature",
+    "hugging_face",
+    "hugging_face_toxicity",
     "includes_words",
-    "is_valid_j_s_o_n",
+    "is_valid_json",
     "is_valid_python",
     "item_match",
     "item_no_match",
@@ -63,9 +57,9 @@ __all__ = [
     "llm_judge",
     "non_letter_character_percentage",
     "oov_words_percentage",
-    "open_a_i_feature",
+    "openai",
     "reg_exp",
-    "semantic_similarity_feature",
+    "semantic_similarity",
     "sentence_count",
     "sentiment",
     "text_length",
