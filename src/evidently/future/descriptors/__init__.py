6355f186716fd886b110c6cbb4bc041464043c23
mike0sv <mike0sv@gmail.com>
2025-01-02 15:38:16 +0000

    fix init

--- src/evidently/future/descriptors/__init__.py
+++ src/evidently/future/descriptors/__init__.py
@@ -1,73 +1,71 @@
 from ._custom_descriptors import CustomColumnDescriptor
 from ._custom_descriptors import CustomDescriptor
 from ._text_length import TextLength
-from .generated_descriptors import begins_with
-from .generated_descriptors import bert_score
-from .generated_descriptors import contains
-from .generated_descriptors import contains_link
-from .generated_descriptors import does_not_contain
-from .generated_descriptors import ends_with
-from .generated_descriptors import exact_match
-from .generated_descriptors import excludes_words
-from .generated_descriptors import hugging_face
-from .generated_descriptors import hugging_face_toxicity
-from .generated_descriptors import includes_words
-from .generated_descriptors import is_valid_json
-from .generated_descriptors import is_valid_python
-from .generated_descriptors import is_valid_sql
-from .generated_descriptors import item_match
-from .generated_descriptors import item_no_match
-from .generated_descriptors import json_match
-from .generated_descriptors import json_schema_match
-from .generated_descriptors import llm_judge
-from .generated_descriptors import non_letter_character_percentage
-from .generated_descriptors import oov_words_percentage
-from .generated_descriptors import openai
-from .generated_descriptors import reg_exp
-from .generated_descriptors import semantic_similarity
-from .generated_descriptors import sentence_count
-from .generated_descriptors import sentiment
-from .generated_descriptors import text_length
-from .generated_descriptors import trigger_words_present
-from .generated_descriptors import word_count
-from .generated_descriptors import word_match
-from .generated_descriptors import word_no_match
-from .generated_descriptors import words_presence
+from .generated_descriptors import BeginsWith
+from .generated_descriptors import BERTScore
+from .generated_descriptors import Contains
+from .generated_descriptors import ContainsLink
+from .generated_descriptors import DoesNotContain
+from .generated_descriptors import EndsWith
+from .generated_descriptors import ExactMatch
+from .generated_descriptors import ExcludesWords
+from .generated_descriptors import HuggingFace
+from .generated_descriptors import HuggingFaceToxicity
+from .generated_descriptors import IncludesWords
+from .generated_descriptors import IsValidJSON
+from .generated_descriptors import IsValidPython
+from .generated_descriptors import IsValidSQL
+from .generated_descriptors import ItemMatch
+from .generated_descriptors import ItemNoMatch
+from .generated_descriptors import JSONMatch
+from .generated_descriptors import JSONSchemaMatch
+from .generated_descriptors import LLMJudge
+from .generated_descriptors import NonLetterCharacterPercentage
+from .generated_descriptors import OOVWordsPercentage
+from .generated_descriptors import OpenAI
+from .generated_descriptors import RegExp
+from .generated_descriptors import SemanticSimilarity
+from .generated_descriptors import SentenceCount
+from .generated_descriptors import Sentiment
+from .generated_descriptors import TriggerWordsPresent
+from .generated_descriptors import WordCount
+from .generated_descriptors import WordMatch
+from .generated_descriptors import WordNoMatch
+from .generated_descriptors import WordsPresence
 
 __all__ = [
     "CustomColumnDescriptor",
     "CustomDescriptor",
     "TextLength",
-    "bert_score",
-    "begins_with",
-    "contains",
-    "contains_link",
-    "does_not_contain",
-    "ends_with",
-    "exact_match",
-    "excludes_words",
-    "hugging_face",
-    "hugging_face_toxicity",
-    "includes_words",
-    "is_valid_json",
-    "is_valid_python",
-    "is_valid_sql",
-    "item_match",
-    "item_no_match",
-    "json_match",
-    "json_schema_match",
-    "llm_judge",
-    "non_letter_character_percentage",
-    "oov_words_percentage",
-    "openai",
-    "reg_exp",
-    "semantic_similarity",
-    "sentence_count",
-    "sentiment",
-    "text_length",
-    "trigger_words_present",
-    "word_count",
-    "word_match",
-    "word_no_match",
-    "words_presence",
+    "BERTScore",
+    "BeginsWith",
+    "Contains",
+    "ContainsLink",
+    "DoesNotContain",
+    "EndsWith",
+    "ExactMatch",
+    "ExcludesWords",
+    "HuggingFace",
+    "HuggingFaceToxicity",
+    "IncludesWords",
+    "IsValidJSON",
+    "IsValidPython",
+    "IsValidSQL",
+    "ItemMatch",
+    "ItemNoMatch",
+    "JSONMatch",
+    "JSONSchemaMatch",
+    "LLMJudge",
+    "NonLetterCharacterPercentage",
+    "OOVWordsPercentage",
+    "OpenAI",
+    "RegExp",
+    "SemanticSimilarity",
+    "SentenceCount",
+    "Sentiment",
+    "TriggerWordsPresent",
+    "WordCount",
+    "WordMatch",
+    "WordNoMatch",
+    "WordsPresence",
 ]
