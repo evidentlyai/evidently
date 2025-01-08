b50eceb865281c9eacbceed0406ac044daee5141
mike0sv <mike0sv@gmail.com>
2025-01-08 19:11:19 +0000

    add llm descriptors

--- src/evidently/future/descriptors/_generate_descriptors.py
+++ src/evidently/future/descriptors/_generate_descriptors.py
@@ -10,11 +10,13 @@ from typing import Type
 
 from evidently import ColumnType
 from evidently._pydantic_compat import import_string
+from evidently.descriptors.llm_judges import BaseLLMEval
 from evidently.features.custom_feature import CustomFeature
 from evidently.features.custom_feature import CustomPairColumnFeature
 from evidently.features.custom_feature import CustomSingleColumnFeature
 from evidently.features.generated_features import GeneratedFeatures
 from evidently.features.llm_judge import BaseLLMPromptTemplate
+from evidently.features.llm_judge import Uncertainty
 from evidently.future.datasets import FeatureDescriptor
 from evidently.pydantic_utils import TYPE_ALIASES
 
@@ -24,6 +26,7 @@ REPLACES = {
     "pandas.core.frame.DataFrame": "DataFrame",
     "evidently.utils.data_preprocessing.DataDefinition": "DataDefinition",
     "pandas.core.series.Series": "Series",
+    "evidently.features.llm_judge.Uncertainty": "Uncertainty",
 }
 
 NAME_MAPPING = {
@@ -111,6 +114,37 @@ class {name}(FeatureDescriptor):
     return res, name
 
 
+def create_llm_descriptor_functions(feature_class: Type[BaseLLMEval]):
+    class_name = feature_class.__name__
+    name = class_name
+    # name = cmpx.lower() + re.sub(r"(?<!^)(?=[A-Z])", "_", class_name[len(cmpx) :]).lower()
+    name = NAME_MAPPING.get(name, name)
+    if name.endswith("Feature"):
+        name = name[: -len("Feature")]
+
+    args, kwargs = get_args_kwargs(feature_class)
+    kwargs["alias"] = ("Optional[str]", "None")
+    kwargs.pop("display_name", None)
+    args_str = ", ".join(f"{a}: {t}" for a, t in args.items())
+    if len(kwargs) > 0:
+        kwargs_str = ", ".join(f"{a}: {t} = {d}" for a, (t, d) in kwargs.items())
+        if len(args_str) > 0:
+            kwargs_str = ", " + kwargs_str
+    else:
+        kwargs_str = ""
+    class_args = ", ".join(f"{k}={k}" for k in chain(args, kwargs) if k != "alias")
+    res = f"""
+class {name}(FeatureDescriptor):
+    def __init__(self, column_name: str, {args_str}{kwargs_str}):
+        from {feature_class.__module__} import {feature_class.__name__} as {feature_class.__name__}V1
+        feature = {class_name}V1({class_args}).feature(column_name)
+        super().__init__(feature, alias=alias)
+    """
+    for substr, repl in REPLACES.items():
+        res = res.replace(substr, repl)
+    return res, name
+
+
 def main():
     for (base_class, _), classpath in list(sorted(TYPE_ALIASES.items(), key=lambda x: x[0][1])):
         if base_class is GeneratedFeatures:
@@ -119,15 +153,7 @@ def main():
 
     srcs = []
     fnames = []
-    imports: List[Type] = [
-        FeatureDescriptor,
-        ColumnType,
-        BaseLLMPromptTemplate,
-        Any,
-        List,
-        Optional,
-        Dict,
-    ]
+    imports: List[Type] = [FeatureDescriptor, ColumnType, BaseLLMPromptTemplate, Any, List, Optional, Dict, Uncertainty]
     for feature_class in sorted(subtypes__, key=lambda x: x.__name__):
         if inspect.isabstract(feature_class):
             continue
@@ -137,6 +163,10 @@ def main():
         fnames.append(fname)
         srcs.append(src)
         # imports.append(feature_class)
+    for llm_feature_class in sorted(BaseLLMEval.__subtypes__(), key=lambda x: x.__name__):
+        src, fname = create_llm_descriptor_functions(llm_feature_class)
+        fnames.append(fname)
+        srcs.append(src)
     with open(Path(__file__).parent / SOURCE_FILE, "w") as f:
         f.write("\n".join(f"from {t.__module__} import {t.__name__}" for t in imports) + "\n\n")
         f.write("\n\n".join(srcs))
