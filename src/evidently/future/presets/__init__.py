8fd49c274928e9ad0feec15f1e4a6c3df343587b
Vyacheslav Morov <Liraim@users.noreply.github.com>
2025-01-21 22:45:43 +0000

    Add ClassificationQuality render and ClassificationPreset. (#1438)

--- src/evidently/future/presets/__init__.py
+++ src/evidently/future/presets/__init__.py
@@ -1,4 +1,5 @@
 from .classification import ClassificationDummyQuality
+from .classification import ClassificationPreset
 from .classification import ClassificationQuality
 from .classification import ClassificationQualityByLabel
 from .dataset_stats import DatasetStats
@@ -12,6 +13,7 @@ from .regression import RegressionQuality
 
 __all__ = [
     "ClassificationDummyQuality",
+    "ClassificationPreset",
     "ClassificationQuality",
     "ClassificationQualityByLabel",
     "ValueStats",
--- src/evidently/future/presets/classification.py
+++ src/evidently/future/presets/classification.py
@@ -25,13 +25,28 @@ from evidently.future.metrics.classification import DummyF1Score
 from evidently.future.metrics.classification import DummyPrecision
 from evidently.future.metrics.classification import DummyRecall
 from evidently.future.report import Context
+from evidently.metrics import ClassificationConfusionMatrix
 from evidently.metrics import ClassificationDummyMetric
+from evidently.metrics import ClassificationPRCurve
+from evidently.metrics import ClassificationPRTable
 from evidently.metrics import ClassificationQualityByClass
 from evidently.metrics import ClassificationQualityMetric
 from evidently.model.widget import BaseWidgetInfo
 
 
 class ClassificationQuality(MetricContainer):
+    def __init__(
+        self,
+        probas_threshold: Optional[float] = None,
+        conf_matrix: bool = False,
+        pr_curve: bool = False,
+        pr_table: bool = False,
+    ):
+        self._probas_threshold = probas_threshold
+        self._conf_matrix = conf_matrix
+        self._pr_curve = pr_curve
+        self._pr_table = pr_table
+
     def generate_metrics(self, context: "Context") -> List[Metric]:
         classification = context.data_definition.get_classification("default")
         if classification is None:
@@ -41,44 +56,52 @@ class ClassificationQuality(MetricContainer):
 
         if isinstance(classification, BinaryClassification):
             metrics = [
-                Accuracy(),
-                Precision(),
-                Recall(),
-                F1Score(),
+                Accuracy(probas_threshold=self._probas_threshold),
+                Precision(probas_threshold=self._probas_threshold),
+                Recall(probas_threshold=self._probas_threshold),
+                F1Score(probas_threshold=self._probas_threshold),
             ]
             if classification.prediction_probas is not None:
                 metrics.extend(
                     [
-                        RocAuc(),
-                        LogLoss(),
+                        RocAuc(probas_threshold=self._probas_threshold),
+                        LogLoss(probas_threshold=self._probas_threshold),
                     ]
                 )
             metrics.extend(
                 [
-                    TPR(),
-                    TNR(),
-                    FPR(),
-                    FNR(),
+                    TPR(probas_threshold=self._probas_threshold),
+                    TNR(probas_threshold=self._probas_threshold),
+                    FPR(probas_threshold=self._probas_threshold),
+                    FNR(probas_threshold=self._probas_threshold),
                 ]
             )
         else:
             metrics = [
-                Accuracy(),
-                Precision(),
-                Recall(),
-                F1Score(),
+                Accuracy(probas_threshold=self._probas_threshold),
+                Precision(probas_threshold=self._probas_threshold),
+                Recall(probas_threshold=self._probas_threshold),
+                F1Score(probas_threshold=self._probas_threshold),
             ]
             if classification.prediction_probas is not None:
                 metrics.extend(
                     [
-                        RocAuc(),
-                        LogLoss(),
+                        RocAuc(probas_threshold=self._probas_threshold),
+                        LogLoss(probas_threshold=self._probas_threshold),
                     ]
                 )
         return metrics
 
     def render(self, context: "Context", results: Dict[MetricId, MetricResult]) -> List[BaseWidgetInfo]:
-        _, render = context.get_legacy_metric(ClassificationQualityMetric())
+        _, render = context.get_legacy_metric(ClassificationQualityMetric(probas_threshold=self._probas_threshold))
+        if self._conf_matrix:
+            render += context.get_legacy_metric(ClassificationConfusionMatrix(probas_threshold=self._probas_threshold))[
+                1
+            ]
+        if self._pr_curve:
+            render += context.get_legacy_metric(ClassificationPRCurve(probas_threshold=self._probas_threshold))[1]
+        if self._pr_table:
+            render += context.get_legacy_metric(ClassificationPRTable(probas_threshold=self._probas_threshold))[1]
         return render
 
 
@@ -117,3 +140,35 @@ class ClassificationDummyQuality(MetricContainer):
     def render(self, context: "Context", results: Dict[MetricId, MetricResult]) -> List[BaseWidgetInfo]:
         _, widgets = context.get_legacy_metric(ClassificationDummyMetric(self._probas_threshold, self._k))
         return widgets
+
+
+class ClassificationPreset(MetricContainer):
+    def __init__(self, probas_threshold: Optional[float] = None):
+        self._probas_threshold = probas_threshold
+        self._quality = ClassificationQuality(
+            probas_threshold=probas_threshold,
+            conf_matrix=True,
+            pr_curve=True,
+            pr_table=True,
+        )
+        self._quality_by_label = ClassificationQualityByLabel(probas_threshold=probas_threshold)
+        self._roc_auc: Optional[RocAuc] = None
+
+    def generate_metrics(self, context: "Context") -> List[Metric]:
+        classification = context.data_definition.get_classification("default")
+        if classification is None:
+            raise ValueError("Cannot use ClassificationPreset without a classification configration")
+        if classification.prediction_probas is not None:
+            self._roc_auc = RocAuc()
+        return (
+            self._quality.metrics(context)
+            + self._quality_by_label.metrics(context)
+            + ([] if self._roc_auc is None else [RocAuc(probas_threshold=self._probas_threshold)])
+        )
+
+    def render(self, context: "Context", results: Dict[MetricId, MetricResult]) -> List[BaseWidgetInfo]:
+        return (
+            self._quality.render(context, results)
+            + self._quality_by_label.render(context, results)
+            + ([] if self._roc_auc is None else context.get_metric_result(self._roc_auc).widget)
+        )
