37c5dae57106d9cf72f6a10c6b8948dec5411c3c
DimaAmega <dima.amega1@gmail.com>
2025-01-21 15:53:37 +0400

    wip

--- ui/packages/evidently-ui-lib/src/router-utils/utils.tsx
+++ ui/packages/evidently-ui-lib/src/router-utils/utils.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'react-router-dom'
 import { makeRouteUrl } from '~/router-utils/router-builder'
 import type { GetLinkParamsByPathOnly } from '~/router-utils/types'
 
-export const CreateRedirect = <Paths extends string>() => {
+export const createRedirect = <Paths extends string>() => {
   const _redirect = <K extends Paths>({ to, paramsToReplace = {} }: GetLinkParamsByPathOnly<K>) =>
     redirect(makeRouteUrl({ paramsToReplace, path: to }))
 
