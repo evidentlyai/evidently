72515a223d8a06d0355329a19772935f48406243
mike0sv <mike0sv@gmail.com>
2025-02-04 01:40:45 +0000

    fix registry import

--- src/evidently/__init__.py
+++ src/evidently/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+from . import _registry
 from ._version import __version__
 from ._version import version_info
 from .core import ColumnType
@@ -7,4 +8,12 @@ from .nbextension import _jupyter_nbextension_paths
 from .pipeline.column_mapping import ColumnMapping
 from .pipeline.column_mapping import TaskType
 
-__all__ = ["__version__", "version_info", "_jupyter_nbextension_paths", "ColumnMapping", "TaskType", "ColumnType"]
+__all__ = [
+    "__version__",
+    "version_info",
+    "_jupyter_nbextension_paths",
+    "ColumnMapping",
+    "TaskType",
+    "ColumnType",
+    "_registry",
+]
