d5258df9c5ef3f0429e20a87c5d2ed6adc684dbd
DimaAmega <dima.amega1@gmail.com>
2025-02-14 18:55:25 +0400

    fix crumb

--- ui/service/src/routes/src/test-suites-layout/test-suites-layout-main.tsx
+++ ui/service/src/routes/src/test-suites-layout/test-suites-layout-main.tsx
@@ -6,6 +6,6 @@ import type { CrumbDefinition } from 'evidently-ui-lib/router-utils/router-build
 
 export const currentRoutePath = '/projects/:projectId/test-suites'
 
-const crumb: CrumbDefinition = { title: 'Test suites' }
+const crumb: CrumbDefinition = { title: 'Test Suites' }
 
 export const handle = { crumb }
