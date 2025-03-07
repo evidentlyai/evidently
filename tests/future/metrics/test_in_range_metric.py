aca1798d1352d05762e6a6ab9bcfe07571a7fdc9
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2025-03-07 13:03:50 +0100

    Add support for smart_union in all metrics config.

---
+++ tests/future/metrics/test_in_range_metric.py
@@ -0,0 +1,7 @@
+from evidently.future.metrics import InRangeValueCount
+
+
+def test_in_range_metric():
+    metric = InRangeValueCount(column="a", left=0.5, right=1)
+    assert metric.left == 0.5
+    assert metric.right == 1
