f292ce831f9ff13bd60818f5a24ee2c93b24cbd2
mike0sv <mike0sv@gmail.com>
2025-03-26 16:24:31 +0000

    multiclass llm eval

---
+++ src/evidently/descriptors/llm_judges_multiclass.py
@@ -0,0 +1,205 @@
+from typing import ClassVar
+from typing import Dict
+from typing import List
+from typing import Literal
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
+from typing import Union
+
+import pandas as pd
+
+from evidently._pydantic_compat import Field
+from evidently.core import ColumnType
+from evidently.descriptors.llm_judges import BaseLLMEval
+from evidently.features.llm_judge import BaseLLMPromptTemplate
+from evidently.features.llm_judge import Uncertainty
+from evidently.pydantic_utils import EnumValueMixin
+from evidently.pydantic_utils import autoregister
+from evidently.utils.llm.base import LLMMessage
+from evidently.utils.llm.prompts import PromptBlock
+
+
+@autoregister
+class MulticlassClassificationPromptTemplate(BaseLLMPromptTemplate, EnumValueMixin):
+    class Config:
+        type_alias = "evidently:prompt_template:MulticlassClassificationPromptTemplate"
+
+    criteria: str = ""
+    instructions_template: str = (
+        "Use the following categories for classification:\n{__categories__}\n{__scoring__}\nThink step by step."
+    )
+
+    anchor_start: str = "___text_starts_here___"
+    anchor_end: str = "___text_ends_here___"
+    uncertainty: Union[Literal["UNKNOWN"], str] = "UNKNOWN"
+
+    category_criteria: Dict[str, str] = {}
+
+    include_category: bool = True
+    include_reasoning: bool = False
+    include_score: bool = False
+    score_range: Tuple[float, float] = (0.0, 1.0)
+
+    output_column: str = "category"
+    output_reasoning_column: str = "reasoning"
+    output_score_column_prefix: str = "score"
+
+    pre_messages: List[LLMMessage] = Field(default_factory=list)
+
+    def get_blocks(self) -> Sequence[PromptBlock]:
+        fields: Dict[str, Tuple[str, str]] = {}
+        if self.include_category:
+            cat = " or ".join(self.category_criteria.keys())
+            if self.uncertainty == Uncertainty.UNKNOWN:
+                cat += " or UNKNOWN"
+            fields["category"] = (cat, self.output_column)
+        if self.include_score:
+            fields.update(
+                {
+                    f"score_{cat}": (f"<score for {cat} here>", self._score_column(cat))
+                    for cat in self.category_criteria.keys()
+                }
+            )
+        if self.include_reasoning:
+            fields["reasoning"] = ("<reasoning here>", self.output_reasoning_column)
+        return [
+            PromptBlock.simple(self.criteria),
+            PromptBlock.simple(
+                f"Classify text between {self.anchor_start} and {self.anchor_end} "
+                f"into categories: " + " or ".join(self.category_criteria.keys()) + "."
+            ),
+            PromptBlock.input().anchored(self.anchor_start, self.anchor_end),
+            PromptBlock.simple(self._instructions()),
+            PromptBlock.json_output(**fields),
+        ]
+
+    def _score_column(self, category: str) -> str:
+        return f"{self.output_score_column_prefix}_{category}"
+
+    def list_output_columns(self) -> List[str]:
+        result = []
+        if self.include_category:
+            result.append(self.output_column)
+        if self.include_score:
+            result.extend(self._score_column(cat) for cat in self.category_criteria.keys())
+        if self.include_reasoning:
+            result.append(self.output_reasoning_column)
+        return result
+
+    def get_main_output_column(self) -> str:
+        return self.output_column
+
+    def get_type(self, subcolumn: Optional[str]) -> ColumnType:
+        if subcolumn == self.output_reasoning_column:
+            return ColumnType.Text
+        if subcolumn == self.output_column or subcolumn is None:
+            return ColumnType.Categorical
+        if subcolumn.startswith(self.output_score_column_prefix):
+            return ColumnType.Numerical
+        raise ValueError(f"Unknown subcolumn {subcolumn}")
+
+    def _instructions(self):
+        categories = (
+            (
+                "\n".join(f"{cat}: {crit}" for cat, crit in self.category_criteria.items())
+                + "\n"
+                + f"{self._uncertainty_class()}: use this category only if the information provided "
+                f"is not sufficient to make a clear determination\n"
+            )
+            if self.include_category
+            else ""
+        )
+        lower, upper = self.score_range
+        scoring = (f"For each category, score text in range from {lower} to {upper}") if self.include_score else ""
+        return self.instructions_template.format(__categories__=categories, __scoring__=scoring)
+
+    def _uncertainty_class(self):
+        if self.uncertainty.upper() == "UNKNOWN":
+            return "UNKNOWN"
+        if self.uncertainty not in self.category_criteria:
+            raise ValueError(f"Unknown uncertainty value: {self.uncertainty}")
+        return self.uncertainty
+
+    def get_messages(self, values, template: Optional[str] = None) -> List[LLMMessage]:
+        return [*self.pre_messages, *super().get_messages(values, template)]
+
+
+class MulticlassClassificationLLMEval(BaseLLMEval):
+    class Config:
+        type_alias = "evidently:descriptor:MulticlassClassificationLLMEval"
+
+    template: ClassVar[MulticlassClassificationPromptTemplate]
+    include_category: Optional[bool] = None
+    include_score: Optional[bool] = None
+    include_reasoning: Optional[bool] = None
+    uncertainty: Optional[Uncertainty] = None
+
+    def get_template(self) -> MulticlassClassificationPromptTemplate:
+        update = {
+            k: getattr(self, k)
+            for k in ("include_category", "include_score", "include_reasoning", "uncertainty")
+            if getattr(self, k) is not None
+        }
+        return self.template.update(**update)
+
+    def get_subcolumn(self) -> Optional[str]:
+        t = self.get_template()
+        if t.include_category:
+            return self.template.output_column
+        if t.include_score:
+            return self.template._score_column(next(iter(self.template.category_criteria.keys())))
+        return None
+
+
+class RelevanceLLMEval(MulticlassClassificationLLMEval):
+    class Config:
+        type_alias = "evidently:descriptor:RelevanceLLMEval"
+
+    name: ClassVar = "Relevance"
+    template: ClassVar = MulticlassClassificationPromptTemplate(
+        criteria="""You are given a question and an answer.
+Classify the answer into one of the following categories based on how well it responds to the question.""",
+        category_criteria={
+            "Irrelevant": "The answer does not address the question or is completely off-topic.",
+            "Partially Relevant": "The answer somewhat addresses the question but misses key details or only answers part of it.",
+            "Relevant": "The answer fully addresses the question in a clear and appropriate way.",
+        },
+        uncertainty="unknown",
+        include_reasoning=True,
+        pre_messages=[LLMMessage.system("TODO")],
+    )
+    include_score = True
+    provider = "openai"
+    model = "gpt-4o-mini"
+
+
+class BrandSafetyLLMEval(MulticlassClassificationLLMEval):
+    class Config:
+        type_alias = "evidently:descriptor:BrandSafetyLLMEval"
+
+    name: ClassVar = "Brand Safety"
+    template: ClassVar = MulticlassClassificationPromptTemplate(
+        criteria="""You are given a brand safety guideline and a piece of text (such as a customer response, post, or message).
+Classify the text based on how well it complies with the brand’s safety and tone guidelines using the following categories""",  # noqa: E501
+        category_criteria={
+            "Fully Compliant": "The text completely aligns with the brand’s safety and tone guidelines. It is appropriate in content, respectful in tone, and consistent with brand values.",  # noqa: E501
+            "Partially Compliant": "The text does not directly violate any major rules, but there are minor issues with tone, phrasing, or alignment that may raise concerns or need slight adjustment.",  # noqa: E501
+            "Incompliant": "The text clearly violates the brand’s safety or tone guidelines. It may include offensive, harmful, or inappropriate content, or be significantly off-brand in tone.",  # noqa: E501
+        },
+        uncertainty="unknown",
+        pre_messages=[LLMMessage.system("TODO")],
+    )
+
+    provider = "openai"
+    model = "gpt-4o-mini"
+
+
+if __name__ == "__main__":
+    data = pd.DataFrame({"col": ["AAAAAA", "BBBBBB"]})
+    # for msg in RelevanceLLMEval().get_template().iterate_messages(data, {"col": "input"}):
+    #     print(msg)
+    print(RelevanceLLMEval().get_template().get_template())
+    # for msg in BrandSafetyLLMEval().get_template().iterate_messages(data, {"col": "input"}):
+    #     print(msg)
+    print(BrandSafetyLLMEval().get_template().get_template())
