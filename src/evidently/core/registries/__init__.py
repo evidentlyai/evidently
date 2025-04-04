da13ab4f6cd6a009a05c3b99aacf1259c3bd1ba3
mike0sv <mike0sv@gmail.com>
2025-04-04 19:53:02 +0100

    registires

--- src/evidently/core/registries/__init__.py
+++ src/evidently/core/registries/__init__.py
@@ -4,6 +4,5 @@ from . import metric_results
 from . import metric_tests
 from . import metrics
 from . import presets
-from . import tests
 
-__all__ = ["descriptors", "tests", "presets", "metrics", "metric_tests", "metric_results", "bound_tests"]
+__all__ = ["descriptors", "presets", "metrics", "metric_tests", "metric_results", "bound_tests"]
