4df4d7408b13b63780014eb54a92be66cfc1fe98
Mikhail Sveshnikov <mike0sv@gmail.com>
2025-04-11 17:21:35 +0100

    local workspace (#1549)
    * local workspace
    
    * hm
    
    * local workspace
    
    * merge and fix lint
    
    * fix tests
    
    * oops

---
+++ src/evidently/ui/storage/local/base.py
@@ -0,0 +1,88 @@
+import json
+import posixpath
+from typing import List
+
+from evidently._pydantic_compat import BaseModel
+from evidently._pydantic_compat import parse_obj_as
+from evidently.core.serialization import SnapshotModel
+from evidently.legacy.ui.storage.local.base import FSLocation
+from evidently.legacy.ui.type_aliases import STR_UUID
+from evidently.legacy.ui.type_aliases import ProjectID
+from evidently.legacy.ui.type_aliases import SnapshotID
+from evidently.legacy.utils import NumpyEncoder
+from evidently.ui.models import DashboardModel
+from evidently.ui.models import ProjectModel
+
+DOT_JSON = ".json"
+
+PROJECT_FILE_NAME = "metadata.json"
+SNAPSHOTS_DIR_NAME = "snapshots"
+
+
+class ProjectWithDashboards(BaseModel):
+    project: ProjectModel
+    dashboard: DashboardModel
+
+
+class LocalState:
+    def __init__(self, path: str):
+        self.path = path
+        self.location = FSLocation(base_path=path)
+
+    def _project_dir(self, project_id: STR_UUID) -> str:
+        return str(project_id)
+
+    def _project_path(self, project_id: STR_UUID) -> str:
+        return posixpath.join(self._project_dir(project_id), PROJECT_FILE_NAME)
+
+    def _snapshot_dir(self, project_id: STR_UUID) -> str:
+        return posixpath.join(self._project_dir(project_id), SNAPSHOTS_DIR_NAME)
+
+    def _snapshot_path(self, project_id: STR_UUID, snapshot_id: STR_UUID) -> str:
+        return posixpath.join(self._snapshot_dir(project_id), str(snapshot_id) + DOT_JSON)
+
+    def read_project(self, project_id: STR_UUID) -> ProjectWithDashboards:
+        with self.location.open(self._project_path(project_id)) as f:
+            return parse_obj_as(ProjectWithDashboards, json.load(f))
+
+    def write_project(self, project: ProjectModel) -> ProjectWithDashboards:
+        self.location.makedirs(str(project.id))
+        if self.location.exists(self._project_path(project.id)):
+            data = self.read_project(project.id)
+            data.project = project
+        else:
+            data = ProjectWithDashboards(project=project, dashboard=DashboardModel(tabs=[], panels=[]))
+        with self.location.open(self._project_path(project.id), "w") as f:
+            json.dump(data.dict(), f, cls=NumpyEncoder, indent=2)
+        return data
+
+    def write_snapshot(self, project_id: STR_UUID, snapshot_id: STR_UUID, snapshot: SnapshotModel):
+        self.location.makedirs(self._snapshot_dir(project_id))
+        with self.location.open(self._snapshot_path(project_id, snapshot_id), "w") as f:
+            json.dump(snapshot.dict(), f, cls=NumpyEncoder)
+
+    def read_snapshot(self, project_id: STR_UUID, snapshot_id: STR_UUID) -> SnapshotModel:
+        with self.location.open(self._snapshot_path(project_id, snapshot_id)) as f:
+            return parse_obj_as(SnapshotModel, json.load(f))
+
+    def list_projects(self) -> List[ProjectID]:
+        return [ProjectID(p) for p in self.location.listdir(".")]
+
+    def list_snapshots(self, project_id: STR_UUID) -> List[SnapshotID]:
+        return [SnapshotID(s[: -len(DOT_JSON)]) for s in self.location.listdir(self._snapshot_dir(project_id))]
+
+    def delete_project(self, project_id: STR_UUID):
+        self.location.rmtree(str(project_id))
+
+    def delete_snapshot(self, project_id: STR_UUID, snapshot_id: STR_UUID):
+        self.location.rmtree(self._snapshot_path(project_id, snapshot_id))
+
+    def write_dashboard(self, project_id: STR_UUID, dashboard: DashboardModel):
+        project = self.read_project(project_id)
+        project.dashboard = dashboard
+        with self.location.open(self._project_path(project_id), "w") as f:
+            json.dump(project.dict(), f, cls=NumpyEncoder, indent=2)
+
+    def read_dashboard(self, project_id: STR_UUID) -> DashboardModel:
+        project = self.read_project(project_id)
+        return project.dashboard
---
+++ tests/future/test_ui/test_workspace/test_local.py
@@ -0,0 +1,91 @@
+import datetime
+
+import pytest
+import uuid6
+
+from evidently.core.report import Report
+from evidently.core.report import Snapshot
+from evidently.legacy.core import new_id
+from evidently.ui.models import DashboardModel
+from evidently.ui.models import DashboardPanelPlot
+from evidently.ui.models import ProjectModel
+from evidently.ui.workspace import Workspace
+from evidently.ui.workspace import WorkspaceBase
+
+
+@pytest.fixture
+def workspace(tmpdir):
+    return Workspace(str(tmpdir))
+
+
+@pytest.fixture
+def mock_project():
+    return ProjectModel(
+        id=uuid6.UUID(int=1, version=7),
+        name="mock project",
+    )
+
+
+@pytest.fixture
+def mock_snapshot():
+    report = Report(metrics=[])
+    snapshot = Snapshot(report, datetime.datetime.now(), metadata={}, tags=[])
+    snapshot._widgets = []
+    return snapshot
+
+
+@pytest.fixture
+def mock_dashboard():
+    return DashboardModel(
+        tabs=[],
+        panels=[
+            DashboardPanelPlot(id=new_id(), title="mock panel", subtitle=None, size="full", values=[], plot_params={})
+        ],
+    )
+
+
+def test_add_get_project(workspace: WorkspaceBase, mock_project: ProjectModel):
+    workspace.add_project(mock_project)
+    assert workspace.get_project(mock_project.id)._project == mock_project
+
+
+def test_list_project(workspace: WorkspaceBase, mock_project: ProjectModel):
+    assert len(workspace.list_projects()) == 0
+    workspace.add_project(mock_project)
+    assert len(workspace.list_projects()) == 1
+
+
+def test_delete_project(workspace: WorkspaceBase, mock_project: ProjectModel):
+    assert len(workspace.list_projects()) == 0
+    workspace.add_project(mock_project)
+    assert len(workspace.list_projects()) == 1
+    workspace.delete_project(mock_project.id)
+    assert len(workspace.list_projects()) == 0
+
+
+def test_add_snapshot(workspace: WorkspaceBase, mock_project: ProjectModel, mock_snapshot):
+    project = workspace.add_project(mock_project)
+    workspace.add_run(project.id, mock_snapshot)
+    # todo
+
+
+def test_list_snapshot(workspace: WorkspaceBase, mock_project: ProjectModel, mock_snapshot):
+    project = workspace.add_project(mock_project)
+    assert len(workspace.list_runs(project.id)) == 0
+    workspace.add_run(project.id, mock_snapshot)
+    assert len(workspace.list_runs(project.id)) == 1
+
+
+def test_delete_snapshot(workspace: WorkspaceBase, mock_project: ProjectModel, mock_snapshot):
+    project = workspace.add_project(mock_project)
+    assert len(workspace.list_runs(project.id)) == 0
+    run_id = workspace.add_run(project.id, mock_snapshot)
+    assert len(workspace.list_runs(project.id)) == 1
+    workspace.delete_run(project.id, run_id)
+    assert len(workspace.list_runs(project.id)) == 0
+
+
+def test_add_get_dashboard(workspace: WorkspaceBase, mock_project: ProjectModel, mock_dashboard):
+    project = workspace.add_project(mock_project)
+    workspace.save_dashboard(project.id, mock_dashboard)
+    assert workspace.get_dashboard(project.id) == mock_dashboard
