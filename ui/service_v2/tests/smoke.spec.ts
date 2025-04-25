b5cd7f277d02c008f95623e8d913e9dac3c4e299
DimaAmega <dima.amega1@gmail.com>
2025-04-26 01:45:20 +0200

    UI: setup e2e for v2

--- ui/service_v2/tests/smoke.spec.ts
+++ ui/service_v2/tests/smoke.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('Has title', async ({ page }) => {
   await page.goto('/')
@@ -19,17 +19,7 @@ test.skip('Can view Snapshot', async ({ page }) => {
   await expect(page.getByRole('columnheader', { name: 'Actions' })).toBeVisible()
 
   await page.getByRole('link', { name: 'View' }).first().click()
-  await expect(page.getByText('Dataset Drift', { exact: true })).toBeVisible()
-
-  await page.getByRole('tab', { name: 'Test suites' }).click()
-
-  await expect(page.getByRole('columnheader', { name: 'Test Suite ID' })).toBeVisible()
-  await expect(page.getByRole('columnheader', { name: 'Tags' })).toBeVisible()
-  await expect(page.getByRole('columnheader', { name: 'Timestamp' })).toBeVisible()
-  await expect(page.getByRole('columnheader', { name: 'Actions' })).toBeVisible()
-
-  await page.getByRole('link', { name: 'View' }).first().click()
-  await expect(page.getByText('Drift per Column', { exact: true }).first()).toBeVisible()
+  await expect(page.getByText('Dataset Summary', { exact: true })).toBeVisible()
 })
 
 test('Download reports', async ({ page }) => {
@@ -55,46 +45,11 @@ test('Download reports', async ({ page }) => {
   }
 })
 
-test('Download test suites', async ({ page }) => {
+test('Filter Reports by tags', async ({ page }) => {
   await page.goto('/')
   await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
 
-  await page.waitForLoadState('domcontentloaded')
-
-  for (const tab of ['Test Suites']) {
-    await page.getByRole('tab', { name: tab }).click()
-
-    for (const downloadType of ['Download HTML', 'Download JSON']) {
-      await page.getByText('Download').first().click()
-
-      const downloadPromise = page.waitForEvent('download')
-
-      await page.getByRole('menuitem', { name: downloadType }).click()
-
-      const download = await downloadPromise
-
-      expect(await download.failure()).toBeNull()
-    }
-  }
-})
-
-// We don't need this since visual testing enabled
-test.skip('We expect to see at least 3 plotly graphs', async ({ page }) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
-  for (let index = 0; index < 3; index++) {
-    await expect(page.locator('.js-plotly-plot').nth(index)).toBeVisible()
-  }
-})
-
-test('Filter Reports and Test Suites by tags', async ({ page }) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
-
-  for (const [tab, columnName] of [
-    ['Reports', 'Report ID'],
-    ['Test Suites', 'Test Suite ID']
-  ]) {
+  for (const [tab, columnName] of [['Reports', 'Report ID']]) {
     await page.getByRole('tab', { name: tab }).click()
     await expect(page.getByRole('columnheader', { name: columnName })).toBeVisible()
 
@@ -122,58 +77,33 @@ test('Altering project title and description', async ({ page }) => {
 
   await expect(page.getByText('Project List')).toBeVisible()
 
-  await page.getByRole('link', { name: 'Demo project - Reviews' }).hover()
+  await page.getByRole('link', { name: 'Demo project - Bikes' }).hover()
 
   await page
-    .getByText(
-      'Demo project - ReviewsA toy demo project using E-commerce Reviews dataset. Text and tabular data, classification.'
-    )
+    .getByText('Demo project - BikesA toy demo project using Bike Demand forecasting dataset')
     .getByTestId('EditIcon')
     .click()
 
-  await page.locator('input[name="name"]').fill('Reviews new title')
-  await page.locator('input[name="description"]').fill('Reviews new description')
+  await page.locator('input[name="name"]').fill('Bikes new title')
+  await page.locator('input[name="description"]').fill('Bikes new description')
 
   await page.getByText('Save').click()
 
   await page.waitForLoadState('domcontentloaded')
 
-  await expect(page.getByText('Reviews new title')).toBeVisible()
-  await expect(page.getByText('Reviews new description')).toBeVisible()
+  await expect(page.getByText('Bikes new title')).toBeVisible()
+  await expect(page.getByText('Bikes new description')).toBeVisible()
 
-  await page.getByRole('link', { name: 'Reviews new title' }).hover()
+  await page.getByRole('link', { name: 'Bikes new title' }).hover()
 
-  await page.getByText('Reviews new titleReviews new description').getByTestId('EditIcon').click()
+  await page.getByText('Bikes new titleBikes new description').getByTestId('EditIcon').click()
 
-  await page.locator('input[name="name"]').fill('Demo project - Reviews')
+  await page.locator('input[name="name"]').fill('Demo project - Bikes')
   await page
     .locator('input[name="description"]')
-    .fill(
-      'A toy demo project using E-commerce Reviews dataset. Text and tabular data, classification.'
-    )
+    .fill('A toy demo project using Bike Demand forecasting dataset')
 
   await page.getByText('Save').click()
 
   await page.waitForLoadState('domcontentloaded')
-})
-
-// We don't need this since visual testing enabled
-test.skip('Check header of ColumnSummaryMetric', async ({ page }) => {
-  const expectedHeaderCells = ['', 'current', 'reference']
-
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Home', exact: true }).click()
-  await page.getByRole('link', { name: 'Demo project - Reviews', exact: true }).click()
-  await page.getByRole('tab', { name: 'Reports', exact: true }).click()
-  await page.getByRole('link', { name: 'View', exact: true }).first().click()
-  const graph = await page.getByText('OOVnumcurrentreference')
-  await expect(graph).toBeVisible()
-
-  const actualHeaderCells = await graph.locator('thead tr th').allInnerTexts()
-
-  for (let i = 0; i < expectedHeaderCells.length; i++) {
-    expect(expectedHeaderCells[i] === actualHeaderCells[i], {
-      message: `expected: "${expectedHeaderCells[i]}", actual: "${actualHeaderCells[i]}"`
-    }).toBeTruthy()
-  }
 })
--- ui/service_v2/tests/visual.spec.ts
+++ ui/service_v2/tests/visual.spec.ts
@@ -1,19 +1,16 @@
-import { test, expect, Page } from '@playwright/test'
+import { type Page, expect, test } from '@playwright/test'
 
 const goToFirstSnapshotAndExpanSomeWidgets = async ({
   projectName,
-  page,
-  isTestSuite
+  page
 }: {
   page: Page
-  isTestSuite: boolean
+
   projectName: string
 }) => {
   await page.goto('/')
   await page.getByRole('link', { name: projectName, exact: true }).click()
-  await page
-    .getByRole('tab', { name: isTestSuite ? 'Test suites' : 'Reports', exact: true })
-    .click()
+  await page.getByRole('tab', { name: 'Reports', exact: true }).click()
   await page.getByRole('link', { name: 'View', exact: true }).first().click()
   await page.waitForLoadState('networkidle')
 
@@ -40,17 +37,14 @@ const goToFirstSnapshotAndExpanSomeWidgets = async ({
 
 const VisualTestSnapshot = async ({
   page,
-  projectName,
-  isTestSuite
+  projectName
 }: {
   page: Page
   projectName: string
-  isTestSuite: boolean
 }) => {
   await goToFirstSnapshotAndExpanSomeWidgets({
     page,
-    projectName,
-    isTestSuite
+    projectName
   })
 
   await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 150 })
@@ -58,32 +52,27 @@ const VisualTestSnapshot = async ({
 
 const goToSnapshotsList = async ({
   projectName,
-  page,
-  isTestSuite
+  page
 }: {
   page: Page
-  isTestSuite: boolean
   projectName: string
 }) => {
   await page.goto('/')
   await page.getByRole('link', { name: projectName }).click()
-  await page.getByRole('tab', { name: isTestSuite ? 'Test Suites' : 'Reports' }).click()
+  await page.getByRole('tab', { name: 'Reports' }).click()
   await page.waitForLoadState('networkidle')
 }
 
 const VisualTestSnapshotsList = async ({
   page,
-  projectName,
-  isTestSuite
+  projectName
 }: {
   page: Page
   projectName: string
-  isTestSuite: boolean
 }) => {
   await goToSnapshotsList({
     page,
-    projectName,
-    isTestSuite
+    projectName
   })
 
   await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 150 })
@@ -97,13 +86,12 @@ const VisualTestDashboard = async ({ page, projectName }: { page: Page; projectN
 }
 
 const BikesDemoProjectName = 'Demo project - Bikes'
-const ReviewsDemoProjectName = 'Demo project - Reviews'
 
 /////////////////////
 ///   Home
 /////////////////////
 
-test(`Home`, async ({ page }) => {
+test('Home', async ({ page }) => {
   await page.goto('/')
   await page.waitForLoadState('networkidle')
   await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 150 })
@@ -115,11 +103,7 @@ test(`Home`, async ({ page }) => {
 
 for (const project of [BikesDemoProjectName]) {
   test(`Reports List: ${project}`, async ({ page }) => {
-    await VisualTestSnapshotsList({ page, projectName: project, isTestSuite: false })
-  })
-
-  test(`Test Suites List: ${project}`, async ({ page }) => {
-    await VisualTestSnapshotsList({ page, projectName: project, isTestSuite: true })
+    await VisualTestSnapshotsList({ page, projectName: project })
   })
 }
 
@@ -127,7 +111,7 @@ for (const project of [BikesDemoProjectName]) {
 ///   Dashboards
 /////////////////////
 
-for (const project of [BikesDemoProjectName, ReviewsDemoProjectName]) {
+for (const project of [BikesDemoProjectName]) {
   test(`Dashboard: ${project}`, async ({ page }) => {
     await VisualTestDashboard({ page, projectName: project })
   })
@@ -137,12 +121,8 @@ for (const project of [BikesDemoProjectName, ReviewsDemoProjectName]) {
 ///   Snapshots
 /////////////////////
 
-for (const project of [BikesDemoProjectName, ReviewsDemoProjectName]) {
+for (const project of [BikesDemoProjectName]) {
   test(`Report: ${project}`, async ({ page }) => {
-    await VisualTestSnapshot({ page, projectName: project, isTestSuite: false })
+    await VisualTestSnapshot({ page, projectName: project })
   })
 }
-
-test(`Test Suite: ${BikesDemoProjectName}`, async ({ page }) => {
-  await VisualTestSnapshot({ page, projectName: BikesDemoProjectName, isTestSuite: true })
-})
