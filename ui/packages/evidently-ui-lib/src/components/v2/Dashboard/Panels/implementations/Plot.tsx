8ac21dfff50dcb4962b33d0b12a8a670228c8126
DimaAmega <dima.amega1@gmail.com>
2025-05-02 21:52:43 +0200

    migrate mui x charts

--- ui/packages/evidently-ui-lib/src/components/v2/Dashboard/Panels/implementations/Plot.tsx
+++ ui/packages/evidently-ui-lib/src/components/v2/Dashboard/Panels/implementations/Plot.tsx
@@ -1,17 +1,23 @@
 import { Stack } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import { useDrawingArea, useXAxis } from '@mui/x-charts'
-import { BarPlot } from '@mui/x-charts/BarChart/BarPlot'
-import { ChartsAxisHighlight } from '@mui/x-charts/ChartsAxisHighlight/ChartsAxisHighlight'
-import { ChartsGrid } from '@mui/x-charts/ChartsGrid/ChartsGrid'
-import { ChartsLegend } from '@mui/x-charts/ChartsLegend/ChartsLegend'
-import { ChartsTooltip } from '@mui/x-charts/ChartsTooltip/ChartsTooltip'
-import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis/ChartsXAxis'
-import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis/ChartsYAxis'
-import { LinePlot } from '@mui/x-charts/LineChart/LinePlot'
-import { MarkPlot } from '@mui/x-charts/LineChart/MarkPlot'
-import { ResponsiveChartContainer } from '@mui/x-charts/ResponsiveChartContainer'
-import type { SeriesProviderProps } from '@mui/x-charts/context/SeriesProvider'
+import {
+  ChartDataProvider,
+  ChartsAxisHighlight,
+  ChartsGrid,
+  ChartsLegend,
+  ChartsSurface,
+  ChartsTooltip,
+  ChartsXAxis,
+  ChartsYAxis,
+  LinePlot,
+  MarkPlot,
+  blueberryTwilightPalette,
+  useDrawingArea,
+  useXAxis
+} from '@mui/x-charts'
+import { BarPlot } from '@mui/x-charts'
+import type { UseChartSeriesParameters } from '@mui/x-charts/internals'
+
 import dayjs from 'dayjs'
 import type { SeriesModel } from 'evidently-ui-lib/api/types/v2'
 import {
@@ -37,10 +43,9 @@ export type PlotPanelProps = MakePanel<{
   title?: string
   description?: string
   height?: number
-  legendMarginRight?: number
 }>
 
-type SeriesType = SeriesProviderProps['series'][number]
+type SeriesType = Exclude<UseChartSeriesParameters['series'], undefined>[number]
 
 const HighlightSelectionCustom = ({ onSelect }: { onSelect: (index: number) => void }) => {
   const { left, top, width, height } = useDrawingArea()
@@ -101,7 +106,6 @@ export const PlotDashboardPanel = ({
   description,
   height = 350,
   labels,
-  legendMarginRight = 300,
   isStacked
 }: PlotPanelProps) => {
   const viewParams = useDashboardViewParams()
@@ -118,7 +122,8 @@ export const PlotDashboardPanel = ({
 
       const common = {
         label: customLabel || defaultLabel,
-        stack: isStacked ? 'total' : undefined
+        stack: isStacked ? 'total' : undefined,
+        labelMarkType: 'square' as const
       }
 
       if (type === 'line') {
@@ -162,42 +167,47 @@ export const PlotDashboardPanel = ({
         {(title || description) && <Divider sx={{ mb: 2, mt: 1 }} />}
 
         <Box height={height} px={3}>
-          <ResponsiveChartContainer
-            series={series}
-            xAxis={xAxis}
-            margin={{ right: legendMarginRight }}
-          >
-            <BarPlot />
-            <LinePlot />
+          <ChartDataProvider series={series} xAxis={xAxis} colors={blueberryTwilightPalette}>
+            <Stack direction={'row'} gap={1} height={'100%'}>
+              <ChartsSurface>
+                <BarPlot />
+                <LinePlot />
 
-            <ChartsXAxis label='Timestamps' position='bottom' />
-            <ChartsYAxis position='left' />
+                <ChartsXAxis label='Timestamps' position='bottom' />
+                <ChartsYAxis position='left' />
 
-            <ChartsLegend
-              slotProps={{
-                legend: {
-                  labelStyle: { fontSize: 15 },
-                  direction: 'column',
-                  position: { vertical: 'top', horizontal: 'right' },
-                  markGap: 5,
-                  itemGap: 15
-                }
-              }}
-            />
+                <ChartsTooltip trigger={'axis'} />
+                <ChartsGrid horizontal />
+                <MarkPlot />
 
-            <ChartsTooltip trigger={'axis'} />
-            <ChartsGrid horizontal />
-            <MarkPlot />
+                <ChartsAxisHighlight x={'band'} />
 
-            <ChartsAxisHighlight x={'band'} />
+                {OnClickComponent && (
+                  <>
+                    <HighlightSelectionCustom onSelect={(index) => setHighlightInfo({ index })} />
+                    {highlightInfo && <Highlight {...highlightInfo} />}
+                  </>
+                )}
+              </ChartsSurface>
 
-            {OnClickComponent && (
-              <>
-                <HighlightSelectionCustom onSelect={(index) => setHighlightInfo({ index })} />
-                {highlightInfo && <Highlight {...highlightInfo} />}
-              </>
-            )}
-          </ResponsiveChartContainer>
+              <Box maxWidth={300}>
+                <ChartsLegend
+                  slotProps={{
+                    legend: {
+                      direction: 'vertical',
+                      sx: {
+                        fontSize: 15,
+                        overflow: 'scroll',
+                        flexWrap: 'nowrap',
+                        height: '85%',
+                        whiteSpace: 'pre'
+                      }
+                    }
+                  }}
+                />
+              </Box>
+            </Stack>
+          </ChartDataProvider>
 
           {OnClickComponent && selectedSnapshotId && (
             <Box position={'relative'}>
