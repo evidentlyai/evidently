5aa8032d00a7dc00e5f345cb69e2ea9ac38b00b8
Vyacheslav Morov <Liraim@users.noreply.github.com>
2025-05-07 14:26:20 +0200

    Add helper functions for pie and bar panels. (#1590)

--- src/evidently/sdk/panels.py
+++ src/evidently/sdk/panels.py
@@ -45,7 +45,6 @@ def line_plot_panel(
     values: List[PanelMetric],
     description: Optional[str] = None,
     size: str = "full",
-    aggregation: str = "last",
 ) -> DashboardPanelPlot:
     return DashboardPanelPlot(
         title=title,
@@ -54,5 +53,43 @@ def line_plot_panel(
         values=values,
         plot_params={
             "plot_type": "line",
+        },
+    )
+
+
+def bar_plot_panel(
+    title: str,
+    values: List[PanelMetric],
+    description: Optional[str] = None,
+    size: str = "full",
+    stacked: bool = False,
+) -> DashboardPanelPlot:
+    return DashboardPanelPlot(
+        title=title,
+        subtitle=description,
+        size=size,
+        values=values,
+        plot_params={
+            "plot_type": "bar",
+            "is_stacked": stacked,
+        },
+    )
+
+
+def pie_plot_panel(
+    title: str,
+    values: List[PanelMetric],
+    description: Optional[str] = None,
+    size: str = "full",
+    aggregation: str = "last",
+) -> DashboardPanelPlot:
+    return DashboardPanelPlot(
+        title=title,
+        subtitle=description,
+        size=size,
+        values=values,
+        plot_params={
+            "plot_type": "pie",
+            "aggregation": aggregation,
         },
     )
