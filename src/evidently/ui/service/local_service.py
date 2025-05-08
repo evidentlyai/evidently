24f782e402578db007d965c9f7efc0f1f8f3969e
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2025-05-08 18:31:54 +0200

    Change default for request_max_body_size to None. Add cli argument `--litestar-request-max-body-size` to override this parameter if needed.

--- src/evidently/ui/service/local_service.py
+++ src/evidently/ui/service/local_service.py
@@ -2,7 +2,9 @@ import logging
 import os
 import time
 from typing import Dict
+from typing import Optional
 
+from litestar import Litestar
 from litestar import Request
 from litestar import Response
 from litestar.di import Provide
@@ -14,6 +16,7 @@ from evidently.ui.service.api.projects import projects_api_dependencies
 from evidently.ui.service.api.service import service_api
 from evidently.ui.service.api.static import assets_router
 from evidently.ui.service.components.base import AppBuilder
+from evidently.ui.service.components.base import Component
 from evidently.ui.service.components.base import ComponentContext
 from evidently.ui.service.components.base import ServiceComponent
 from evidently.ui.service.components.dashboard import DashboardComponent
@@ -109,9 +112,18 @@ def create_logging() -> dict:
     }
 
 
+class LitestarComponent(Component):
+    __section__ = "litestar"
+    request_max_body_size: Optional[int] = None
+
+    def finalize(self, ctx: ComponentContext, app: Litestar):
+        app.request_max_body_size = self.request_max_body_size
+
+
 class LocalConfig(AppConfig):
     security: SecurityComponent = NoSecurityComponent()
     service: ServiceComponent = LocalServiceComponent()
     storage: StorageComponent = LocalStorageComponent()
     telemetry: TelemetryComponent = TelemetryComponent()
     dashboard: DashboardComponent = DashboardComponent()
+    litestar: LitestarComponent = LitestarComponent()
