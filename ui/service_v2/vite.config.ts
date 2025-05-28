78fa26ac83fdc53dd6f2e716acfd808ff4d3fc8c
DimaAmega <dima.amega1@gmail.com>
2025-05-28 20:24:49 +0200

    commit UI assets

--- ui/service_v2/vite.config.ts
+++ ui/service_v2/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://127.0.0.1:8000'
+      '/api': 'https://demo.evidentlyai.com'
     }
   },
   build: {
