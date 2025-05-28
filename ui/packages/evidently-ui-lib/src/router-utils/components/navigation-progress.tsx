09b043a145aae1abf5c4c7defc8c0df7c67bdf4f
DimaAmega <dima.amega1@gmail.com>
2025-05-28 20:24:16 +0200

    better

--- ui/packages/evidently-ui-lib/src/router-utils/components/navigation-progress.tsx
+++ ui/packages/evidently-ui-lib/src/router-utils/components/navigation-progress.tsx
@@ -1,4 +1,5 @@
 import { Box, CircularProgress, LinearProgress } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { useFetchers, useNavigation, useRevalidator } from 'react-router-dom'
 
 export const NavigationProgressWithFetchers = () => {
@@ -25,9 +26,7 @@ export const NavigationProgressWithFetchers = () => {
 export const NavigationProgress = () => {
   const navigation = useNavigation()
 
-  const isNavigation = navigation.state !== 'idle'
-
-  return <CustomProgress isProgress={isNavigation} />
+  return <CustomProgress isProgress={navigation.state !== 'idle'} />
 }
 
 export const FetchersProgress = () => {
@@ -40,14 +39,28 @@ export const FetchersProgress = () => {
     return null
   }
 
-  return (
-    <>
-      <CircularProgress color='warning' size={15} />
-    </>
-  )
+  return <CircularProgress color='warning' size={15} />
 }
 
-const CustomProgress = ({ isProgress }: { isProgress?: boolean }) => {
+const CustomProgress = ({ isProgress }: { isProgress: boolean }) => {
+  const [isProgressProxy, setIsProgressProxy] = useState(false)
+  const [progressJustFinished, setProgressJustFinished] = useState(false)
+
+  if (!isProgressProxy && isProgress) {
+    setIsProgressProxy(true)
+  }
+
+  if (isProgressProxy && !isProgress) {
+    setIsProgressProxy(false)
+    setProgressJustFinished(true)
+  }
+
+  useEffect(() => {
+    if (progressJustFinished) {
+      setTimeout(() => setProgressJustFinished(false), 500)
+    }
+  }, [progressJustFinished])
+
   return (
     <Box width={1} sx={{ position: 'fixed', top: 0, left: 0, zIndex: 1000 }}>
       <Box
@@ -55,11 +68,19 @@ const CustomProgress = ({ isProgress }: { isProgress?: boolean }) => {
         sx={[
           {
             background: 'linear-gradient(90deg, #ed0500 0%, #FF1BE1 70%)',
-            transitionProperty: 'width',
-            transitionTimingFunction: 'cubic-bezier(0.22, 0.61, 0.36, 1);'
+            transitionProperty: 'width'
           },
           { width: 0 },
-          Boolean(isProgress) && { transitionDuration: '10s', width: 0.8 }
+          isProgressProxy && {
+            width: 0.8,
+            transitionDuration: '10s',
+            transitionTimingFunction: 'cubic-bezier(0.22, 0.61, 0.36, 1)'
+          },
+          progressJustFinished && {
+            width: 1,
+            transitionDuration: '0.4s',
+            transitionTimingFunction: 'linear'
+          }
         ]}
       />
     </Box>
