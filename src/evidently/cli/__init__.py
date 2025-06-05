42e2f920d95306bad157ec53c861a8b000225210
mike0sv <mike0sv@gmail.com>
2025-06-05 18:24:33 +0100

    add run report and descriptors cli

--- src/evidently/cli/__init__.py
+++ src/evidently/cli/__init__.py
@@ -1,9 +1,10 @@
 from evidently.cli.demo_project import generate_demo_project
 from evidently.cli.legacy_ui import legacy_ui
 from evidently.cli.main import app
+from evidently.cli.report import run_report
 from evidently.cli.ui import ui
 
-__all__ = ["app", "ui", "legacy_ui", "generate_demo_project"]
+__all__ = ["app", "ui", "legacy_ui", "generate_demo_project", "run_report"]
 
 if __name__ == "__main__":
     app()
