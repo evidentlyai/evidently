1929b719944d33bd3c82c2cbdb881c18f18efb99
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2025-07-11 21:02:44 +0400

    UI: create `projects` crumb (#1656)

--- ui/service_v2/src/routes/src/index.tsx
+++ ui/service_v2/src/routes/src/index.tsx
@@ -9,6 +9,7 @@ import { Route } from 'evidently-ui-lib/router-utils/router-builder'
 import { Home } from './home/import'
 import { LoadPanelPointsAPIV2 } from './load-panel-points/import'
 import { Project } from './project/import'
+import { ProjectsCrumbPrefixRoute } from './projects-crumb-prefix/import'
 import { ProjectsLayout } from './projects-layout/import'
 import { ProjectsList } from './projects-list/import'
 import { ReportsLayout } from './reports-layout/import'
@@ -17,20 +18,24 @@ import { ReportsList } from './reports-list/import'
 export const routes = [
   Route(Home, {
     children: [
-      Route(ProjectsList, { index: true } as const),
-      Route(ProjectsLayout, {
-        path: 'projects',
+      Route(ProjectsCrumbPrefixRoute, {
         children: [
-          Route(Project, {
-            path: ':projectId',
+          Route(ProjectsList, { index: true } as const),
+          Route(ProjectsLayout, {
+            path: 'projects',
             children: [
-              Route(DashboardLazy, { index: true } as const),
-              Route(LoadPanelPointsAPIV2, { path: 'load-panel-points' } as const),
-              Route(ReportsLayout, {
-                path: 'reports',
+              Route(Project, {
+                path: ':projectId',
                 children: [
-                  Route(ReportsList, { index: true } as const),
-                  Route(SnapshotIdLazy, { path: ':snapshotId' } as const)
+                  Route(DashboardLazy, { index: true } as const),
+                  Route(LoadPanelPointsAPIV2, { path: 'load-panel-points' } as const),
+                  Route(ReportsLayout, {
+                    path: 'reports',
+                    children: [
+                      Route(ReportsList, { index: true } as const),
+                      Route(SnapshotIdLazy, { path: ':snapshotId' } as const)
+                    ]
+                  } as const)
                 ]
               } as const)
             ]
