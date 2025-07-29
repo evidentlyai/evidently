6eedf72ccce5afbfcb9c9427214fcada10a70a43
Vyacheslav Morov <vyacheslav.morov@gmail.com>
2025-07-30 00:57:54 +0200

    Add tests and support passing metric kwargs directly to generator.

--- tests/future/generators/test_generator.py
+++ tests/future/generators/test_generator.py
@@ -3,6 +3,7 @@ import pandas as pd
 from evidently.core.report import Report
 from evidently.generators import ColumnMetricGenerator
 from evidently.presets import ValueStats
+from evidently.tests import gt
 
 
 def test_generator_renders():
@@ -10,3 +11,10 @@ def test_generator_renders():
     report = Report([generator])
     snapshot = report.run(pd.DataFrame(data={"a": [1, 2, 3, 4], "b": [1, 2, 3, 4]}))
     assert len(snapshot._widgets) == 2
+
+
+def test_generator_kwargs():
+    generator = ColumnMetricGenerator(ValueStats, columns=["a", "b"], metric_kwargs={"tests": [gt(0)]})
+    generator2 = ColumnMetricGenerator(ValueStats, columns=["a", "b"], tests=[gt(0)])
+
+    assert generator.dict() == generator2.dict()
