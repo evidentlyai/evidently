aa65ca855fd89d695ce9a2bb8aa4f4693e064d60
DimaAmega <dima.amega1@gmail.com>
2025-08-06 17:39:56 +0200

    Update Vite plugin from @vitejs/plugin-react-swc to @vitejs/plugin-react-oxc version 0.2.3

--- ui/service_v2/vite.config.ts
+++ ui/service_v2/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-oxc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
