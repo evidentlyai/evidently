e7b82e487b3cbe1a7d7aeaeac5fe07ded3c60b02
Vyacheslav Morov <Liraim@users.noreply.github.com>
2025-09-15 12:13:22 +0100

    Tracing support for guardrails. (#1708)

--- src/evidently/guardrails/decorators.py
+++ src/evidently/guardrails/decorators.py
@@ -6,6 +6,13 @@ from typing import Union
 from evidently.guardrails.core import GuardrailBase
 from evidently.guardrails.core import validate_guards
 
+try:
+    import tracely
+
+    tracely_installed = True
+except ImportError:
+    tracely_installed = False
+
 
 def guard(guard: Union[GuardrailBase, List[GuardrailBase]], input_arg: str = "input"):
     def decorator(func):
@@ -18,8 +25,13 @@ def guard(guard: Union[GuardrailBase, List[GuardrailBase]], input_arg: str = "in
             if input_arg not in bound.arguments:
                 raise Exception(f"{input_arg} is not a valid argument")
             if isinstance(guard, list):
+                if tracely_installed and tracely.get_current_span():
+                    guard_names = "|".join([g.name() for g in guard])
+                    tracely.get_current_span().set_context_value("evidently.guardrails", guard_names)
                 validate_guards(bound.arguments[input_arg], guard)
             else:
+                if tracely_installed and tracely.get_current_span():
+                    tracely.get_current_span().set_context_value("evidently.guardrails", guard.name())
                 guard.validate(bound.arguments[input_arg])
             return func(*args, **kwargs)
 
