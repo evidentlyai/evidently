d89a4dd7cbc64020c0773fe98dc689078b279187
DimaAmega <dima.amega1@gmail.com>
2025-11-20 15:53:44 +0100

    UI: add `preloadAllScripts` plugin

--- ui/service/vite.config.ts
+++ ui/service/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
 
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), preloadAllScripts()],
   server: {
     port: 3000,
     proxy: {
@@ -32,3 +35,36 @@ export default defineConfig({
     }
   }
 })
+
+
+function preloadAllScripts() {
+  const [headClosedTag, tabs4, tabs2] = ['</head>', '    ', '  ']
+
+  return {
+    name: 'insert modulepreload additional js chunk link tags',
+    transformIndexHtml: (html, ctx) => {
+      if (!ctx.bundle) {
+        return
+      }
+
+      const scriptNames = Object.values(ctx.bundle)
+        .map(({ type, fileName }) =>
+          type === 'asset' || html.includes(fileName)
+            ? // return `false` to skip file if
+              // file is css/img etc, or if
+              // this file already in html
+              false
+            : fileName
+        )
+        .filter((e): e is string => typeof e === 'string')
+
+      const modulepreloadLinks = scriptNames
+        .map((scriptName) => `${tabs4}<link rel="modulepreload" crossorigin href="/${scriptName}">`)
+        .join('\n')
+
+      const additionalLinks = `\n${tabs4}${'<!-- Preload all additional js chunks -->'}\n${modulepreloadLinks}\n${tabs2}`
+
+      return html.replace(headClosedTag, `${additionalLinks}${headClosedTag}`)
+    }
+  } satisfies PluginOption
+}
