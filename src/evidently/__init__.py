f1bd3d26368e8dcce40e0bbda2a3eb1ab153d1b0
DimaAmega <dima.amega1@gmail.com>
2025-12-18 00:15:32 +0100

    remove unused

--- src/evidently/__init__.py
+++ src/evidently/__init__.py
@@ -24,12 +24,10 @@ from evidently.legacy.core import ColumnType
 from . import _registry
 from ._version import __version__
 from ._version import version_info
-from .nbextension import _jupyter_nbextension_paths
 
 __all__ = [
     "__version__",
     "version_info",
-    "_jupyter_nbextension_paths",
     "_registry",
     "Report",
     "Run",
